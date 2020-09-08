363873b7aa642516c8b2dc30ff09dbc43f4f7d70
Kyle MacDonald <kmacdonald@hashicorp.com>
2020-09-08 15:06:24 -0400

    jest: configure an svg mock

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleNameMapper: {
     '\\.graphql$': '../../__test-helpers/mocks/graphql-fragment-mock.js',
+    '\\.svg$': '../../__test-helpers/mocks/svg-mock.js',
   },
   verbose: true,
 }
