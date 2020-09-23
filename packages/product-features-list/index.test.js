90e99537a7addbc25e2ae362d8769ee6382635c8
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-09-23 12:46:30 -0400

    Prep for tests

--- packages/product-features-list/index.test.js
+++ packages/product-features-list/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import ProductFeaturesList from './dist/index'
+import ProductFeaturesList from './'
 
 const propsBase = {
   heading: 'Why Nomad?',
