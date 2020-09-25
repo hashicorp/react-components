49baba711500ae8e867e2918971a0b5297175e39
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-25 18:00:23 -0400

    add call to action component

---
+++ packages/call-to-action/index.js
@@ -0,0 +1,67 @@
+import Button from '@hashicorp/react-button'
+
+function CallToAction(props) {
+  const { heading, content, links, variant, brand, theme } = props
+  if (!heading && !content) {
+    throw new Error('<CallToAction /> requires either heading or content')
+  }
+  const hasLinks = links && links.length > 0
+  if (hasLinks && links.filter((l) => !l.text || !l.url).length > 0) {
+    throw new Error('<CallToAction /> `links` must have both a title and a URL')
+  }
+  return (
+    <div
+      className={`g-call-to-action variant-${variant} theme-${theme} brand-${brand}`}
+    >
+      <div className="g-grid-container">
+        {heading && (
+          <h2 data-testid="heading" className="g-type-display-2">
+            {heading}
+          </h2>
+        )}
+        <div className="content-and-links">
+          {content && (
+            <p data-testid="content" className="g-type-body-large">
+              {content}
+            </p>
+          )}
+          {links && (
+            <div data-testid="links" className="links">
+              {links.map((link, stableIdx) => {
+                const buttonVariant =
+                  variant === 'links'
+                    ? 'tertiary-neutral'
+                    : stableIdx === 0
+                    ? 'primary'
+                    : 'secondary'
+                const linkType =
+                  variant === 'links' ? link.type || 'inbound' : link.type
+                return (
+                  <Button
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={stableIdx}
+                    linkType={linkType}
+                    theme={{
+                      variant: buttonVariant,
+                      brand,
+                      background: theme,
+                    }}
+                    title={link.text}
+                    url={link.url}
+                  />
+                )
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+CallToAction.defaultProps = {
+  theme: 'light',
+  variant: 'centered',
+}
+
+export default CallToAction
---
+++ packages/call-to-action/index.test.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import CallToAction from './dist/index.js'
+
+const baseProps = {
+  heading: 'Lorem Ipsum Dolor Sit Amet',
+  content:
+    'Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  links: [
+    {
+      text: 'First Action',
+      url: 'https://www.example.com',
+    },
+    {
+      text: 'Second Action',
+      url: 'https://www.example.com',
+    },
+  ],
+}
+
+describe('<CallToAction />', () => {
+  it('should render a `.g-call-to-action` <div> root element', () => {
+    const { container } = render(<CallToAction {...baseProps} />)
+    const rootElem = container.firstChild
+    expect(rootElem.tagName).toBe('DIV')
+    expect(rootElem).toHaveClass('g-call-to-action')
+  })
+
+  it('should display simple heading and content strings and links', () => {
+    const { heading, content } = baseProps
+    render(<CallToAction {...baseProps} />)
+    expect(screen.getByText(heading)).toBeVisible()
+    expect(screen.getByText(content)).toBeVisible()
+    expect(screen.getByTestId('heading')).toBeVisible()
+    expect(screen.getByTestId('content')).toBeVisible()
+    expect(screen.getByTestId('links')).toBeVisible()
+  })
+
+  it('should render links passed to it', () => {
+    render(<CallToAction {...baseProps} />)
+    baseProps.links.forEach((link) => {
+      expect(screen.getByText(link.text)).toBeVisible()
+    })
+  })
+
+  it('should throw an error when neither a heading or content is supplied', () => {
+    const basePropsModified = Object.assign({}, baseProps)
+    delete basePropsModified.heading
+    delete basePropsModified.content
+    //  Suppress console.error for this test, we expect an error
+    jest.spyOn(console, 'error')
+    global.console.error.mockImplementation(() => {})
+    expect(() => {
+      render(<CallToAction {...basePropsModified} />)
+    }).toThrowError()
+    //  Restore console.error for further tests
+    global.console.error.mockRestore()
+  })
+
+  it('should throw an error when a link with missing properties is supplied', () => {
+    const basePropsModified = Object.assign({}, baseProps)
+    delete basePropsModified.links
+    //  Suppress console.error for this test, we expect an error
+    jest.spyOn(console, 'error')
+    global.console.error.mockImplementation(() => {})
+    const urlButNoText = [{ url: 'URL but no text' }]
+    expect(() => {
+      render(<CallToAction {...basePropsModified} links={urlButNoText} />)
+    }).toThrowError()
+    const textButNoUrl = [{ text: 'text But No URL' }]
+    expect(() => {
+      render(<CallToAction {...basePropsModified} links={textButNoUrl} />)
+    }).toThrowError()
+    //  Restore console.error for further tests
+    global.console.error.mockRestore()
+  })
+
+  it('should not display an empty heading tag when no heading is passed', () => {
+    const basePropsModified = Object.assign({}, baseProps)
+    delete basePropsModified.heading
+
+    render(<CallToAction {...basePropsModified} />)
+    expect(screen.queryByTestId('heading')).not.toBeInTheDocument()
+  })
+
+  it('should not display an empty content container when no content is passed', () => {
+    const basePropsModified = Object.assign({}, baseProps)
+    delete basePropsModified.content
+
+    render(<CallToAction {...basePropsModified} />)
+    expect(screen.queryByTestId('content')).not.toBeInTheDocument()
+  })
+
+  it('should not display an empty links container with not links provided', () => {
+    const basePropsModified = Object.assign({}, baseProps)
+    delete basePropsModified.links
+
+    const { queryByTestId } = render(<CallToAction {...basePropsModified} />)
+    expect(queryByTestId('links')).not.toBeInTheDocument()
+  })
+})
