c8c85811bc3153e86911f3fbc386042ee26fd758
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-28 13:17:33 -0400

    fix tests

--- packages/subnav/index.test.js
+++ packages/subnav/index.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import Subnav from './dist/index.js'
+import Subnav from './'
 
 const baseProps = {
   titleLink: {
@@ -59,16 +64,18 @@ const baseProps = {
 }
 
 describe('<Subnav />', () => {
-  it('should render a `.g-subnav` <nav> root element', () => {
+  it('should render a `.g-subnav` <nav> root element', async () => {
     const { container } = render(<Subnav {...baseProps} />)
+    await waitForGithubStarsUpdate()
     const rootElem = container.firstChild
     expect(rootElem.tagName).toBe('NAV')
     expect(rootElem).toHaveClass('g-subnav')
   })
 
-  it('should render all top-level menuItems as expected', () => {
+  it('should render all top-level menuItems as expected', async () => {
     const { menuItems } = baseProps
     render(<Subnav {...baseProps} />)
+    await waitForGithubStarsUpdate()
     menuItems.forEach((menuItem) => {
       if (menuItem === 'divider') return true
       const menuItemTextElem = screen.getAllByText(menuItem.text)[0]
@@ -81,8 +88,9 @@ describe('<Subnav />', () => {
     })
   })
 
-  it('should highlight the active link', () => {
+  it('should highlight the active link', async () => {
     render(<Subnav {...baseProps} />)
+    await waitForGithubStarsUpdate()
     const activeMenuItem = baseProps.menuItems.filter((menuItem) => {
       return menuItem.url === baseProps.currentPath
     })[0]
@@ -92,11 +100,12 @@ describe('<Subnav />', () => {
     expect(linkElem.classList.contains('is-active')).toBe(true)
   })
 
-  it('should highlight the dropdown that contains the active link', () => {
+  it('should highlight the dropdown that contains the active link', async () => {
     const testProps = Object.assign({}, baseProps, {
       currentPath: '/use-cases/infrastructure-as-code',
     })
     render(<Subnav {...testProps} />)
+    await waitForGithubStarsUpdate()
     const activeMenuItem = testProps.menuItems.filter((menuItem) => {
       if (!menuItem.submenu) return false
       const hasActiveChild = menuItem.submenu.reduce((acc, s) => {
@@ -110,11 +119,12 @@ describe('<Subnav />', () => {
     expect(linkElem.classList.contains('is-active')).toBe(true)
   })
 
-  it('should use a provided Link component to render all links', () => {
+  it('should use a provided Link component to render all links', async () => {
     function TestLink({ href }) {
       return <div data-testid={href} />
     }
     render(<Subnav {...baseProps} Link={TestLink} />)
+    await waitForGithubStarsUpdate()
     //  Test for Link use in title link
     const { titleLink } = baseProps
     const titleElem = screen.getAllByTestId(titleLink.url)[0]
@@ -138,9 +148,10 @@ describe('<Subnav />', () => {
     })
   })
 
-  it('should render ctaLinks as hyperlinks', () => {
+  it('should render ctaLinks as hyperlinks', async () => {
     const { ctaLinks } = baseProps
     render(<Subnav {...baseProps} />)
+    await waitForGithubStarsUpdate()
     ctaLinks.forEach((ctaLink) => {
       const ctaTextElem = screen.getAllByText(ctaLink.text)[0]
       expect(ctaTextElem).toBeVisible()
@@ -149,27 +160,30 @@ describe('<Subnav />', () => {
     })
   })
 
-  it('should render the title linked to the titleUrl', () => {
+  it('should render the title linked to the titleUrl', async () => {
     const { titleLink } = baseProps
     const { text, url } = titleLink
     render(<Subnav {...baseProps} />)
+    await waitForGithubStarsUpdate()
     const titleElem = screen.getAllByTitle(text)[0]
     expect(titleElem).toBeVisible()
     expect(titleElem.tagName).toBe('A')
     expect(titleElem.getAttribute('href')).toBe(url)
   })
 
-  it('should render product logos for product titles, with alt text', () => {
-    const logoSlugs = [
-      'terraform',
-      'vault',
-      'consul',
-      'nomad',
-      'packer',
-      'vagrant',
-    ]
-    logoSlugs.forEach((logoSlug) => {
+  const logoSlugs = [
+    'terraform',
+    'vault',
+    'consul',
+    'nomad',
+    'packer',
+    'vagrant',
+  ]
+
+  logoSlugs.map((logoSlug) => {
+    it(`should render product logos for product title "${logoSlug}", with alt text`, async () => {
       render(<Subnav {...baseProps} titleLink={{ text: logoSlug }} />)
+      await waitForGithubStarsUpdate()
       const logoElem = screen.getAllByTitle(logoSlug)[0]
       expect(logoElem).toBeVisible()
       expect(logoElem.querySelector('svg')).toBeVisible()
@@ -182,3 +196,11 @@ describe('<Subnav />', () => {
 
   it.todo('should allow keyboard access to submenus')
 })
+
+async function waitForGithubStarsUpdate() {
+  return waitFor(() =>
+    expect(screen.queryByTestId('github-stars')?.innerHTML).not.toBe(
+      '<span>—</span>'
+    )
+  )
+}
