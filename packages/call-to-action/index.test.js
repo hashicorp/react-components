6abc86f3731cdfe39ccfc6ad7f39bea20034beb1
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-28 13:30:11 -0400

    fix tests

--- packages/call-to-action/index.test.js
+++ packages/call-to-action/index.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import CallToAction from './dist/index.js'
+import CallToAction from './'
 
 const baseProps = {
   heading: 'Lorem Ipsum Dolor Sit Amet',
