44ce252b595d7b5995b2d8f9cbbb9c255013e416
Jonathan Neal <jonathantneal@hotmail.com>
2020-09-29 17:25:25 -0400

    Add EnterpriseAlert component (#12)

---
+++ packages/enterprise-alert/README.md
@@ -0,0 +1,9 @@
+# EnterpriseAlert
+
+The `<EnterpriseAlert>` component renders an alert used to note the requirement of an enterprise product.
+
+## Installation
+
+```sh
+npm install @hashicorp/react-enterprise-alert
+```
---
+++ packages/enterprise-alert/index.test.jsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import EnterpriseAlert from './'
+
+describe('<EnterpriseAlert />', () => {
+  test('renders correctly', () => {
+    const { container } = render(<EnterpriseAlert product="consul" />)
+    const rootElem = container.firstChild
+    expect(rootElem).toHaveClass('g-enterprise-alert')
+    expect(rootElem).toHaveClass(`consul`)
+    expect(rootElem).not.toHaveClass(`inline`)
+    expect(rootElem.querySelector('a')).toHaveAttribute(
+      'href',
+      `https://www.hashicorp.com/products/consul`
+    )
+    expect(rootElem.querySelector('strong')).toHaveTextContent(
+      `Consul Enterprise`
+    )
+  })
+
+  test('renders with { product: "nomad", inline: true } options', () => {
+    const { container } = render(
+      <EnterpriseAlert product="nomad" inline={true} />
+    )
+    const rootElem = container.firstChild
+    expect(rootElem).toHaveClass('g-enterprise-alert')
+    expect(rootElem).toHaveClass(`nomad`)
+    expect(rootElem).toHaveClass(`inline`)
+    expect(rootElem.querySelector('a')).toBe(null)
+    expect(rootElem.querySelector('strong')).toBe(null)
+  })
+
+  test('renders with { product: "consul", content: "Hello, World!" } options', () => {
+    const { container } = render(
+      <EnterpriseAlert product="consul">Hello, World!</EnterpriseAlert>
+    )
+    const rootElem = container.firstChild
+    expect(rootElem).toHaveClass('g-enterprise-alert')
+    expect(rootElem).toHaveClass(`consul`)
+    expect(rootElem).not.toHaveClass(`inline`)
+    expect(rootElem.querySelector('p')).toHaveTextContent(`Hello, World!`)
+  })
+})
