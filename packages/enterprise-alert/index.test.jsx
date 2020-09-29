da6b7b90ad5b6b7c6ce6c5bec9a743bdb066a8f2
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-29 17:21:51 -0400

    cleanup

--- packages/enterprise-alert/index.test.jsx
+++ packages/enterprise-alert/index.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import EnterpriseAlert from './index.jsx'
+import EnterpriseAlert from './'
 
 describe('<EnterpriseAlert />', () => {
   test('renders correctly', () => {
@@ -10,12 +10,19 @@ describe('<EnterpriseAlert />', () => {
     expect(rootElem).toHaveClass('g-enterprise-alert')
     expect(rootElem).toHaveClass(`consul`)
     expect(rootElem).not.toHaveClass(`inline`)
-    expect(rootElem.querySelector('a')).toHaveAttribute('href', `https://www.hashicorp.com/products/consul`)
-    expect(rootElem.querySelector('strong')).toHaveTextContent(`Consul Enterprise`)
+    expect(rootElem.querySelector('a')).toHaveAttribute(
+      'href',
+      `https://www.hashicorp.com/products/consul`
+    )
+    expect(rootElem.querySelector('strong')).toHaveTextContent(
+      `Consul Enterprise`
+    )
   })
 
-	test('renders with { product: "nomad", inline: true } options', () => {
-    const { container } = render(<EnterpriseAlert product="nomad" inline={true} />)
+  test('renders with { product: "nomad", inline: true } options', () => {
+    const { container } = render(
+      <EnterpriseAlert product="nomad" inline={true} />
+    )
     const rootElem = container.firstChild
     expect(rootElem).toHaveClass('g-enterprise-alert')
     expect(rootElem).toHaveClass(`nomad`)
@@ -24,8 +31,10 @@ describe('<EnterpriseAlert />', () => {
     expect(rootElem.querySelector('strong')).toBe(null)
   })
 
-	test('renders with { product: "consul", content: "Hello, World!" } options', () => {
-    const { container } = render(<EnterpriseAlert product="consul" content="Hello, World!" />)
+  test('renders with { product: "consul", content: "Hello, World!" } options', () => {
+    const { container } = render(
+      <EnterpriseAlert product="consul">Hello, World!</EnterpriseAlert>
+    )
     const rootElem = container.firstChild
     expect(rootElem).toHaveClass('g-enterprise-alert')
     expect(rootElem).toHaveClass(`consul`)
