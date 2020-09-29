6c38de47ad2b27c812a3943f548c617bc77c8260
Brandon Romano <BrandonRRomano@gmail.com>
2020-09-28 17:09:06 -0700

    Update ProductFeaturesList to support links

--- packages/product-features-list/index.js
+++ packages/product-features-list/index.js
@@ -1,9 +1,11 @@
+import Button from '@hashicorp/react-button'
+
 export default function ProductFeaturesList({ heading, features }) {
   return (
     <div className="g-product-features-list g-grid-container">
       <h2 className="g-type-display-2">{heading}</h2>
       <div className="features-container">
-        {features.map(({ title, content, icon }) => (
+        {features.map(({ title, content, icon, link }) => (
           <div key={title} className="feature">
             <div className="feature-icon">
               <img src={icon} alt={title} />
@@ -11,6 +13,14 @@ export default function ProductFeaturesList({ heading, features }) {
             <div className="content">
               <h4 className="g-type-display-4">{title}</h4>
               <p className="g-type-body-small">{content}</p>
+              {link && (
+                <Button
+                  linkType={link.type}
+                  theme={{ variant: 'tertiary-neutral'}}
+                  title={link.text}
+                  url={link.url}
+                />
+              )}
             </div>
           </div>
         ))}
