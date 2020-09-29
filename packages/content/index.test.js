b3d09e91693bdc155d17927bd53dcf5cbeffd10f
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-29 16:33:27 -0400

    shore up tests

--- packages/content/index.test.js
+++ packages/content/index.test.js
@@ -1,7 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Content from './'
 
-// TODO: rewrite this
+test('renders content accurately', () => {
+  render(<Content content={<p data-testid="test">test</p>} />)
+  expect(screen.getByTestId('test')).toHaveTextContent('test')
+})
 
-test('works', () => {
-  expect(true).toBe(true)
+test('renders a product class if one is passed', () => {
+  const { container } = render(<Content product="test" />)
+  expect(container.firstChild).toHaveClass('test')
 })
