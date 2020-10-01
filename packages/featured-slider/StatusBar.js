566b046a5513b6d0ff0c2bab322113d9a27006a4
Jeff Escalante <jescalan@users.noreply.github.com>
2020-10-01 13:01:57 -0400

    add featured slider

---
+++ packages/featured-slider/StatusBar.js
@@ -0,0 +1,14 @@
+export default function StatusBar({ theme, active, timing, brand }) {
+  return (
+    <div className={`progress-bar ${theme}`}>
+      <span
+        className={`${active ? ' active' : ''} ${brand ? brand : ''}`}
+        style={
+          active
+            ? { animationDuration: `${timing}s` }
+            : { animationDuration: '0s' }
+        }
+      />
+    </div>
+  )
+}
