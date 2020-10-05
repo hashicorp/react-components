adc0e1b31c003a209169108c1c82c81349f63bfc
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2020-10-05 17:09:39 -0500

    Simplify and achieve new design thru layout

--- packages/hashi-stack-menu/browse-panel/index.jsx
+++ packages/hashi-stack-menu/browse-panel/index.jsx
@@ -3,7 +3,7 @@ import styles from './browse-panel.module.css'
 export default function BrowsePanel({ isOpen, children }) {
   return (
     <section className={`${styles.browsePanel} ${isOpen ? styles.isOpen : ''}`}>
-      <div className={styles.panelInner}>{children}</div>
+      {children}
     </section>
   )
 }
