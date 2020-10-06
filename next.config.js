2eda1a8916f46c563292e3038e97b8a9995e9b5b
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2020-10-06 15:21:48 -0500

    Add a few tweaks for placement on io sites

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ const withHashicorp = require('@hashicorp/nextjs-scripts')
 const withOctavo = require('@hashicorp/octavo')
 
 module.exports = withHashicorp({
-  transpileModules: ['@hashicorp/octavo', '@hashicorp/react-.*'],
-})(withOctavo({ componentsRoot: 'packages/*' })())
+  transpileModules: [
+    '@hashicorp/octavo',
+    '@hashicorp/react-.*',
+  ],
+})(
+  withOctavo({
+    componentsRoot: 'packages/*',
+  })({ svgo: { plugins: [{ removeViewBox: false }] } })
+)
