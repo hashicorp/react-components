371bb8dcc18d0e901b3e9dbb9115978c9abcaf28
Jeff Escalante <jescalan@users.noreply.github.com>
2020-10-12 17:06:21 -0400

    add select input

---
+++ packages/select-input/index.test.js
@@ -0,0 +1,14 @@
+import SelectInput from './'
+
+test.todo('opens the select menu when clicked')
+test.todo('closes the select menu when clicked after its open')
+test.todo('closes the select menu when theres a click outside after its open')
+test.todo('selects an option when the menu is open and an option is clicked')
+test.todo(
+  'displays the label and of each option and uses the name as the programmatic value'
+)
+test.todo('fires onValueChange function when an option is selected')
+test.todo('displays an alternate placeholder if `defaultLabel` is specified')
+test.todo('displays a custom label if `label` is specified')
+test.todo('uses the `name` if specified for the input')
+test.todo('sets the `value` is one is provided via prop')
