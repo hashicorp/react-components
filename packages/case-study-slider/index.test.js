10e63efb4645f9f73aa25d86d75f586043825196
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-10-21 15:14:12 -0400

    More 🔪

--- packages/case-study-slider/index.test.js
+++ packages/case-study-slider/index.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import CaseStudySlider from './'
-// import StatusBar from './StatusBar'
 import transformProps from '../../__test-helpers/transform-props'
 
 const defaultProps = transformProps(__dirname)
