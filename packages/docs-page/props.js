022f3d2cee162e79368b892ebd616a84ccb01391
Jeff Escalante <jescalan@users.noreply.github.com>
2020-11-09 16:24:25 -0500

    full refactor of docs-page
    - makes the component more dry
    - nows works specifcially with next-mdx-remote
    - includes server-side utilities for fetching static props/paths

--- packages/docs-page/props.js
+++ packages/docs-page/props.js
@@ -1,76 +1,59 @@
 module.exports = {
   product: {
-    type: 'string',
-    description: 'Name of the product this page is being rendered for',
-    control: { type: 'select' },
-    options: [
-      'terraform',
-      'vault',
-      'nomad',
-      'consul',
-      'packer',
-      'vagrant',
-      'red',
-      'blue',
-    ],
+    type: 'object',
+    description: 'Name and slug of the product this page is being rendered for',
+    properties: {
+      name: {
+        type: 'string',
+        description: 'Human-readable proper case product name',
+      },
+      slug: {
+        type: 'string',
+        description: 'HashiCorp product slug',
+        control: { type: 'select' },
+        options: [
+          'terraform',
+          'vault',
+          'nomad',
+          'consul',
+          'packer',
+          'vagrant',
+          'red',
+          'blue',
+        ],
+      },
+    },
   },
-  resourceUrl: {
+  subpath: {
     type: 'string',
     description:
-      'A link to each page in github, for the "edit on github" link at the bottom of the page',
+      'The path this page is rendering under, for example "docs" or "api-docs". Passed directly to the `category` prop of `@hashicorp/react-docs-sidenav`',
   },
-  head: {
+  order: {
     type: 'object',
-    description: 'Configuration passed to the `react-head` component',
-    required: true,
-    properties: {
-      is: {
-        type: 'React.Element',
-        description: 'Pass in `next/head` here',
-      },
-      title: {
-        type: 'string',
-        description:
-          'The title of each documentation page should be rendered here',
-      },
-      description: {
-        type: 'string',
-        description:
-          'The description of each documentation page should be rendered here',
-      },
-      siteName: {
-        type: 'string',
-        description: 'Name of the website to be passed to the og:site_name tag',
-      },
-    },
+    description:
+      'Pass in the export of a `data/xxx-navigation.js` file, this is the user-defined navigation order and structure. Passed directly to the `order` prop to `@hashicorp/react-docs-sidenav` - see that component for details on object structure.',
   },
-  sidenav: {
+  additionalComponents: {
     type: 'object',
-    description: 'Configuration passed to the `react-docs-sidenav` component',
-    required: true,
-    properties: {
-      Link: {
-        type: 'React.Element',
-        description: 'Pass in `next/link` here',
-      },
-      category: {
-        type: 'string',
-        description: 'The root url of the current set of docs pages',
-      },
-      currentPage: {
-        type: 'string',
-        description: 'Relative URL of the current page',
-      },
-      data: {
-        type: 'object',
-        description: 'Same as the `data` object passed to `react-docs-sidenav`',
-      },
-      order: {
-        type: 'array',
-        description:
-          'Same as the `order` object passed to `react-docs-sidenav`',
-        required: true,
-      },
-    },
+    description:
+      'Object containing additional components to be made available within mdx pages. Uses the format { [key]: Component }, for example, `{ TestComponent: () => <p>hello world</p> }`',
+  },
+  showEditPage: {
+    type: 'boolean',
+    description:
+      'if true, an "edit this page" link will appear on the bottom right',
+    default: true,
+  },
+  mainBranch: {
+    type: 'string',
+    description:
+      'The default branch of the project being documented, typically either "master" or "main". Used for the `showEditPage` prop',
+    default: 'main',
+  },
+  staticProps: {
+    type: 'object',
+    description:
+      'Directly pass the return value of `server/generateStaticProps` in here.',
   },
 }
