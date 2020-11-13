adaac2c35dee9a63745a98dc9e56a1b05e8e6687
Zack Tanner <zacktanner@gmail.com>
2020-11-13 13:10:55 -0800

    Add CommandLineTerminal component

---
+++ packages/terminal/props.js
@@ -0,0 +1,36 @@
+module.exports = {
+  theme: {
+    type: 'string',
+    description: 'Color theme to match a specific product',
+    control: { type: 'select' },
+    options: ['consul', 'terraform', 'nomad', 'vault', 'red', 'blue'],
+    testValue: 'blue',
+  },
+  lines: {
+    type: 'array',
+    testValue: [
+      {
+        code: '» Building . . . . . . . . . . . . .',
+      },
+      {
+        color: 'gray',
+        code: '  Creating new buildpack-based image using builder:',
+      },
+    ],
+    description:
+      'The lines of code to be rendered. Indentation can be represented by using whitespace.',
+    properties: [
+      {
+        type: 'object',
+        properties: {
+          code: { type: 'string', description: 'Code to be rendered' },
+          color: {
+            type: 'string',
+            description: 'Color of rendered code',
+            options: ['navy', 'gray', 'white'],
+          },
+        },
+      },
+    ],
+  },
+}
