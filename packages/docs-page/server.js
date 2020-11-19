ba865192c25db3b63d5d5817d674f3edbfc0f7e6
Jeff Escalante <jescalan@users.noreply.github.com>
2020-11-18 22:21:16 -0500

    small refactor on fast read frontmatter

--- packages/docs-page/server.js
+++ packages/docs-page/server.js
@@ -96,47 +96,42 @@ async function renderPageMdx(root, pagePath, components) {
   }
 }
 
-function fastReadFrontMatter(p) {
-  return new Promise((resolve) => {
-    const fm = []
-    readdirp(p, { fileFilter: '*.mdx' })
-      .on('data', (entry) => {
-        let lineNum = 0
-        const content = []
-        fm.push(
-          new Promise((resolve2, reject2) => {
-            lineReader.eachLine(
-              entry.fullPath,
-              (line) => {
-                // if it has any content other than `---`, the file doesn't have front matter, so we close
-                if (lineNum === 0 && !line.match(/^---$/)) {
-                  console.warn(
-                    `WARNING: The file "${entry.path}" is missing front matter. Please add front matter to ensure the file's metadata is properly populated.`
-                  )
-                  content.push('---')
-                  content.push('page_title: "ERROR: Missing Frontmatter"')
-                  return false
-                }
-                // if it's not the first line and we have a bottom delimiter, exit
-                if (lineNum !== 0 && line.match(/^---$/)) return false
-                // now we read lines until we match the bottom delimiters
-                content.push(line)
-                // increment line number
-                lineNum++
-              },
-              (err) => {
-                if (err) return reject2(err)
-                content.push(`__resourcePath: "${entry.path}"`)
-                resolve2(safeLoad(content.slice(1).join('\n')), {
-                  filename: entry.fullPath,
-                })
-              }
-            )
-          })
+async function fastReadFrontMatter(p) {
+  const fm = []
+  for await (const entry of readdirp(p, { fileFilter: '*.mdx' })) {
+    let lineNum = 0
+    const content = []
+    fm.push(
+      new Promise((resolve, reject) => {
+        lineReader.eachLine(
+          entry.fullPath,
+          (line) => {
+            // if it has any content other than `---`, the file doesn't have front matter, so we close
+            if (lineNum === 0 && !line.match(/^---$/)) {
+              console.warn(
+                `WARNING: The file "${entry.path}" is missing front matter. Please add front matter to ensure the file's metadata is properly populated.`
+              )
+              content.push('---')
+              content.push('page_title: "ERROR: Missing Frontmatter"')
+              return false
+            }
+            // if it's not the first line and we have a bottom delimiter, exit
+            if (lineNum !== 0 && line.match(/^---$/)) return false
+            // now we read lines until we match the bottom delimiters
+            content.push(line)
+            // increment line number
+            lineNum++
+          },
+          (err) => {
+            if (err) return reject(err)
+            content.push(`__resourcePath: "${entry.path}"`)
+            resolve(safeLoad(content.slice(1).join('\n')), {
+              filename: entry.fullPath,
+            })
+          }
         )
       })
-      .on('end', () => {
-        Promise.all(fm).then((res) => resolve(res))
-      })
-  })
+    )
+  }
+  return Promise.all(fm)
 }
