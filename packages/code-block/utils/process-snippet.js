3a775972313425fac6eb2cf6a77e9a1f915ddf5a
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2020-12-02 11:48:56 -0500

    Remove comment now captured as task

--- packages/code-block/utils/process-snippet.js
+++ packages/code-block/utils/process-snippet.js
@@ -1,22 +1,6 @@
 import shellwords from 'shellwords'
 
 /**
- * @TODO
- *
- * Assess whether we might be able to modify our
- * workflow around processing snippets, based on
- * the fact we have a `shell-session` highlight language,
- * and that all `shell-symbols` should therefore be already
- * wrapped in a `<span class="shell-symbol">` element.
- *
- * Maybe, rather than re-processing the code with `shellwords`,
- * we could remove all `<span class="shell-symbol">` elements?
- * This might be more robust, as it rather than just replacing `$`
- * characters, it might let us leverage parsing of the snippet
- * that our highlighting library has already done for us.
- */
-
-/**
  * Given a snippet of code,
  * process it based on the detected language,
  * returning a modified snippet that has is
