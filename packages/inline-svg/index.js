fdf93e029eae86bb3a5836795c1a3b8920d19e68
Jeff Escalante <jescalan@users.noreply.github.com>
2020-12-07 20:59:16 -0500

    roll back inline svg to its previous state

--- packages/inline-svg/index.js
+++ packages/inline-svg/index.js
@@ -1,23 +1,3 @@
 export default function InlineSvg({ src, ...props }) {
-  // match the attributes and content from the serialized svg
-  const [, attrs, __html] = src?.match(svgMatch) || []
-
-  // combine the attributes and props into new spreadable props
-  const svgProps = attrs
-    ? {
-        ...[...attrs.matchAll(attrMatch)].reduce(
-          (svgProps, [, name, value]) => ({
-            ...svgProps,
-            [name]: value,
-          }),
-          {}
-        ),
-        ...props,
-      }
-    : props
-
-  return <svg {...svgProps} dangerouslySetInnerHTML={{ __html }} />
+  return <div dangerouslySetInnerHTML={{ __html: src }} {...props}></div>
 }
-
-const svgMatch = /^[\W\w]*<svg([^>]+)>([\W\w]*)(<\/svg>[\W\w]*?)?$/
-const attrMatch = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|\s*\/?[>"']))+.)["']?/g
