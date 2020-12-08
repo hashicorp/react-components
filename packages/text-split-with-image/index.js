09046f6e6a8ffab3357a373fb3ea0b8cffb8b104
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2020-12-08 09:40:16 -0500

    Move text-split-with-image to CSS modules

--- packages/text-split-with-image/index.js
+++ packages/text-split-with-image/index.js
@@ -1,12 +1,13 @@
 import TextSplit from '@hashicorp/react-text-split'
 import Image from '@hashicorp/react-image'
+import styles from './styles/text-split-with-image.module.css'
 
 export default function TextSplitWithImage({ image, textSplit }) {
   const altWithFallback = image.alt || textSplit.heading || ''
   return (
     <TextSplit {...textSplit}>
       <div className="g-text-split-with-image">
-        <Image {...image} alt={altWithFallback} />
+        <Image {...image} alt={altWithFallback} className={styles.imgElem} />
       </div>
     </TextSplit>
   )
