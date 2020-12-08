85bd01b782348a3a4a0f95c7a7fb2af7f02782cc
Jeff Escalante <jescalan@users.noreply.github.com>
2020-12-08 10:56:12 -0500

    Add search bar to DocsPage (#94)
    * add search bar to docspage
    * add documentation
    * Update packages/docs-page/index.js
    
    Co-authored-by: will kent-daggett <willkentdaggett@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,11 +7,5 @@ import UsageDetails from '../swingset-extensions/usage-details'
 
 const components = { Head, Link, SearchProvider, UsageDetails }
 
-// for search component
-process.env.NEXT_PUBLIC_ALGOLIA_APP_ID = 'YY0FFNI7MF'
-process.env.NEXT_PUBLIC_ALGOLIA_INDEX = 'product_NOMAD'
-process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY =
-  '5037da4824714676226913c65e961ca0'
-
 export default createPage({ components })
 export const getStaticProps = createStaticProps({ components })
