3e7842219e42a079a320bb6e4040c47b201a3f78
Kendall Strautman <kendallstrautman@gmail.com>
2021-01-27 15:12:58 -0800

    adds TabProvider to _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
 import './global.css'
+import { TabProvider } from '../packages/tabs'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TabProvider>
+      <Component {...pageProps} />
+    </TabProvider>
+  )
 }
