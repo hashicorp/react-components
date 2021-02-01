ad04c147474b8637d07f3322cd6518c05421a6d7
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-01 17:41:27 -0500

    test: update tests to reflect md >> HTML API change

--- packages/section-header/index.test.js
+++ packages/section-header/index.test.js
@@ -1,20 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import SectionHeader from './'
 
-test('renders headline and description, including markdown', () => {
+test('renders headline and description, including HTML for description', () => {
   render(
     <SectionHeader
-      headline="Headline **with markdown**"
-      description="Description **with markdown**"
+      headline="Headline with plain text"
+      description="Description <strong>with html</strong>"
     />
   )
-  expect(screen.getByTestId('h2')).toHaveTextContent('Headline with markdown')
+  expect(screen.getByTestId('h2')).toHaveTextContent('Headline with plain text')
   expect(screen.getByTestId('description')).toHaveTextContent(
-    'Description with markdown'
+    'Description with html'
   )
 })
 
 test('renders an h1 when `useH1` prop is true', () => {
-  render(<SectionHeader headline="Headline **with markdown**" useH1={true} />)
-  expect(screen.getByTestId('h1')).toHaveTextContent('Headline with markdown')
+  render(<SectionHeader headline="Headline with plain text" useH1={true} />)
+  expect(screen.getByTestId('h1')).toHaveTextContent('Headline with plain text')
 })
