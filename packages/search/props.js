56f8095c249c6eadff69b0ef0db288d9b0fd168c
will kent-daggett <willkentdaggett@gmail.com>
2021-02-06 00:13:40 -0800

    update props, fix prop name

--- packages/search/props.js
+++ packages/search/props.js
@@ -1,8 +1,17 @@
 module.exports = {
+  onSubmit: {
+    type: 'function',
+    description: 'Handle search form submission',
+    default: '(event) => { event.preventDefault() }',
+  },
   placeholder: {
     type: 'string',
     description: 'Placeholder content within the search box',
     default: 'Search',
+  },
+  renderCalloutCta: {
+    type: 'function',
+    description: 'Render function for displaying a small CTA beneath the search results',
   },
   renderHitContent: {
     type: 'function',
@@ -15,4 +24,9 @@ module.exports = {
     description:
       'Optional function to transform the results of any given hit. Primarily used to remove `/index` from resuling urls',
   },
+  showSearchLegend: {
+    type: 'boolean',
+    description: 'Enable the search keyboard legend',
+    default: false,
+  }
 }
