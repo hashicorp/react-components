39055dd77b8625ac5fc70eb947e28e5ee53d54b1
Kendall Strautman <kendallstrautman@gmail.com>
2021-02-09 11:16:29 -0800

    refactor(react-content): use product meta hook to reset theme

--- packages/content/index.js
+++ packages/content/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import useProductMeta from '@hashicorp/nextjs-scripts/lib/providers/product-meta'
 
 export default function Content({ content, product }) {
+  const { themeClass } = useProductMeta(product)
+
   return (
-    <article className={`g-content g-type-long-body ${product ? product : ''}`}>
+    <article className={`g-content g-type-long-body ${themeClass ?? ''}`}>
       {content}
     </article>
   )
