e9ffac99e3584865c0b026b8617c3c59bcd51ab6
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-03-05 15:32:48 -0500

    Add reference file for docs-page

---
+++ packages/docs-page/_temp-reference-index-test.js
@@ -0,0 +1,16 @@
+test.todo(
+  'passes `title`, `description`, and `siteName` correctly to <HashiHead>'
+)
+test.todo(
+  'passes `product`, `category`, `currentPage`, `data`, and `order` correctly to <DocsSidenav>'
+)
+test.todo('passes `product` and `content` correctly to <Content>')
+test.todo('displays `showEditPage` as true by default')
+test.todo('renders the `mainBranch` correctly within the edit page link')
+test.todo('if `showEditPage` is set to false, does not display')
+test.todo(
+  'passes `additionalComponents` to mdx remote for rendering if present'
+)
+test.todo(
+  'initializes jump to section UI if there are more than one `h2`s in the content'
+)
