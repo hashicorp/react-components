e9e8a665e080144c0b146b16089eaab4da10b71f
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-03-05 16:22:22 -0500

    A11y improvements, and split out utilities for clarity
    - change mobile-menu-related divs to buttons so they're focusable
    - properly hide mobile-menu so links are NOT focusable when hidden

--- packages/docs-sidenav/index.js
+++ packages/docs-sidenav/index.js
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import useProductMeta from '@hashicorp/nextjs-scripts/lib/providers/product-meta'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+// @hashicorp imports
+import useProductMeta from '@hashicorp/nextjs-scripts/lib/providers/product-meta'
 import LinkWrap, { isAbsoluteURL } from '@hashicorp/react-link-wrap'
 import InlineSvg from '@hashicorp/react-inline-svg'
+// local utilities
+import flagActiveNodes from './utils/flag-active-nodes'
+import filterContent from './utils/filter-content'
+import useEventListener from './utils/use-event-listener'
+// svg
 import svgMenuIcon from './icons/menu.svg?include'
 import svgChevron from './icons/chevron.svg?include'
 import svgBullet from './icons/bullet.svg?include'
 import svgExternalLink from './icons/external-link.svg?include'
-import fuzzysearch from 'fuzzysearch'
+// styles
 import s from './style.module.css'
 
 export default function DocsSidenav({
@@ -20,20 +26,47 @@ export default function DocsSidenav({
 }) {
   const router = useRouter()
   const pathname = router ? router.pathname : null
-  const [isMobileOpen, setIsMobileOpen] = useState(false)
+
+  // Get theme class
+  // ( note: we could consider getting the product prop here,
+  // rather than requiring it to be passed in )
+  const { themeClass } = useProductMeta(product)
+
+  // Set up filtering state
   const [filterInput, setFilterInput] = useState('')
   const [content, setContent] = useState(navData)
   const [filteredContent, setFilteredContent] = useState(navData)
-  const { themeClass } = useProductMeta(product)
 
-  // When currentPath changes, update content to ensure
+  // isMobileOpen controls menu open / close state
+  const [isMobileOpen, setIsMobileOpen] = useState(false)
+  // isMobileFullyHidden reflects if the menu is fully transitioned to a hidden state
+  const [isMenuFullyHidden, setIsMenuFullyHidden] = useState(true)
+  // We want to avoid exposing links to keyboard navigation
+  // when the menu is hidden on mobile. But we don't want our
+  // menu to flash when hide and shown. To meet both needs,
+  // we listen for transition end on the menu element, and when
+  // a transition ends and the menu is not open, we set isMenuFullyHidden
+  // which translates into a visibility: hidden CSS property
+  const menuRef = useRef(null)
+  const handleMenuTransitionEnd = useCallback(() => {
+    setIsMenuFullyHidden(!isMobileOpen)
+  }, [isMobileOpen, setIsMenuFullyHidden])
+  useEventListener('transitionend', handleMenuTransitionEnd, menuRef.current)
+
+  // When client-side navigation occurs,
+  // we want to close the mobile rather than keep it open
+  useEffect(() => {
+    setIsMobileOpen(false)
+  }, [pathname])
+
+  // When path-related data changes, update content to ensure
   // `__isActive` props on each content item are up-to-date
   // Note: we could also reset filter input here, if we don't
   // want to filter input to persist across client-side nav, ie:
   // setFilterInput("")
   useEffect(() => {
     if (!navData) return
-    setContent(addIsActiveToNodes(navData, currentPath, pathname))
+    setContent(flagActiveNodes(navData, currentPath, pathname))
   }, [currentPath, navData, pathname])
 
   // When filter input changes, update content
@@ -52,13 +85,18 @@ export default function DocsSidenav({
           <InlineSvg src={svgMenuIcon} /> Documentation Menu
         </span>
       </button>
-      <ul className={s.rootList} data-is-mobile-open={isMobileOpen}>
-        <div
+      <ul
+        className={s.rootList}
+        ref={menuRef}
+        data-is-mobile-hidden={!isMobileOpen && isMenuFullyHidden}
+        data-is-mobile-open={isMobileOpen}
+      >
+        <button
           className={s.mobileClose}
           onClick={() => setIsMobileOpen(!isMobileOpen)}
         >
           &times;
-        </div>
+        </button>
         {!disableFilter && (
           <input
             className={s.filterInput}
@@ -76,61 +114,6 @@ export default function DocsSidenav({
       </ul>
     </div>
   )
-}
-
-function addIsActiveToNodes(navNodes, currentPath, pathname) {
-  return navNodes
-    .slice()
-    .map((node) => addIsActiveToNode(node, currentPath, pathname))
-}
-
-function addIsActiveToNode(navNode, currentPath, pathname) {
-  // If it's a node with child routes, return true
-  // if any of the child routes are active
-  if (navNode.routes) {
-    const routesWithActive = addIsActiveToNodes(
-      navNode.routes,
-      currentPath,
-      pathname
-    )
-    const isActive = routesWithActive.filter((r) => r.__isActive).length > 0
-    return { ...navNode, routes: routesWithActive, __isActive: isActive }
-  }
-  // If it's a node with a path value,
-  // return true if the path is a match
-  if (navNode.path) {
-    const isActive = navNode.path === currentPath
-    return { ...navNode, __isActive: isActive }
-  }
-  // If it's a direct link,
-  // return true if the path matches the router.pathname
-  if (navNode.href) {
-    const isActive = navNode.href === pathname
-    return { ...navNode, __isActive: isActive }
-  }
-  // Otherwise, it's a divider, so return unmodified
-  return navNode
-}
-
-function filterContent(content, searchValue) {
-  // if there's no search searchValue we short-circuit and return everything
-  if (!searchValue) return content
-  return content.reduce((acc, item) => {
-    // if this is a divider node, don't show it in filtered results
-    if (item.divider) return acc
-    // all other nodes have a title, use it to check if the item is a direct match
-    const isTitleMatch = fuzzysearch(searchValue, item.title.toLowerCase())
-    //  For nodes with no children, return early, only add the item if the title matches
-    if (!item.routes) return isTitleMatch ? acc.concat(item) : acc
-    // for branch nodes with matching children, return a clone of the
-    // node with filtered content children
-    const filteredRoutes = filterContent(item.routes, searchValue)
-    const filteredItem = isTitleMatch
-      ? { ...item, __isFiltered: true }
-      : { ...item, routes: filteredRoutes, __isFiltered: true }
-    const isCategoryMatch = isTitleMatch || filteredRoutes.length > 0
-    return isCategoryMatch ? acc.concat(filteredItem) : acc
-  }, [])
 }
 
 function NavTree({ baseRoute, content }) {
@@ -177,24 +160,6 @@ function NavTree({ baseRoute, content }) {
   })
 }
 
-function NavLeaf({ title, url, isActive }) {
-  // if the item has a path, it's a leaf node so we render a link to the page
-  return (
-    <li>
-      <Link href={url}>
-        <a className={s.navItem} data-is-active={isActive}>
-          <InlineSvg
-            src={svgBullet}
-            className={s.navLeafIcon}
-            data-is-active={isActive}
-          />
-          <span dangerouslySetInnerHTML={{ __html: title }} />
-        </a>
-      </Link>
-    </li>
-  )
-}
-
 function NavBranch({ title, routes, baseRoute, isActive, isFiltered }) {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -226,8 +191,22 @@ function NavBranch({ title, routes, baseRoute, isActive, isFiltered }) {
   )
 }
 
-function Divider() {
-  return <hr className={s.divider} />
+function NavLeaf({ title, url, isActive }) {
+  // if the item has a path, it's a leaf node so we render a link to the page
+  return (
+    <li>
+      <Link href={url}>
+        <a className={s.navItem} data-is-active={isActive}>
+          <InlineSvg
+            src={svgBullet}
+            className={s.navLeafIcon}
+            data-is-active={isActive}
+          />
+          <span dangerouslySetInnerHTML={{ __html: title }} />
+        </a>
+      </Link>
+    </li>
+  )
 }
 
 function DirectLink({ title, href, isActive }) {
@@ -251,4 +230,8 @@ function DirectLink({ title, href, isActive }) {
       </LinkWrap>
     </li>
   )
+}
+
+function Divider() {
+  return <hr className={s.divider} />
 }
