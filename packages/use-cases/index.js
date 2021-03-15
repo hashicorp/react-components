684684ba5e6c3dadc396cad2336a35a5ca8c53b9
Kendall Strautman <kendallstrautman@gmail.com>
2021-03-15 08:56:37 -0700

    feat: adds use-cases package

---
+++ packages/use-cases/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import marked from 'marked'
+import Image from '@hashicorp/react-image'
+import Arrow from './arrow'
+import fragment from './fragment.graphql'
+
+function UseCases({ items }) {
+  return (
+    <div className="g-use-cases" data-testid="root">
+      {items.map((item) => (
+        <a
+          className="use-case"
+          href={item.link.url}
+          {...(item.link.external && {
+            rel: 'noopener',
+            target: '_blank',
+          })}
+          key={item.title}
+          data-testid={`anchor-${item.title}`}
+        >
+          <div className="content">
+            {item.image && (
+              <div className="icon">
+                <Image
+                  alt=""
+                  data-testid={`image-${item.title}`}
+                  {...item.image}
+                />
+              </div>
+            )}
+            <div className="text">
+              <h3
+                className="title g-type-display-4"
+                data-testid={`title-${item.title}`}
+              >
+                {item.title}
+              </h3>
+              <div
+                className="description g-type-body"
+                dangerouslySetInnerHTML={{
+                  __html: item.description ? marked(item.description) : '',
+                }}
+                data-testid={`description-${item.title}`}
+              />
+            </div>
+          </div>
+          <div
+            className="faux-link g-type-buttons-and-standalone-links"
+            data-testid={`faux-link-${item.title}`}
+          >
+            {item.link.title}
+            <Arrow />
+          </div>
+        </a>
+      ))}
+    </div>
+  )
+}
+
+UseCases.fragmentSpec = { fragment, dependencies: [Image] }
+
+export default UseCases
---
+++ packages/use-cases/index.test.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import UseCases from './index'
+
+const defaultProps = {
+  items: [
+    {
+      title: 'Easily onboard and manage users',
+      description: 'Use SSO to manage onboarding and off-boarding users.',
+      image: {
+        url:
+          'https://www.datocms-assets.com/2885/1565300480-workload-orchestration.png',
+      },
+      link: {
+        title: 'Learn more',
+        url:
+          'https://learn.hashicorp.com/tutorials/boundary/getting-started-config',
+      },
+    },
+    {
+      title: 'Open and extensible remote access',
+      description:
+        'Integrate with existing tooling and APIs to simplify access.',
+      image: {
+        url:
+          'https://www.datocms-assets.com/2885/1565300480-workload-orchestration.png',
+      },
+      link: {
+        title: 'Learn more',
+        url: '/docs/common-workflows/manage-users-groups',
+      },
+    },
+    {
+      title: 'Compliance without overhead',
+      description:
+        'Provide session visibility that enables teams to stay compliant.',
+      image: {
+        url:
+          'https://www.datocms-assets.com/2885/1565300480-workload-orchestration.png',
+      },
+      link: {
+        title: 'Learn more',
+        url: '/docs/common-workflows/manage-sessions',
+      },
+    },
+  ],
+}
+
+describe('<UseCases />', () => {
+  it('should render', () => {
+    render(<UseCases {...defaultProps} />)
+    expect(screen.getByTestId('root').className).toContain('g-use-cases')
+  })
+
+  it('should render all props correctly', () => {
+    render(<UseCases {...defaultProps} />)
+
+    defaultProps.items.map((useCase) => {
+      const key = useCase.title
+
+      // anchor
+      if (useCase.link) {
+        expect(screen.queryByTestId(`anchor-${key}`)).toHaveAttribute(
+          'href',
+          useCase.link.url
+        )
+      } else {
+        expect(screen.queryByTestId(`anchor-${key}`)).not.toBeInTheDocument()
+      }
+
+      // image
+      const image = screen.queryByTestId(`image-${key}`)
+      if (useCase.image) {
+        expect(image.src).toMatch(useCase.image.url)
+      } else {
+        expect(image).not.toBeInTheDocument()
+      }
+
+      // title
+      if (useCase.title) {
+        expect(screen.getByText(useCase.title)).toBeInTheDocument()
+      } else {
+        expect(
+          screen.queryByTestId(`title-${useCase.title}`)
+        ).not.toBeInTheDocument()
+      }
+
+      // description
+      if (useCase.description) {
+        expect(screen.getByText(useCase.description)).toBeInTheDocument()
+      } else {
+        expect(
+          screen.queryByTestId(`description-${key}`)
+        ).not.toBeInTheDocument()
+      }
+
+      // faux-link
+      if (useCase.link) {
+        expect(screen.queryByTestId(`faux-link-${key}`)).toHaveTextContent(
+          useCase.link.title
+        )
+      } else {
+        expect(screen.queryByTestId(`faux-link-${key}`)).not.toBeInTheDocument()
+      }
+    })
+  })
+})
