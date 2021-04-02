864a4087b857f1c75bf6a9b3cb5bb8ee53744448
Jeff Escalante <jescalan@users.noreply.github.com>
2021-04-02 17:56:25 -0400

    contain page area within aria landmark

--- packages/product-download-page/partials/release-information/index.tsx
+++ packages/product-download-page/partials/release-information/index.tsx
@@ -25,7 +25,7 @@ export default function ReleaseInformation({
 
   return (
     <div className={styles.root}>
-      <div className="g-container">
+      <main className="g-container">
         <h2>Release Information</h2>
         <div className={styles.grid}>
           {releases.length > 0 && (
@@ -126,7 +126,7 @@ export default function ReleaseInformation({
             </>
           )}
         </div>
-      </div>
+      </main>
     </div>
   )
 }
