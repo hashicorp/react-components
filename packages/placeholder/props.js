fd063b09e1ab621f786b0824558c153cfec494df
Bryce Kalow <bkalow@hashicorp.com>
2021-04-13 15:43:05 -0500

    improve docs and update props

--- packages/placeholder/props.js
+++ packages/placeholder/props.js
@@ -5,8 +5,4 @@ module.exports = {
       'Render prop used to render the placeholder layout. The function accepts a single argument, which is the renderable placeholder component.',
     required: true,
   },
-  className: {
-    type: 'string',
-    description: 'class to be passed to the wrapping element.',
-  },
 }
