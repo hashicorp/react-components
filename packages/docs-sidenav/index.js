a05e0716979706ee831a9b611d3a2dff0775f234
Bryce Kalow <bkalow@hashicorp.com>
2021-04-14 11:33:54 -0500

    remove type as text is default

--- packages/docs-sidenav/index.js
+++ packages/docs-sidenav/index.js
@@ -103,7 +103,6 @@ export default function DocsSidenav({
             placeholder="Filter..."
             onChange={(e) => setFilterInput(e.target.value)}
             value={filterInput}
-            type="text"
           />
         )}
         <NavTree
