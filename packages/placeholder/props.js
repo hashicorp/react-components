8ece104d4aab3b12f1dfd125b683983a7fdfc9c9
Bryce Kalow <bkalow@hashicorp.com>
2021-04-19 10:24:21 -0500

    feat: <Placeholder /> component (#211)

---
+++ packages/placeholder/props.js
@@ -0,0 +1,8 @@
+module.exports = {
+  children: {
+    type: 'function',
+    description:
+      'Render prop used to render the placeholder layout. The function accepts a single argument, which is the renderable placeholder component.',
+    required: true,
+  },
+}
