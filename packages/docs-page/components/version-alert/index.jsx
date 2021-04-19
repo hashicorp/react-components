b7ac773b7813cfa8562081714acc11bc504a7729
Bryce Kalow <bkalow@hashicorp.com>
2021-04-19 15:57:39 -0500

    use removeVersionFromPath in the alert

--- packages/docs-page/components/version-alert/index.jsx
+++ packages/docs-page/components/version-alert/index.jsx
@@ -1,6 +1,9 @@
 import Alert from '@hashicorp/react-alert'
 import { useRouter } from 'next/router'
-import { getVersionFromPath } from '@hashicorp/versioned-docs/client'
+import {
+  getVersionFromPath,
+  removeVersionFromPath,
+} from '@hashicorp/versioned-docs/client'
 import s from './style.module.css'
 
 export default function VersionAlert({ product }) {
@@ -9,12 +12,10 @@ export default function VersionAlert({ product }) {
 
   if (!versionInPath) return null
 
-  const currentUrl = `/${router.asPath.split('/')[1]}`
-
   return (
     <div className={s.wrapper}>
       <Alert
-        url={currentUrl}
+        url={removeVersionFromPath(router.asPath)}
         tag="old version"
         text={`You're looking at documentation for ${product} ${versionInPath}. Click here to view the latest content.`}
         state="warning"
