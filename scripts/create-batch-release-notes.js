406dc381d3d246b930b37c5e4d9cd96677883447
Kendall Strautman <kendallstrautman@gmail.com>
2021-04-21 14:39:20 -0700

    refactor: takes sha as argument

--- scripts/create-batch-release-notes.js
+++ scripts/create-batch-release-notes.js
@@ -6,13 +6,13 @@ const { execFileSync } = require('child_process')
  * To create batch release notes! Thanks @BRKalow
  *
  * Create a new octokit access token and add it to .env -- https://github.com/octokit/core.js#rest-api-example
- * Swap out the Publish commit sha and the release body and go for it 🎉
+ * Pass in the publish commit SHA as an argument and update the release body notes to generate releases 🎉
  *
  * */
 
 ;(async function () {
   const octokit = new Octokit({ auth: process.env.OCTOKIT_ACCESS_TOKEN })
-  const SHA = '7c8682475ca9c35392f973332e20dc4b89ef05bf' // swap out the sha for your publish commit
+  const [, , SHA] = process.argv
   const RELEASE_BODY = `Add the release notes here`
   const tags = String(execFileSync('git', ['tag', '--points-at', SHA])).split(
     '\n'
