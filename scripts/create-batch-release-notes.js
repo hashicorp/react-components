0782e298decd16bd49d2eef7f751d2c871ef3aa4
Kendall Strautman <36613477+kendallstrautman@users.noreply.github.com>
2021-04-22 09:37:34 -0700

    Update scripts/create-batch-release-notes.js
    Co-authored-by: Bryce Kalow <bkalow@hashicorp.com>

--- scripts/create-batch-release-notes.js
+++ scripts/create-batch-release-notes.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const { Octokit } = require('@octokit/core')
 const { execFileSync } = require('child_process')
 
+// Update this value with your release notes
+const RELEASE_BODY = `Add the release notes here`
+
 /**
- * To create batch release notes! Thanks @BRKalow
+ * To create batch release notes! Thanks @BRKalow & @kendallstrautman
  *
  * Create a new octokit access token and add it to .env -- https://github.com/octokit/core.js#rest-api-example
  * Pass in the publish commit SHA as an argument and update the release body notes to generate releases 🎉
@@ -13,7 +16,6 @@ const { execFileSync } = require('child_process')
 ;(async function () {
   const octokit = new Octokit({ auth: process.env.OCTOKIT_ACCESS_TOKEN })
   const [, , SHA] = process.argv
-  const RELEASE_BODY = `Add the release notes here`
   const tags = String(execFileSync('git', ['tag', '--points-at', SHA])).split(
     '\n'
   )
