cc1d58735dee98941ff0f5ea2483fb11ca87359b
Bryce Kalow <bkalow@hashicorp.com>
2021-05-14 16:21:43 -0500

    fix(docs-page): mobile tweaks & versioned fixes (#233)

--- packages/docs-page/components/version-alert/index.jsx
+++ packages/docs-page/components/version-alert/index.jsx
@@ -5,9 +5,11 @@ import {
   removeVersionFromPath,
 } from '@hashicorp/versioned-docs/client'
 import s from './style.module.css'
+import useIsMobile from '../../use-is-mobile'
 
 export default function VersionAlert({ product }) {
   const router = useRouter()
+  const isMobile = useIsMobile()
   const versionInPath = getVersionFromPath(router.asPath)
 
   if (!versionInPath) return null
@@ -16,8 +18,12 @@ export default function VersionAlert({ product }) {
     <div className={s.wrapper}>
       <Alert
         url={removeVersionFromPath(router.asPath)}
-        tag="old version"
-        text={`You're looking at documentation for ${product} ${versionInPath}. Click here to view the latest content.`}
+        tag={`old version ${isMobile ? `(${versionInPath})` : ''}`}
+        text={
+          isMobile
+            ? `Click to view latest`
+            : `You're looking at documentation for ${product} ${versionInPath}. Click here to view the latest content.`
+        }
         state="warning"
         textColor="dark"
       />
