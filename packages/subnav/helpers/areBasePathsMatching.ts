52cffc1f54b622100aaa37f634a65396ef815bdb
Bryce Kalow <bkalow@hashicorp.com>
2021-06-04 16:50:09 -0500

    upgrade swingset and update integration (#242)
    * upgrade swingset and update integration
    
    * fix case where currentPath is undefined
    
    * update index page

--- packages/subnav/helpers/areBasePathsMatching.ts
+++ packages/subnav/helpers/areBasePathsMatching.ts
@@ -1,4 +1,5 @@
 export function areBasePathsMatching(pathA: string, pathB: string) {
+  if (!pathA || !pathB) return false
   // use .filter(Boolean) to remove any falsy values, like ""
   const [pathABase] = pathA.split('/').filter(Boolean)
   const [pathBBase] = pathB.split('/').filter(Boolean)
