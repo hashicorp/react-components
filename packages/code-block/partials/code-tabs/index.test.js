6a06c56f0de868ef0071d7dbebc71198cdf7e70e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-07 14:16:48 -0400

    Add test file for CodeTabs

---
+++ packages/code-block/partials/code-tabs/index.test.js
@@ -0,0 +1,12 @@
+// import {
+//   cleanup,
+//   fireEvent,
+//   render,
+//   screen,
+//   waitFor,
+// } from '@testing-library/react'
+// import CodeTabs from './'
+
+it('should have tests for CodeTabs', () => {
+  expect(false).toBe(true)
+})
