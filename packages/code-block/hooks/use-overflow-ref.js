1d8bf6b5219753d6955b4e146ce6527e1c93abb7
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-08 14:19:02 -0400

    Cleanup use-overflow-ref

--- packages/code-block/hooks/use-overflow-ref.js
+++ packages/code-block/hooks/use-overflow-ref.js
@@ -1,25 +1,16 @@
 import { useEffect, useRef, useState } from 'react'
 import useWindowSize from './use-window-size'
 
-/*
-
-Overflow is determined by whether the width of a overflowRef.current element’s content is
-greater than the width of its layout.
-
-*/
-
 function useOverflowRef() {
   const [hasOverflow, setHasOverflow] = useState(true)
   const { width: windowWidth } = useWindowSize()
   const overflowRef = useRef()
 
   useEffect(() => {
-    const cleanup = () => null
-    if (!overflowRef.current) return cleanup
+    if (!overflowRef.current) return
     const { scrollWidth, offsetWidth } = overflowRef.current
     const nowHasOverflow = scrollWidth > offsetWidth
     if (hasOverflow !== nowHasOverflow) setHasOverflow(nowHasOverflow)
-    return cleanup
   }, [windowWidth, overflowRef.current])
 
   return [hasOverflow, overflowRef]
