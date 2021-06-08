8c7f16487f7c29167d958e500d2135b84e30755a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-08 17:00:02 -0400

    Add JSDoc comment for hidden-copy-content

--- packages/code-block/partials/hidden-copy-content/index.js
+++ packages/code-block/partials/hidden-copy-content/index.js
@@ -1,8 +1,12 @@
 import { forwardRef } from 'react'
 
-/* This hidden element acts solely as a container that we can fetch textContent to copy to clipboard.
-        This allows to ignore the "how will we copy this cleanly" concern when splitting code into lines,
-        and adding line numbers and so on in other parts of this component.*/
+/**
+ * This hidden element acts solely as a container
+ * that we can fetch textContent to copy to clipboard.
+ * This allows to ignore the "how will we copy this cleanly"
+ * concern when splitting code into lines, and adding
+ * line numbers and so on in other parts of code-block.
+ */
 function HiddenCopyContent({ code }, copyRef) {
   return (
     <pre ref={copyRef} style={{ display: 'none' }}>
