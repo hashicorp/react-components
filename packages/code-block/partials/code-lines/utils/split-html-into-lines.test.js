bbb5762ae7ddd847c13693ce5c59c979ecb4028b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-08 16:12:16 -0400

    Remove cruft-y tests

--- packages/code-block/partials/code-lines/utils/split-html-into-lines.test.js
+++
@@ -1,7 +0,0 @@
-import splitHtmlIntoLines from './split-html-into-lines'
-
-it('splits a simple string into multiple span elements', () => {
-  const htmlInput = `<span>console.log("Hello world!");</span>\n<span>alert("Another line");</span>`
-  const jsxResult = splitHtmlIntoLines(htmlInput)
-  expect(jsxResult.length).toBe(2)
-})
