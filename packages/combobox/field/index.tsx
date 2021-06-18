e4d17c8d8bb25b78e9f5e7662444203b9045596c
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-06-18 12:32:14 -0500

    Add setTouched for consumers

--- packages/combobox/field/index.tsx
+++ packages/combobox/field/index.tsx
@@ -4,11 +4,13 @@ import Combobox, { ComboboxProps } from '../'
 type ComboboxFieldProps = { name: string } & Omit<ComboboxProps, 'onSelect'>
 
 export default function ComboboxField({ name, ...props }: ComboboxFieldProps) {
-  if (!name) return null
-  const [, , helpers] = useField(name)
+  const [, meta, helpers] = useField(name)
   return (
     <Combobox
       onInputChange={(e) => {
+        if (!meta.touched) {
+          helpers.setTouched(true, false) // Set touched if the input value changes at all, allow consumers to handle validation
+        }
         if (e.currentTarget?.value === '')
           return helpers.setValue(e.currentTarget.value, true)
       }}
