5662bbbc7797f701217e095282480bc916588feb
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-06-18 17:43:08 -0500

    Add handler for touched to onSelect for field

--- packages/combobox/field/index.tsx
+++ packages/combobox/field/index.tsx
@@ -5,16 +5,22 @@ type ComboboxFieldProps = { name: string } & Omit<ComboboxProps, 'onSelect'>
 
 export default function ComboboxField({ name, ...props }: ComboboxFieldProps) {
   const [, meta, helpers] = useField(name)
+
+  function handleTouched() {
+    if (meta.touched) return
+    return helpers.setTouched(true, false) // Set touched if the input value changes at all, allow consumers to handle validation
+  }
   return (
     <Combobox
       onInputChange={(e) => {
-        if (!meta.touched) {
-          helpers.setTouched(true, false) // Set touched if the input value changes at all, allow consumers to handle validation
-        }
+        handleTouched()
         if (e.currentTarget?.value === '')
           return helpers.setValue(e.currentTarget.value, true)
       }}
-      onSelect={(value) => helpers.setValue(value, true)}
+      onSelect={(value) => {
+        handleTouched()
+        helpers.setValue(value, true)
+      }}
       {...props}
     />
   )
