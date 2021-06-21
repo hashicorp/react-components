dfe5a521cdc700896ac4df2db198207e0e6c2973
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-06-21 14:45:40 -0500

    Fix the approach to arbitrary input for validation control

--- packages/combobox/field/index.tsx
+++ packages/combobox/field/index.tsx
@@ -1,27 +1,35 @@
 import { useField } from 'formik'
 import Combobox, { ComboboxProps } from '../'
 
-type ComboboxFieldProps = { name: string } & Omit<ComboboxProps, 'onSelect'>
+type ComboboxFieldProps = { name: string; setValueOnChange?: boolean } & Omit<
+  ComboboxProps,
+  'onSelect'
+>
 
-export default function ComboboxField({ name, ...props }: ComboboxFieldProps) {
-  const [, meta, helpers] = useField(name) // https://formik.org/docs/api/useField#reference
+export default function ComboboxField({
+  name,
+  setValueOnChange = false, // Allows consumers to override the default behavior and validate arbitrary user input (i.e. not just from onSelect)
+  ...props
+}: ComboboxFieldProps) {
+  const [field, meta, helpers] = useField(name) // https://formik.org/docs/api/useField#reference
 
   function handleTouched() {
     if (meta.touched) return
-    return helpers.setTouched(true, false)
+    return helpers.setTouched(true, !setValueOnChange) // Validation will occur after the input
   }
+
   return (
     <Combobox
       onInputChange={(e) => {
         handleTouched() // Set touched if the input value changes at all
-        if (e.currentTarget?.value === '')
+        if (e.currentTarget?.value === '' || !!setValueOnChange)
           return helpers.setValue(e.currentTarget.value, true)
       }}
       onSelect={(value) => {
         handleTouched() // Set touched if the selected value changes at all
         helpers.setValue(value, true)
       }}
-      invalidInputValue={meta.error && true}
+      invalidInputValue={meta.error ? true : false}
       {...props}
     />
   )
