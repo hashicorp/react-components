cb27213c4ab417f4944f1697c67adff0eebf52b4
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-23 16:47:15 -0400

    feat: use jest-raw-loader for .svg imports

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,18 @@ module.exports = {
   moduleNameMapper: {
     '\\.module.css$': 'identity-obj-proxy',
     '\\.graphql$': '<rootDir>/__test-helpers/mocks/graphql-fragment-mock.js',
-    '\\.svg(?:\\?.*)*$': '<rootDir>/__test-helpers/mocks/svg-mock.js',
+    // Remove ?include suffixes from .svg imports
+    '(.*)\\.svg(?:\\?.*)*$': '$1.svg',
+  },
+  transform: {
+    // Note: babel-jest transform is from Jest's defaults for `transform`
+    // ref: https://jestjs.io/docs/next/configuration
+    '\\.[jt]sx?$': 'babel-jest',
+    // Load all .svg imports as raw strings.
+    '\\.svg(?:\\?.*)*$': 'jest-raw-loader',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@hashicorp/react-|@hashicorp/sentinel-embedded|@hashicorp/nextjs-scripts))',
+    '/node_modules/(?!(@hashicorp/mktg-logos|@hashicorp/react-|@hashicorp/sentinel-embedded|@hashicorp/nextjs-scripts))',
     '\\.pnp\\.[^\\/]+$',
   ],
   verbose: true,
