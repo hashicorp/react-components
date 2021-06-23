c380305e415f71ea45ed4f6ca8556cc0301cd1a2
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-23 17:00:04 -0400

    fix: tweak loader setup for better granularity

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,18 @@ module.exports = {
   moduleNameMapper: {
     '\\.module.css$': 'identity-obj-proxy',
     '\\.graphql$': '<rootDir>/__test-helpers/mocks/graphql-fragment-mock.js',
-    // Remove ?include suffixes from .svg imports
-    '(.*)\\.svg(?:\\?.*)*$': '$1.svg',
+    // For .svg?include imports, remove the ?include suffix,
+    // so that it can be resolved and loaded with jest-raw-loader
+    '(.*)\\.svg\\?include$': '$1.svg',
+    // For all other svg imports, use a mock
+    '\\.svg$': '<rootDir>/__test-helpers/mocks/svg-mock.js',
   },
   transform: {
     // Note: babel-jest transform is from Jest's defaults for `transform`
     // ref: https://jestjs.io/docs/next/configuration
     '\\.[jt]sx?$': 'babel-jest',
     // Load all .svg imports as raw strings.
-    '\\.svg(?:\\?.*)*$': 'jest-raw-loader',
+    '\\.svg$': 'jest-raw-loader',
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(@hashicorp/mktg-logos|@hashicorp/react-|@hashicorp/sentinel-embedded|@hashicorp/nextjs-scripts))',
