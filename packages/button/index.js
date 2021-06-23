ccfb059a0e95498abde260184839e9739fd9617a
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-06-23 15:33:35 -0400

    feat(button): update anchor icon to corner arrow

--- packages/button/index.js
+++ packages/button/index.js
@@ -5,13 +5,13 @@ import useProductMeta from '@hashicorp/nextjs-scripts/lib/providers/product-meta
 import InlineSvg from '@hashicorp/react-inline-svg'
 import svgArrowRight from './icons/arrow-right.svg?include'
 import svgExternalLink from './icons/external-link.svg?include'
-import svgChevronDown from './icons/chevron-down.svg?include'
+import svgCornerRightDown from './icons/corner-right-down.svg?include'
 import svgDownload from './icons/download.svg?include'
 
 const linkTypeToIcon = {
   inbound: svgArrowRight,
   outbound: svgExternalLink,
-  anchor: svgChevronDown,
+  anchor: svgCornerRightDown,
   download: svgDownload,
 }
 
