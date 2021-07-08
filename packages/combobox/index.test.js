a06957391ddd93e05fde97ad1dec7c499cb82415
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-07-08 10:26:45 -0500

    Address PR feedback

--- packages/combobox/index.test.js
+++ packages/combobox/index.test.js
@@ -4,63 +4,62 @@ import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 afterEach(cleanup)
 
 it('should render an input element with a proper aria-label', () => {
-  const label = 'Fruit picker'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi'],
+  }
   const role = 'combobox'
-  render(
-    <Combobox
-      onSelect={(value) => value}
-      options={['Apple', 'Orange', 'Banana', 'Pineapple', 'Kiwi']}
-      label={label}
-    />
-  )
 
-  expect(screen.getByRole(role)).toHaveAttribute('aria-label', label)
+  render(<Combobox {...defaultProps} />)
+
+  expect(screen.getByRole(role)).toHaveAttribute(
+    'aria-label',
+    defaultProps.label
+  )
 })
 
 it('should render our input with error state when invalidInputValue is true', () => {
-  const label = 'Fruit picker'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi'],
+  }
   const role = 'combobox'
-  render(
-    <Combobox
-      onSelect={(value) => value}
-      options={['Apple', 'Orange', 'Banana', 'Pineapple', 'Kiwi']}
-      label={label}
-      invalidInputValue
-    />
-  )
+
+  render(<Combobox {...defaultProps} invalidInputValue />)
+
   expect(screen.getByRole(role)).toHaveAttribute('data-has-error', 'true')
 })
 
 it('should focus the input when dropdown button is clicked', () => {
-  const label = 'Fruit picker'
-  const role = 'combobox'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi'],
+  }
+
+  const inputRole = 'combobox'
   const buttonLabel = 'Pick a fruit'
 
-  render(
-    <Combobox
-      onSelect={(value) => value}
-      options={['Apple', 'Orange', 'Banana', 'Pineapple', 'Kiwi']}
-      label={label}
-      buttonLabel={buttonLabel}
-    />
-  )
+  render(<Combobox {...defaultProps} buttonLabel={buttonLabel} />)
+
   // Check for focus on input after click of dropdown button
-  fireEvent.click(screen.getByText(buttonLabel).parentElement) // label is rendered as visually hidden span for a11y within button
-  expect(screen.getByRole(role)).toHaveFocus
+  fireEvent.click(screen.getByText(buttonLabel)) // label is rendered as visually hidden span for a11y within button, the event will bubble to the button
+  expect(screen.getByRole(inputRole)).toHaveFocus
 })
 
 it('should filter to the proper options given enough characters', () => {
-  const label = 'Fruit picker'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi'],
+  }
+
   const inputRole = 'combobox'
   const optionListRole = 'listbox'
 
   render(
     <Combobox
-      onSelect={(value) => value}
       //  'Apple' should be the best match and is intentionally not placed as the first option.
       // Filter functionality should work such that it is rendered as the first option
-      options={['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi']}
-      label={label}
+      {...defaultProps}
     />
   )
   const input = screen.getByRole(inputRole)
@@ -79,101 +78,111 @@ it('should filter to the proper options given enough characters', () => {
 
   // Should show 1 option for this exact match
   expect(screen.getByRole(optionListRole).children.length).toBe(1)
+
+  // Type `appl` again
+  fireEvent.change(input, { target: { value: 'appl' } })
+  // Select the second option in the list this time
+  fireEvent.click(screen.getByRole(optionListRole).firstChild.nextSibling)
+  // Should match 'Pineapple', filter functionality should work such that it was rendered as the second option
+  expect(input).toHaveValue('Pineapple')
 })
 
 it('should render a custom option component', () => {
-  const label = 'Fruit picker'
-  const optionListRole = 'listbox'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi'],
+  }
+  const inputRole = 'combobox'
   const expectedTestId = 'foo-bar-Kiwi'
-  const options = ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi']
-  const buttonLabel = 'Pick a fruit'
 
   render(
     <Combobox
-      onSelect={(value) => value}
+      {...defaultProps}
       renderOption={(option) => (
         <div data-testid={`foo-bar-${option}`}>{option}</div>
       )}
-      options={options}
-      label={label}
-      buttonLabel={buttonLabel}
     />
   )
-  // Open the menu
-  fireEvent.click(screen.getByText(buttonLabel).parentElement) // label is rendered as visually hidden span for a11y within button
 
-  // Length of option element collection should match length of options array
-  expect(screen.getByRole(optionListRole).children.length).toBe(options.length)
+  const input = screen.getByRole(inputRole)
+
+  // Focus the input to open the menu
+  fireEvent.focus(input)
+
   // Custom component should render with test id
   expect(screen.getByTestId(expectedTestId)).toBeInTheDocument()
   // Custom component should render option text
   expect(screen.getByTestId(expectedTestId).innerHTML).toBe('Kiwi')
 })
 
-it('should allow custom options to be selected', () => {
-  const label = 'Fruit picker'
+it('should render the right number of options', () => {
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi'],
+  }
+  const inputRole = 'combobox'
   const optionListRole = 'listbox'
+
+  render(<Combobox {...defaultProps} />)
+
+  const input = screen.getByRole(inputRole)
+
+  // Focus the input to open the menu
+  fireEvent.focus(input)
+  // Length of option element collection should match length of options array
+  expect(screen.getByRole(optionListRole).children.length).toBe(
+    defaultProps.options.length
+  )
+})
+
+it('should allow custom options to be selected', () => {
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi'],
+  }
   const expectedTestId = 'foo-bar-Kiwi'
   const inputRole = 'combobox'
-  const options = ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi']
   const expectedInputValue = 'Kiwi'
 
   render(
     <Combobox
-      onSelect={(value) => value}
+      {...defaultProps}
       renderOption={(option) => (
         <div data-testid={`foo-bar-${option}`}>{option}</div>
       )}
-      options={options}
-      label={label}
     />
   )
+
   const input = screen.getByRole(inputRole)
 
-  // Type `kiw`
-  fireEvent.change(input, { target: { value: 'kiw' } })
+  // Focus the input to open the menu
+  fireEvent.focus(input)
 
-  expect(
-    screen.getByRole(optionListRole).firstChild.firstChild
-  ).toHaveAttribute('data-testid', expectedTestId)
-
-  // Select first option in the list
-  fireEvent.click(screen.getByRole(optionListRole).firstChild)
+  // Select an option by a valid testId
+  fireEvent.click(screen.getByTestId(expectedTestId))
 
   // Should match expectedInputValue, filter functionality should work such that it was rendered as the first option
   expect(input).toHaveValue(expectedInputValue)
 })
 
 test('when an `onSelect` prop is provided and the user selects an option, the value should be passed to the `onSelect` handler ', () => {
-  const label = 'Fruit picker'
-  const optionListRole = 'listbox'
-  const expectedTestId = 'foo-bar-Kiwi'
+  const defaultProps = {
+    label: 'Fruit picker',
+    options: ['Orange', 'Banana', 'Apple', 'Pineapple', 'Kiwi'],
+  }
   const inputRole = 'combobox'
-  const options = ['Orange', 'Banana', 'Pineapple', 'Apple', 'Kiwi']
   const expectedInputValue = 'Kiwi'
   const mockHandler = jest.fn((value) => value + ' is tasty')
 
-  render(
-    <Combobox
-      onSelect={mockHandler}
-      renderOption={(option) => (
-        <div data-testid={`foo-bar-${option}`}>{option}</div>
-      )}
-      options={options}
-      label={label}
-    />
-  )
+  render(<Combobox {...defaultProps} onSelect={mockHandler} />)
+
   const input = screen.getByRole(inputRole)
 
-  // Type `kiw`
-  fireEvent.change(input, { target: { value: 'kiw' } })
+  // Focus the input
+  fireEvent.focus(input)
 
-  expect(
-    screen.getByRole(optionListRole).firstChild.firstChild
-  ).toHaveAttribute('data-testid', expectedTestId)
-
-  // Select first option in the list
-  fireEvent.click(screen.getByRole(optionListRole).firstChild)
+  // Select an option by a valid testId
+  fireEvent.click(screen.getByText(expectedInputValue))
 
   // Should match expectedInputValue, filter functionality should work such that it was rendered as the first option
   expect(input).toHaveValue(expectedInputValue)
