2fd2588e692fbdc280ba1a661e1d08581f3996a4
Bryce Kalow <bkalow@hashicorp.com>
2021-07-15 08:43:01 -0500

    determine nested HC deps in the monorepo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+const {
+  getHashicorpPackages,
+} = require('@hashicorp/platform-nextjs-plugin/dist/get-hashicorp-packages')
+
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 const withSwingset = require('swingset')
 
+// Determine HC packages which are included in the monorepo to ensure we are transpiling all of our dependencies
+const workspaceRoot = path.join(process.cwd(), 'packages')
+let hashicorpPackagesInWorkspaces = []
+
+fs.readdirSync(workspaceRoot).forEach((dir) => {
+  hashicorpPackagesInWorkspaces = hashicorpPackagesInWorkspaces.concat(
+    getHashicorpPackages(path.join(workspaceRoot, dir))
+  )
+})
+
 module.exports = withHashicorp({
-  transpileModules: ['swingset', '@hashicorp/sentinel-embedded'],
+  transpileModules: [
+    'swingset',
+    '@hashicorp/sentinel-embedded',
+    ...hashicorpPackagesInWorkspaces,
+  ],
 })(
   withSwingset({
     componentsRoot: 'packages/*',
