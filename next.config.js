b41e94ef927a66c4705631271d6a4fa8f82205b6
Bryce Kalow <bkalow@hashicorp.com>
2021-08-10 12:05:02 -0500

    removes unneeded localstorage polyfill and workspace deps check

--- next.config.js
+++ next.config.js
@@ -1,30 +1,11 @@
 const path = require('path')
 const fs = require('fs')
-const {
-  getHashicorpPackages,
-} = require('@hashicorp/platform-nextjs-plugin/dist/get-hashicorp-packages')
 
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 const withSwingset = require('swingset')
 
-// Determine HC packages which are included in the monorepo to ensure we are transpiling all of our dependencies
-const workspaceRoot = path.join(process.cwd(), 'packages')
-let hashicorpPackagesInWorkspaces = []
-
-fs.readdirSync(workspaceRoot).forEach((dir) => {
-  hashicorpPackagesInWorkspaces = hashicorpPackagesInWorkspaces.concat(
-    getHashicorpPackages(path.join(workspaceRoot, dir))
-  )
-})
-
-console.log([...new Set(hashicorpPackagesInWorkspaces)])
-
 module.exports = withHashicorp({
-  transpileModules: [
-    'swingset',
-    '@hashicorp/sentinel-embedded',
-    ...[...new Set(hashicorpPackagesInWorkspaces)],
-  ],
+  transpileModules: ['swingset', '@hashicorp/sentinel-embedded'],
 })(
   withSwingset({
     componentsRoot: 'packages/*',
