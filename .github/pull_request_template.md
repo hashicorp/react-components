57720a9f36e6372d2ae38fc5701dc6c428fc971c
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-16 15:28:05 -0400

    feat(alert-banner)!: convert to CSS modules (#283)
    * feat(alert-banner): refactor to use CSS modules
    
    BREAKING CHANGE: consumers MUST remove style.css imports from this package
    
    * refactor(alert-banner): switch to function component with hooks
    
    * fix(alert-banner): refactor tests for CSS modules
    
    * refactor(alert-banner): convert to Typescript
    
    * chore(alert-banner): remove redundant type descriptions
    
    * chore(alert-banner): use ReachUI's VisuallyHidden
    
    * fix(alert-banner): add graphql type file to resolve TS error
    
    * chore: remove stray typo from PR template
    
    * fix(alert-banner): use functional updater in show-hide useEffect
    
    * fix(alert-banner): assert on isShown class to avoid unnecessary data-test attr

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -19,4 +19,3 @@ Items in this checklist may not may not apply to your PR, but please consider ea
 - [ ] Conduct reasonable cross browser testing for both compatibility and responsive behavior (We have a [Sauce Labs](https://app.saucelabs.com/) account for this, if you don't have access, just ask!).
 - [ ] Conduct reasonable accessibility review (use the [WAS](https://accessible.org/Web-Accessibility-Standards-WAS-2.pdf) as a guide or an [axe browser plugin](https://www.deque.com/axe/) until we establish more formal checks).
 - [ ] Identify (in the description above) and document (add Asana tasks on [this board](https://app.asana.com/0/1100423001970639/list)) any technical debt that you're aware of, but are not addressing as part of this PR.
-      1
---
+++ packages/alert-banner/index.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react'
+import cookie from 'js-cookie'
+import slugify from 'slugify'
+import classNames from 'classnames'
+import VisuallyHidden from '@reach/visually-hidden'
+import useProductMeta from '@hashicorp/platform-product-meta'
+import InlineSvg from '@hashicorp/react-inline-svg'
+import CloseIcon from './img/close-icon.svg?include'
+import fragment from './fragment.graphql'
+import s from './style.module.css'
+import analytics from './analytics'
+import { HashiCorpProduct } from '../../types'
+
+interface AlertBannerProps {
+  tag: string
+  text: string
+  url: string
+  expirationDate?: string
+  hideOnMobile?: boolean
+  linkText?: string
+  name?: string
+  product?: HashiCorpProduct
+}
+
+/**
+ * AlertBanner is a component used widely across HashiCorp websites to
+ * draw attention to an important release, event, etc. It appears in
+ * bold colors at the top of the website, and can be dismissed.
+ */
+function AlertBanner({
+  expirationDate,
+  hideOnMobile,
+  linkText,
+  name,
+  product = 'vagrant', // override useProductMeta's default "hashicorp", as it would result in a dull gray background
+  tag,
+  text,
+  url,
+}: AlertBannerProps): React.ReactElement {
+  const dismissalCookieId = `banner_${name || slugify(text, { lower: true })}`
+  const [isShown, setIsShown] = useState(true)
+  const { themeClass } = useProductMeta(product)
+
+  /**
+   * On mount, hide the banner if it is expired or
+   * if a cookie indicates it was previously closed
+   */
+  useEffect(() => {
+    const hasBeenDismissed = cookie.get(dismissalCookieId)
+    const hasExpired = expirationDate && Date.now() > Date.parse(expirationDate)
+    const shouldBeShown = !hasBeenDismissed && !hasExpired
+    setIsShown((current) =>
+      current !== shouldBeShown ? shouldBeShown : current
+    )
+  }, [expirationDate])
+
+  /**
+   * Dismiss the banner, and set a cookie
+   * to remember the dismissal of this banner
+   */
+  function closeBanner() {
+    cookie.set(dismissalCookieId, 1)
+    setIsShown(false)
+    analytics.trackClose({ linkText, product, tag, text })
+  }
+
+  return (
+    <div
+      className={classNames(
+        s.root,
+        themeClass,
+        { [s.isShown]: isShown },
+        { [s.hideOnMobile]: hideOnMobile }
+      )}
+    >
+      <a
+        href={url}
+        className={s.linkElem}
+        onClick={() => analytics.trackClick({ linkText, product, tag, text })}
+      >
+        <span className={s.textContainer}>
+          <span className={s.tag}>{tag} </span>
+          <span className={s.text}>
+            {text}
+            {linkText ? <span className={s.linkText}> {linkText}</span> : null}
+          </span>
+        </span>
+      </a>
+      <button className={s.closeButton} onClick={closeBanner}>
+        <InlineSvg src={CloseIcon} />
+        <VisuallyHidden>Dismiss alert</VisuallyHidden>
+      </button>
+    </div>
+  )
+}
+
+AlertBanner.fragmentSpec = { fragment }
+
+export default AlertBanner
