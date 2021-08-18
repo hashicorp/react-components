a891822fff4ff9c55bfdd7d9f5e156563916170b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-18 18:02:37 -0400

    check out work from button branch

--- packages/button/index.js
+++ packages/button/index.js
@@ -7,6 +7,9 @@ import svgArrowRight from './icons/arrow-right.svg?include'
 import svgExternalLink from './icons/external-link.svg?include'
 import svgCornerRightDown from './icons/corner-right-down.svg?include'
 import svgDownload from './icons/download.svg?include'
+import s from './style.module.css'
+import sTheme from './theme.module.css'
+import useHover from './hooks/use-hover'
 
 const linkTypeToIcon = {
   inbound: svgArrowRight,
@@ -32,35 +35,40 @@ function Button({
   size,
   ...attrs
 }) {
+  const [hoverRef, isHovered] = useHover()
   const themeObj = normalizeButtonTheme(theme)
   const { themeClass } = useProductMeta(themeObj.brand)
   const gaSlug = slugify(title, { lower: true })
   const isExternal = url && (linkType === 'outbound' || external)
   const Elem = url ? 'a' : 'button'
-  const parsedIcon = linkTypeToIcon[linkType]
+  const iconProps = linkTypeToIcon[linkType]
     ? {
         svg: linkTypeToIcon[linkType],
         position: icon ? icon.position : 'right',
         animationId: linkType,
         isAnimated: icon ? icon.isAnimated : true,
+        isHovered,
+        size,
       }
-    : icon
-  const hasIcon = parsedIcon && parsedIcon.svg
-  const hasRightIcon = hasIcon && parsedIcon.position !== 'left'
-  const hasLeftIcon = hasIcon && parsedIcon.position === 'left'
+    : { ...icon, size, isHovered }
+  const hasIcon = iconProps && iconProps.svg
+  const hasRightIcon = hasIcon && iconProps.position !== 'left'
+  const hasLeftIcon = hasIcon && iconProps.position === 'left'
+
   return (
     <Elem
       className={classNames(
-        'g-btn',
-        `size-${size}`,
-        `variant-${themeObj.variant}`,
+        s.root,
         themeClass,
-        { 'brand-neutral': themeObj.brand === 'neutral' },
-        `background-${themeObj.background}`,
+        s[`size-${size}`],
+        sTheme[`variant-${themeObj.variant}`],
+        { [sTheme['brand-neutral']]: themeObj.brand === 'neutral' },
+        sTheme[`background-${themeObj.background}`],
         className
       )}
       data-ga-button={`${ga_prefix ? ga_prefix + ' | ' : ''}${gaSlug}`}
       href={url}
+      ref={hoverRef}
       rel={isExternal ? 'noopener' : undefined}
       target={isExternal ? '_blank' : undefined}
       onClick={onClick}
@@ -68,25 +76,32 @@ function Button({
       aria-label={label}
       {...attrs}
     >
-      {hasLeftIcon && <ButtonIcon icon={parsedIcon} />}
-      <span className="text g-type-buttons-and-standalone-links">{title}</span>
-      {hasRightIcon && <ButtonIcon icon={parsedIcon} />}
+      {hasLeftIcon && <ButtonIcon {...iconProps} />}
+      <span className={s.text}>{title}</span>
+      {hasRightIcon && <ButtonIcon {...iconProps} />}
     </Elem>
   )
 }
 
-function ButtonIcon(props) {
-  const { svg, position, animationId, isAnimated } = props.icon
-  const SvgComponent = () => <InlineSvg src={svg} />
+function ButtonIcon({
+  svg,
+  position,
+  animationId,
+  isAnimated,
+  isHovered,
+  size,
+}) {
   return (
-    <span
-      className={classNames('icon', `at-${position}`, {
-        'is-animated': isAnimated,
-      })}
-      data-animation={animationId}
-    >
-      <SvgComponent />
-    </span>
+    <InlineSvg
+      className={classNames(
+        s.icon,
+        s[`size-${size}`],
+        s[`at-${position}`],
+        { [s.isHovered]: isHovered },
+        { [s[`animation-${animationId}`]]: isAnimated }
+      )}
+      src={svg}
+    />
   )
 }
 
