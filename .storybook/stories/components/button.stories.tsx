ca58bd2f4b019559d9f1e280bae3d0f676b89d2b
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-19 11:29:32 -0400

    refactor(button): convert to Typescript, minor tweaks

--- .storybook/stories/components/button.stories.tsx
+++ .storybook/stories/components/button.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-
 import Button from '../../../packages/button'
+import BgColor from '../../components/bg-color'
+import { Theme as ButtonTheme } from '../../../packages/button/types'
 
 export default {
   title: 'Components/Button',
@@ -20,3 +21,67 @@ const Template: ComponentStory<typeof Button> = (args) => {
 }
 
 export const Basic = Template.bind({})
+
+/* Product Colors */
+export const Themes: ComponentStory<typeof Button> = (args) => {
+  const backgrounds = ['light', 'dark']
+  const variants = [
+    'primary',
+    'secondary',
+    'tertiary',
+    'tertiary-neutral',
+    'ghost',
+  ]
+  const products = [
+    'hashicorp',
+    'packer',
+    'vagrant',
+    'terraform',
+    'consul',
+    'boundary',
+    'vault',
+    'nomad',
+    'waypoint',
+  ]
+  return (
+    <>
+      {backgrounds.map((background) => {
+        return (
+          <div
+            style={{
+              display: 'grid',
+              gap: '1rem',
+              gridTemplateColumns: 'repeat(5, max-content)',
+              position: 'relative',
+              padding: '1rem',
+            }}
+          >
+            <BgColor color={background === 'light' ? 'white' : 'black'} />
+            {products.map((product) => {
+              return (
+                <>
+                  {variants.map((variant) => {
+                    return (
+                      <Button
+                        {...args}
+                        theme={
+                          {
+                            brand: product,
+                            background,
+                            variant,
+                          } as ButtonTheme
+                        }
+                      />
+                    )
+                  })}
+                </>
+              )
+            })}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+Themes.args = { linkType: 'outbound' }
+Themes.argTypes = {}
