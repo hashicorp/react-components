a96796944723492184da629fe9741d30a22a49ae
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-19 16:17:04 -0400

    feat(storybook): improve call-to-action stories

--- .storybook/stories/components/call-to-action.stories.tsx
+++ .storybook/stories/components/call-to-action.stories.tsx
@@ -5,20 +5,36 @@ import CallToAction from '../../../packages/call-to-action'
 export default {
   title: 'Components/CallToAction',
   component: CallToAction,
+  args: {
+    heading: 'Ready to get started?',
+    content:
+      'Get critical workloads running in the cloud faster with fewer resources in order to meet today‘s challenges.',
+    links: [
+      {
+        text: 'Get Started',
+        url:
+          'https://portal.cloud.hashicorp.com/sign-up?utm_source=cloud_landing&utm_content=footer',
+      },
+      {
+        text: 'Explore HCP on HashiCorp Learn',
+        url:
+          'https://learn.hashicorp.com/cloud?utm_source=cloud_landing&utm_content=footer',
+        type: 'outbound',
+      },
+    ],
+    product: 'packer',
+  },
 } as ComponentMeta<typeof CallToAction>
 
 const Template: ComponentStory<typeof CallToAction> = (args) => {
   return <CallToAction {...args} />
 }
 
-export const Basic = Template.bind({})
-Basic.args = {
-  heading: 'Test Heading',
-  content: 'test content',
-  links: [
-    { text: 'test link', url: '#', type: 'inbound' },
-    { text: 'test link', url: '#' },
-  ],
-  product: 'packer',
-  variant: 'centered',
-}
+export const Centered = Template.bind({})
+Centered.args = { variant: 'centered' }
+
+export const Compact = Template.bind({})
+Compact.args = { variant: 'compact' }
+
+export const Links = Template.bind({})
+Links.args = { variant: 'links' }
