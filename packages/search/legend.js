63df8c7687c07949b7176258c5d82cd772bd4dc2
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-19 12:34:29 -0400

    refactor(search)!: convert to CSS modules

--- packages/search/legend.js
+++ packages/search/legend.js
@@ -2,28 +2,30 @@ import InlineSvg from '@hashicorp/react-inline-svg'
 import IconEnter from './img/search-legend-enter.svg.js'
 import IconPage from './img/search-legend-page.svg.js'
 import IconEscape from './img/search-legend-escape.svg.js'
+import s from './legend.module.css'
+import VisuallyHidden from '@reach/visually-hidden'
 
 export default function SearchLegend() {
   return (
-    <div className="c-search-legend">
-      <figure className="legend-item">
+    <div className={s.searchLegend}>
+      <figure className={s.legendItem}>
         <InlineSvg src={IconEnter} aria-hidden={true} />
-        <p className="g-type-tag-label">
-          <span className="visually-hidden">Enter</span>
+        <p className={s.legendLabel}>
+          <VisuallyHidden>Enter</VisuallyHidden>
           to select
         </p>
       </figure>
-      <figure className="legend-item">
+      <figure className={s.legendItem}>
         <InlineSvg src={IconPage} aria-hidden={true} />
-        <p className="g-type-tag-label">
-          <span className="visually-hidden">Tab</span>
+        <p className={s.legendLabel}>
+          <VisuallyHidden>Tab</VisuallyHidden>
           to navigate
         </p>
       </figure>
-      <figure className="legend-item">
+      <figure className={s.legendItem}>
         <InlineSvg src={IconEscape} aria-hidden={true} />
-        <p className="g-type-tag-label">
-          <span className="visually-hidden">Escape</span>
+        <p className={s.legendLabel}>
+          <VisuallyHidden>Escape</VisuallyHidden>
           to dismiss
         </p>
       </figure>
