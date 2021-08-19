a69135f356add234608a024a371905e0fd881fc0
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-19 15:03:24 -0400

    refactor(accordion)!: convert to CSS modules

--- packages/accordion/index.js
+++ packages/accordion/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import AccordionItems, { AccordionItem } from './partials/AccordionItems'
+import s from './style.module.css'
 
 function Accordion({ heading, items }) {
   return (
-    <section className="g-accordion">
+    <section className={s.root}>
       <div className="g-grid-container">
         {heading && (
-          <h2 className="g-type-display-2" data-testid="heading">
+          <h2 className={s.heading} data-testid="heading">
             {heading}
           </h2>
         )}
