3109fad3018f4494d23e62ab4ccffbd2951f9127
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-19 20:16:22 -0400

    fix(storybook): tweak story for content-cta

--- .storybook/stories/components/content-cta.stories.tsx
+++ .storybook/stories/components/content-cta.stories.tsx
@@ -5,6 +5,22 @@ import ContentCta from '../../../packages/content-cta'
 export default {
   title: 'Components/ContentCta',
   component: ContentCta,
+  args: {
+    product: 'terraform',
+    heading: 'My Custom Heading',
+    content:
+      'Compare Terraform Cloud and Terraform Enterprise features to find the best fit for your organization.',
+    links: [
+      {
+        title: 'Compare Features',
+        url: 'example.com',
+      },
+      {
+        title: 'Contact Sales',
+        url: 'example.com',
+      },
+    ],
+  },
 } as ComponentMeta<typeof ContentCta>
 
 const Template: ComponentStory<typeof ContentCta> = (args) => {
@@ -12,19 +28,14 @@ const Template: ComponentStory<typeof ContentCta> = (args) => {
 }
 
 export const Basic = Template.bind({})
-Basic.args = {
-  product: 'terraform',
-  heading: 'My Custom Heading',
-  content:
-    'Compare Terraform Cloud and Terraform Enterprise features to find the best fit for your organization.',
-  links: [
-    {
-      title: 'Compare Features',
-      url: 'example.com',
-    },
-    {
-      title: 'Contact Sales',
-      url: 'example.com',
-    },
-  ],
+
+export const ContentRenderProp = Template.bind({})
+ContentRenderProp.args = {
+  heading: 'With Content Render Prop',
+  content: () => (
+    <p className="g-type-body">
+      Custom content text. This is an <a href="example.com">example link</a>.
+      Donec sed odio dui. Nullam id dolor id nibh ultricies vehicula ut id elit.
+    </p>
+  ),
 }
