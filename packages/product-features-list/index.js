f3ebcefec8314bd4ffef8ea01987f5e2edfdd3f7
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-23 20:37:23 -0400

    refactor(product-features-list)!: convert to CSS modules

--- packages/product-features-list/index.js
+++ packages/product-features-list/index.js
@@ -1,20 +1,22 @@
 import Button from '@hashicorp/react-button'
+import s from './style.module.css'
 
 export default function ProductFeaturesList({ heading, features }) {
   return (
-    <div className="g-product-features-list g-grid-container">
-      <h2 className="g-type-display-2">{heading}</h2>
-      <div className="features-container">
+    <div className={s.root}>
+      <h2 className={s.heading}>{heading}</h2>
+      <div className={s.featuresContainer}>
         {features.map(({ title, content, icon, link }) => (
-          <div key={title} className="feature">
-            <div className="feature-icon">
+          <div key={title} className={s.feature}>
+            <div className={s.featureIcon}>
               <img src={icon} alt={title} />
             </div>
-            <div className="content">
-              <h4 className="g-type-display-4">{title}</h4>
-              <p className="g-type-body-small">{content}</p>
+            <div>
+              <h4 className={s.featureHeading}>{title}</h4>
+              <p className={s.featureContent}>{content}</p>
               {link && (
                 <Button
+                  className={s.featureButton}
                   linkType={link.type}
                   theme={{ variant: 'tertiary-neutral' }}
                   title={link.text}
