0130827bcba7543780993394e27057b47887dfc6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-24 15:03:20 -0400

    refactor(tabs): convert to CSS modules

--- packages/tabs/partials/TabTriggers/index.js
+++ packages/tabs/partials/TabTriggers/index.js
@@ -1,147 +1,177 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import InlineSvg from '@hashicorp/react-inline-svg'
 import TabTrigger from './partials/TabTrigger/index.js'
 import SvgChevronRight from './icons/chevron-right_black.svg.js'
 import smoothScroll from './helpers/smoothScroll.js'
+import classNames from 'classnames'
+import s from './style.module.css'
+import useWindowSize from '../../hooks/use-window-size'
+import useScroll from '../../hooks/use-scroll-data'
+import { useEffect } from 'react'
 
-class TabTriggers extends React.Component {
-  constructor(props) {
-    super(props)
-    this.parentRef = React.createRef()
-    this.handleResize = this.handleResize.bind(this)
-    this.getScrollOffset = this.getScrollOffset.bind(this)
-    this.updateScrollOffset = this.updateScrollOffset.bind(this)
-  }
+function TabTriggers({
+  tabs,
+  activeTabIdx,
+  setActiveTab,
+  centered,
+  fullWidthBorder,
+}) {
+  // const tabTriggerParentRef = useRef()
+  const sizeReferenceRef = useRef()
+  const parentRef = useRef()
+  // const scrollRef = useRef()
+  const windowSize = useWindowSize()
+  const [scrollRef, { scrollLeft }] = useScroll()
+  const [hiddenArrows, setHiddenArrows] = useState({
+    prev: true,
+    next: true,
+  })
+  const [hasOverflow, setHasOverflow] = useState(false)
 
-  componentWillUnmount() {
-    const scrollWrapper = this.parentRef.current.querySelector('.inner')
-    scrollWrapper.removeEventListener('scroll', this.handleResize)
-    const windowElem = scrollWrapper.closest('html').parentNode.defaultView
-    windowElem.removeEventListener('resize', this.handleResize)
-  }
-
-  componentDidMount() {
-    this.handleResize()
-    const scrollWrapper = this.parentRef.current.querySelector('.inner')
-    scrollWrapper.addEventListener('scroll', this.handleResize)
-    const windowElem = scrollWrapper.closest('html').parentNode.defaultView
-    windowElem.addEventListener('resize', this.handleResize)
-  }
-
-  handleResize() {
-    const parentElem = this.parentRef.current
-    const elems = {
-      scrollWrapper: parentElem.querySelector('.inner'),
-      sizeReference: parentElem.querySelector('.size-reference'),
-      tabTriggers: parentElem.querySelectorAll('.g-tab-trigger'),
-      prevArrow: parentElem.querySelector('.arrow.prev'),
-      nextArrow: parentElem.querySelector('.arrow.next'),
-    }
+  /* handle resize and other changes */
+  useEffect(() => {
     //  Determine combined width of contained tabs
     let contentWidth = 0
-    elems.tabTriggers.forEach((t) => (contentWidth += t.offsetWidth))
+    const tabTriggers = scrollRef.current.children
+    tabTriggers.forEach((t) => (contentWidth += t.offsetWidth))
     //  Determine width of available space
-    //  using the .g-grid-container.size-reference element
-    const availableSpace = elems.sizeReference.offsetWidth
+    //  using the .g-grid-container .size-reference element
+    const availableSpace = sizeReferenceRef.current.offsetWidth
     //  If more content width than available space, set to overflow-friendly styling
     //  (note: overflow-friendly styling may not immediately cause actual overflow / scrolling,
     //  as it increases space for nav links slightly)
-    const hasOverflow = contentWidth > availableSpace
-    //  Add attribute to parent and scroll container
-    parentElem.setAttribute('data-overflow', hasOverflow.toString())
-    elems.scrollWrapper.setAttribute('data-overflow', hasOverflow.toString())
-    //  Determine if there is area left to scroll,
-    //  Set visibility of next / prev arrows based on that
-    const scrollWidth = elems.scrollWrapper.scrollWidth
-    const outerWidth = parentElem.offsetWidth
-    const hidePrev = elems.scrollWrapper.scrollLeft === 0
-    elems.prevArrow.setAttribute('data-overflow', hasOverflow.toString())
-    elems.prevArrow.setAttribute('data-invisible', hidePrev.toString())
-    const hideNext = elems.scrollWrapper.scrollLeft + outerWidth >= scrollWidth
-    elems.nextArrow.setAttribute('data-overflow', hasOverflow.toString())
-    elems.nextArrow.setAttribute('data-invisible', hideNext.toString())
-  }
+    setHasOverflow(contentWidth > availableSpace)
+  }, [scrollRef, windowSize])
 
-  updateScrollOffset(activeFilterIdx) {
-    const newScrollLeft = this.getScrollOffset(activeFilterIdx)
-    const scrollWrapper = this.parentRef.current.querySelector('.inner')
-    const windowElem = scrollWrapper.closest('html').parentNode.defaultView
-    smoothScroll(windowElem, scrollWrapper, { x: newScrollLeft })
-    this.setState({ activeIdx: activeFilterIdx })
-  }
+  /* hide next / prev arrows as needed if
+  fully scrolled to one end or the other */
+  useEffect(() => {
+    const scrollElem = scrollRef.current
+    const parentWidth = parentRef.current.offsetWidth
+    setHiddenArrows({
+      prev: scrollElem.scrollLeft === 0,
+      next: scrollElem.scrollLeft + parentWidth >= scrollElem.scrollWidth,
+    })
+  }, [scrollLeft, scrollRef])
 
-  getScrollOffset(targetIdx) {
-    if (targetIdx === 0) return -1 // Scroll to start of container
-    const scrollWrapper = this.parentRef.current.querySelector('.inner')
-    const targetSelector = `.g-tab-trigger[data-tabindex='${targetIdx}']`
-    const targetElem = scrollWrapper.querySelector(targetSelector)
-    const targetMidpoint = targetElem.offsetLeft + targetElem.offsetWidth / 2
-    const newScrollLeft = targetMidpoint - scrollWrapper.offsetWidth / 2
-    return newScrollLeft
-  }
+  /* automatically smoothly scroll to 
+  center a tab in the scrollable area as needed */
+  useEffect(() => {
+    if (!hasOverflow) return null
+    updateScrollOffset(activeTabIdx)
+  }, [hasOverflow, activeTabIdx, updateScrollOffset, scrollRef])
 
-  render() {
-    const { tabs, activeTabIdx, setActiveTab } = this.props
+  /* allow smooth scrolling to the active tab.
+  used both when the activeTabIdx updates, and
+  when the next or previous arrow is clicked but
+  does not cause an activeTabIdx update */
+  const updateScrollOffset = useCallback(
+    (targetTabIdx) => {
+      let newScrollLeft
+      // If first tab, scroll to start of container.
+      // Otherwise, calculate the midpoint of the
+      // active tab trigger
+      if (targetTabIdx === 0) {
+        newScrollLeft = -1
+      } else {
+        const targetSelector = `[data-tabindex='${targetTabIdx}']`
+        const targetElem = scrollRef.current.querySelector(targetSelector)
+        const targetMidpoint =
+          targetElem.offsetLeft + targetElem.offsetWidth / 2
+        newScrollLeft = targetMidpoint - scrollRef.current.offsetWidth / 2
+      }
+      console.log({ scrollLeft, newScrollLeft })
+      const windowElem = scrollRef.current.closest('html').parentNode
+        .defaultView
+      smoothScroll(windowElem, scrollRef.current, { x: newScrollLeft })
+    },
+    [scrollLeft, scrollRef]
+  )
 
-    return (
-      <div className="g-tab-triggers" ref={this.parentRef}>
-        <div className="g-grid-container">
-          <div className="size-reference"></div>
-        </div>
-        <div className="inner-wrapper">
-          <div className="inner">
-            <div className="content-container g-grid-container">
-              <div className="tooltip-space-overflow">
-                {tabs.map((tab, stableIdx) => (
-                  <TabTrigger
-                    // This array is stable, so we can use index as key
-                    // eslint-disable-next-line react/no-array-index-key
-                    key={stableIdx}
-                    activeTabIdx={activeTabIdx}
-                    setActiveTab={(targetIdx, groupId) => {
-                      setActiveTab(targetIdx, groupId)
-                      this.updateScrollOffset(targetIdx)
-                    }}
-                    tab={tab}
-                  />
-                ))}
-              </div>
+  return (
+    <div
+      className={classNames(s.root, { [s.fullWidthBorder]: fullWidthBorder })}
+      ref={parentRef}
+    >
+      <div className="g-grid-container">
+        {/* Note: the sizeReference element has zero height, but is still "visible".
+     Its width is used to determine when to use [data-overflow='true']  */}
+        <div ref={sizeReferenceRef}></div>
+      </div>
+      <div className={s.innerWrapper}>
+        <div className={s.inner}>
+          <div className={s.contentContainer}>
+            <div
+              className={classNames(s.tooltipSpaceOverflow, {
+                [s.centered]: centered,
+                [s.hasOverflow]: hasOverflow,
+              })}
+              ref={scrollRef}
+            >
+              {tabs.map((tab, stableIdx) => (
+                <TabTrigger
+                  // This array is stable, so we can use index as key
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={stableIdx}
+                  hasOverflow={hasOverflow}
+                  activeTabIdx={activeTabIdx}
+                  setActiveTab={(targetIdx, groupId) => {
+                    setActiveTab(targetIdx, groupId)
+                    updateScrollOffset(targetIdx)
+                  }}
+                  tab={tab}
+                />
+              ))}
             </div>
           </div>
-          <div
-            className="arrow prev"
-            onClick={() => {
-              if (activeTabIdx > 0) {
-                setActiveTab(activeTabIdx - 1, tabs[activeTabIdx - 1].group)
-                this.updateScrollOffset(activeTabIdx - 1)
-              } else {
-                this.updateScrollOffset(activeTabIdx)
-              }
-            }}
-          >
-            <InlineSvg src={SvgChevronRight} />
-          </div>
-          <div
-            className="arrow next"
-            onClick={() => {
-              if (activeTabIdx < tabs.length - 1) {
-                setActiveTab(activeTabIdx + 1, tabs[activeTabIdx + 1].group)
-                this.updateScrollOffset(activeTabIdx + 1)
-              } else {
-                this.updateScrollOffset(activeTabIdx)
-              }
-            }}
-          >
-            <InlineSvg src={SvgChevronRight} />
-          </div>
         </div>
-        <div className="g-grid-container">
-          <div className="bottom-border default"></div>
+        <div
+          className={classNames(s.arrow, s.prevArrow, {
+            [s.hasOverflow]: hasOverflow,
+            [s.hidden]: hiddenArrows.prev,
+          })}
+          onClick={() => {
+            if (activeTabIdx > 0) {
+              setActiveTab(activeTabIdx - 1, tabs[activeTabIdx - 1].group)
+            } else {
+              updateScrollOffset(activeTabIdx)
+            }
+          }}
+        >
+          <InlineSvg src={SvgChevronRight} />
         </div>
-        <div className="bottom-border for-overflow"></div>
+        <div
+          className={classNames(s.arrow, s.nextArrow, {
+            [s.hasOverflow]: hasOverflow,
+            [s.hidden]: hiddenArrows.next,
+          })}
+          onClick={() => {
+            if (activeTabIdx < tabs.length - 1) {
+              setActiveTab(activeTabIdx + 1, tabs[activeTabIdx + 1].group)
+            } else {
+              updateScrollOffset(activeTabIdx)
+            }
+          }}
+        >
+          <InlineSvg src={SvgChevronRight} />
+        </div>
       </div>
-    )
-  }
+      <div className="g-grid-container">
+        <div
+          className={classNames(s.bottomBorder, s.forDefault, {
+            [s.hasOverflow]: hasOverflow,
+            [s.fullWidthBorder]: fullWidthBorder,
+          })}
+        ></div>
+      </div>
+      <div
+        className={classNames(s.bottomBorder, s.forOverflow, {
+          [s.hasOverflow]: hasOverflow,
+          [s.fullWidthBorder]: fullWidthBorder,
+        })}
+      ></div>
+    </div>
+  )
 }
 
 export default TabTriggers
