191d1b819b0e7aaa06651db0ab7bcfde3379edc2
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-08-27 09:45:30 -0400

    chore(tabs): update function name to match file name

--- packages/tabs/hooks/use-scroll-left.js
+++ packages/tabs/hooks/use-scroll-left.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 
-export default function useScroll() {
+export default function useScrollLeft() {
   const scrollRef = useRef()
   const [scrollLeft, setScrollLeft] = useState()
 
