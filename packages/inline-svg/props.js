1c36b63014aa9c3809f7321e55b8bcd413ddfaaf
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-01 13:35:41 -0400

    Add prop documentation

--- packages/inline-svg/props.js
+++ packages/inline-svg/props.js
@@ -7,4 +7,11 @@ module.exports = {
       '<svg viewBox="0 0 300 200">\n  <rect width="100%" height="100%" fill="#000000" />\n  <circle cx="150" cy="100" r="80" fill="#1563ff" />\n  <text x="150" y="125" font-size="60" text-anchor="middle" fill="#ffffff">SVG</text>\n</svg>',
     required: true,
   },
+  spanWrapper: {
+    description: 'Render wrapping element as a span',
+    control: { type: 'json' },
+    type: 'boolean',
+    testValue: false,
+    required: false,
+  },
 }
