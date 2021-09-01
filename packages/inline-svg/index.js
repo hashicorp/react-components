c412cf2fd76fc2731311010a5b42e58143df647b
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-09-01 13:24:15 -0400

    Update to spanWrapper boolean

--- packages/inline-svg/index.js
+++ packages/inline-svg/index.js
@@ -1,9 +1,4 @@
-const supportedElements = {
-  div: 'div',
-  span: 'span',
-}
-
-export default function InlineSvg({ wrapper, src, ...props }) {
-  const Component = supportedElements[wrapper] || supportedElements['div']
+export default function InlineSvg({ spanWrapper, src, ...props }) {
+  const Component = spanWrapper ? 'span' : 'div'
   return <Component dangerouslySetInnerHTML={{ __html: src }} {...props} />
 }
--- packages/inline-svg/index.test.js
+++ packages/inline-svg/index.test.js
@@ -48,9 +48,9 @@ describe('<InlineSvg />', () => {
     expect(rootElem.childNodes.length).toBe(0)
   })
 
-  it('should render custom wrapper element', () => {
+  it('should render wrapper element as span', () => {
     const props = {
-      wrapper: 'span',
+      spanWrapper: true,
       src:
         '<svg width="23" height="19" xmlns="http://www.w3.org/2000/svg"><path d="M22.475 2.163a9.248 9.248 0 01-2.649.725 4.612 4.612 0 002.028-2.55 9.21 9.21 0 01-2.93 1.118 4.611 4.611 0 00-7.856 4.205A13.09 13.09 0 011.565.843 4.614 4.614 0 002.992 7a4.627 4.627 0 01-2.09-.576v.057a4.612 4.612 0 003.7 4.522 4.498 4.498 0 01-1.215.162c-.298 0-.586-.028-.867-.082a4.61 4.61 0 004.306 3.2A9.25 9.25 0 011.1 16.26c-.372 0-.74-.02-1.1-.063a13.06 13.06 0 007.067 2.071c8.484 0 13.12-7.026 13.12-13.12 0-.2-.004-.4-.013-.597a9.339 9.339 0 002.301-2.387" fill-rule="evenodd"/></svg>',
     }
