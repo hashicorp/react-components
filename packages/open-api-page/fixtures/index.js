f6ca0e809edffee096f7177e48d69a8747ecddba
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-09-10 10:20:40 -0400

    chore(open-api-page): remove outdated comment

--- packages/open-api-page/fixtures/index.js
+++ packages/open-api-page/fixtures/index.js
@@ -2,10 +2,6 @@ import PackerSwagger from './generated/packer.swagger.json'
 import BoundarySwagger from './generated/boundary.swagger.json'
 import { getPropsForPage } from '../utils/routing-utils'
 
-// TODO: dereference swagger JSON.
-// Has to be done async, so may have be done in a decorator
-// within the actual story.
-
 const Packer = {
   ...getPropsForPage(PackerSwagger, { page: ['build-service'] }),
   productSlug: 'packer',
