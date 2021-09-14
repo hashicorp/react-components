119f03662201d2b603edc7431375bcda70b44a6f
Jeff Escalante <jescalan@users.noreply.github.com>
2021-09-14 12:23:32 -0400

    add headline feature

--- packages/docs-sidenav/index.js
+++ packages/docs-sidenav/index.js
@@ -171,6 +171,12 @@ function NavTree({ baseRoute, content }) {
       // eslint-disable-next-line react/no-array-index-key
       return <Divider key={idx} />
     }
+
+    // Headings
+    if (item.heading) {
+      return <h3>{item.heading}</h3>
+    }
+
     // Direct links
     if (item.title && item.href) {
       return (
