40afe45a59c8c7d6fcdc097dcf67b382e6b2543b
Kevin Wang <kwangsan@gmail.com>
2021-09-15 12:24:15 -0400

    chore: DocsPage to TypeScript (#337)

---
+++ .changeset/gentle-penguins-argue.md
@@ -0,0 +1,5 @@
+---
+'@hashicorp/react-docs-page': minor
+---
+
+This converts DocsPage to TypeScript. There are no new changes to the component API!
--- packages/docs-page/index.tsx
+++ packages/docs-page/index.tsx
@@ -1,16 +1,19 @@
-import { useEffect } from 'react'
+import { useEffect, FunctionComponent, ReactElement } from 'react'
 import classNames from 'classnames'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Content from '@hashicorp/react-content'
 import DocsSidenav from '@hashicorp/react-docs-sidenav'
+import { NavData } from '@hashicorp/react-docs-sidenav/types'
 import HashiHead from '@hashicorp/react-head'
-import { MDXRemote } from 'next-mdx-remote'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { SearchProvider } from '@hashicorp/react-search'
 import {
   VersionSelect,
   getVersionFromPath,
 } from '@hashicorp/versioned-docs/client'
+import { MDXProviderComponentsProp } from '@mdx-js/react'
+
 import SearchBar from './components/search-bar'
 import VersionAlert from './components/version-alert'
 import generateComponents from './components'
@@ -19,7 +22,20 @@ import LoadingSkeleton from './components/loading-skeleton'
 import useIsMobile from './use-is-mobile'
 import s from './style.module.css'
 
-export function DocsPageWrapper({
+interface DocsPageWrapperProps {
+  canonicalUrl: string
+  description: string
+  navData: NavData
+  currentPath: string
+  pageTitle: string
+  baseRoute: string
+  githubFileUrl: string
+  product: { name: string; slug: string }
+  showEditPage: boolean
+  versions: { name: string; label: string }[]
+}
+
+const DocsPageWrapper: FunctionComponent<DocsPageWrapperProps> = ({
   canonicalUrl,
   children,
   description,
@@ -31,7 +47,7 @@ export function DocsPageWrapper({
   product: { name, slug },
   showEditPage = true,
   versions,
-}) {
+}) => {
   const isMobile = useIsMobile()
   const { asPath } = useRouter()
   const versionInPath = getVersionFromPath(asPath)
@@ -127,6 +143,25 @@ export function DocsPageWrapper({
   )
 }
 
+export interface DocsPageProps {
+  product: { name: string; slug: string }
+  baseRoute: string
+  showEditPage: boolean
+  additionalComponents: MDXProviderComponentsProp
+  staticProps: {
+    mdxSource: MDXRemoteSerializeResult
+    frontMatter: {
+      canonical_url: string
+      description: string
+      page_title: string
+    }
+    currentPath: string
+    navData: NavData
+    githubFileUrl: string
+    versions: { name: string; label: string }[]
+  }
+}
+
 export default function DocsPage({
   product,
   baseRoute,
@@ -140,7 +175,7 @@ export default function DocsPage({
     githubFileUrl,
     versions,
   },
-}) {
+}: DocsPageProps): ReactElement {
   const router = useRouter()
 
   // This component is written to work with next-mdx-remote -- here it hydrates the content
