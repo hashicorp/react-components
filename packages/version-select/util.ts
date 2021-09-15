845bcfe9b141969a5fab2cb0cb75cc57b67e66d3
Kevin Wang <kwangsan@gmail.com>
2021-09-15 17:20:07 -0400

    refactor(version-select): utils handle `v0.5.x`

--- packages/version-select/util.ts
+++ packages/version-select/util.ts
@@ -1,4 +1,4 @@
-const SEMVER_REGEX = /^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/i
+const REGEX = /^v([0-9]+)\.([0-9]+)\.(x|[0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/i
 
 /**
  * Extract the version from a path string, or an array of path segments
@@ -6,19 +6,19 @@ const SEMVER_REGEX = /^v([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A
 export function getVersionFromPath(path: string): string | undefined {
   const pathSegments = path.split('/')
 
-  const version = pathSegments.find((el) => SEMVER_REGEX.test(el))
+  const version = pathSegments.find((el) => REGEX.test(el))
 
   return version
 }
 
 /**
- * Removes a semver string from a path, and returns the new path.
- * Returns the original string if no semver is present.
+ * Removes a version string from a path, and returns the new path.
+ * Returns the original string if no version is present.
  */
 export function removeVersionFromPath(path: string): string {
   const pathSegments = path.split('/')
 
-  const i = pathSegments.findIndex((el) => SEMVER_REGEX.test(el))
+  const i = pathSegments.findIndex((el) => REGEX.test(el))
 
   if (i > -1) {
     return [...pathSegments.slice(0, i), ...pathSegments.slice(i + 1)].join('/')
