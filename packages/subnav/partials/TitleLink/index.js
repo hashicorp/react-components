36a62a5c27bf2ccf0ed5f31cfc481aab6264ffb7
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-04 22:18:27 -0400

    fix: use proper colorwhite logos for dark theme

--- packages/subnav/partials/TitleLink/index.js
+++ packages/subnav/partials/TitleLink/index.js
@@ -1,7 +1,8 @@
 import LinkWrap from '@hashicorp/react-link-wrap'
 import InlineSvg from '@hashicorp/react-inline-svg'
 import s from './style.module.css'
-
+import classNames from 'classnames'
+/* main logos, for light theme */
 import ConsulLogo from '@hashicorp/mktg-logos/product/consul/primary/color.svg?include'
 import HCPLogo from '@hashicorp/mktg-logos/product/hcp/primary/black.svg?include'
 import NomadLogo from '@hashicorp/mktg-logos/product/nomad/primary/color.svg?include'
@@ -12,23 +13,47 @@ import VaultLogo from '@hashicorp/mktg-logos/product/vault/primary/color.svg?inc
 import BoundaryLogo from '@hashicorp/mktg-logos/product/boundary/primary/color.svg?include'
 import WaypointLogo from '@hashicorp/mktg-logos/product/waypoint/primary/color.svg?include'
 import TerraformCloudLogo from '@hashicorp/mktg-logos/product/terraform-cloud/primary/color.svg?include'
-import classNames from 'classnames'
+/* white logos, for dark theme */
+import ConsulLogoWhite from '@hashicorp/mktg-logos/product/consul/primary/colorwhite.svg?include'
+import HCPLogoWhite from '@hashicorp/mktg-logos/product/hcp/primary/white.svg?include'
+import NomadLogoWhite from '@hashicorp/mktg-logos/product/nomad/primary/colorwhite.svg?include'
+import PackerLogoWhite from '@hashicorp/mktg-logos/product/packer/primary/colorwhite.svg?include'
+import TerraformLogoWhite from '@hashicorp/mktg-logos/product/terraform/primary/colorwhite.svg?include'
+import VagrantLogoWhite from '@hashicorp/mktg-logos/product/vagrant/primary/colorwhite.svg?include'
+import VaultLogoWhite from '@hashicorp/mktg-logos/product/vault/primary/colorwhite.svg?include'
+import BoundaryLogoWhite from '@hashicorp/mktg-logos/product/boundary/primary/colorwhite.svg?include'
+import WaypointLogoWhite from '@hashicorp/mktg-logos/product/waypoint/primary/colorwhite.svg?include'
+import TerraformCloudLogoWhite from '@hashicorp/mktg-logos/product/terraform-cloud/primary/colorwhite.svg?include'
 
 const logoDict = {
-  boundary: BoundaryLogo,
-  consul: ConsulLogo,
-  hcp: HCPLogo,
-  nomad: NomadLogo,
-  packer: PackerLogo,
-  terraform: TerraformLogo,
-  tfc: TerraformCloudLogo,
-  vagrant: VagrantLogo,
-  vault: VaultLogo,
-  waypoint: WaypointLogo,
+  light: {
+    boundary: BoundaryLogo,
+    consul: ConsulLogo,
+    hcp: HCPLogo,
+    nomad: NomadLogo,
+    packer: PackerLogo,
+    terraform: TerraformLogo,
+    tfc: TerraformCloudLogo,
+    vagrant: VagrantLogo,
+    vault: VaultLogo,
+    waypoint: WaypointLogo,
+  },
+  dark: {
+    boundary: BoundaryLogoWhite,
+    consul: ConsulLogoWhite,
+    hcp: HCPLogoWhite,
+    nomad: NomadLogoWhite,
+    packer: PackerLogoWhite,
+    terraform: TerraformLogoWhite,
+    tfc: TerraformCloudLogoWhite,
+    vagrant: VagrantLogoWhite,
+    vault: VaultLogoWhite,
+    waypoint: WaypointLogoWhite,
+  },
 }
 
-function TitleLink({ text, url, product, Link }) {
-  const Logo = logoDict[text.toLowerCase()]
+function TitleLink({ text, url, product, Link, theme }) {
+  const Logo = logoDict[theme][text.toLowerCase()]
   return (
     <LinkWrap
       Link={Link}
