806c219be8000aa594499a52d814bbb6f1e7a54a
Bryce Kalow <bkalow@hashicorp.com>
2021-10-12 12:14:57 -0500

    remove old files

--- packages/docs-page/server/index.ts
+++ packages/docs-page/server/index.ts
@@ -8,8 +8,6 @@ import { DataLoader } from './loaders/types'
 // where we want to re-use these utilities to build
 // getStaticPaths and getStaticProps functions that
 // fall outside the use case of local-only content
-export { generateStaticPaths } from './generate-static-paths'
-export { generateStaticProps } from './generate-static-props'
 export { getNodeFromPath } from './get-node-from-path'
 export { getPathsFromNavData } from './get-paths-from-nav-data'
 export { validateNavData } from './validate-nav-data'
