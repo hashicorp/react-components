4eb266252e458a7bb124e012b914860a480d626a
Bryce Kalow <bkalow@hashicorp.com>
2021-10-12 11:39:43 -0500

    remove unused type

--- packages/docs-page/server/loaders/types.ts
+++ packages/docs-page/server/loaders/types.ts
@@ -10,14 +10,3 @@ export interface DataLoaderOpts {
   product: string
   paramId?: string
 }
-
-export interface DataLoaderContext {
-  navDataFile: string
-  localContentDir: string
-  params: Record<string, string[]> // {} | { page: ["destroy"] }
-  product: string
-  mainBranch?: string // = 'main',
-  remarkPlugins?: any[]
-  scope?: any // optional, I think?
-  basePath: string // 'docs'
-}
