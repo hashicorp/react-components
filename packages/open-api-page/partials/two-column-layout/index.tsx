5a465c73ece2fe1adeaadd2154ee6aff7d4f370e
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-17 00:59:04 -0400

    style(open-api-page): convert two-column-layout to TypeScript

--- packages/open-api-page/partials/two-column-layout/index.tsx
+++ packages/open-api-page/partials/two-column-layout/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import s from './style.module.css'
 
-function TwoColumnLayout({ columnOne, columnTwo }) {
+function TwoColumnLayout({
+  columnOne,
+  columnTwo,
+}: {
+  /** Render slot for the first column. */
+  columnOne: React.ReactNode
+  /** Render slot for the second column. */
+  columnTwo: React.ReactNode
+}): React.ReactElement {
   return (
     <div className={s.twoColumnLayout}>
       <div>{columnOne}</div>
