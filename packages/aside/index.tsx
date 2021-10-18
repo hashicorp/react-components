d5a807469e37dedb9affe313dc3d4d9561abd2f6
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-18 11:38:08 -0400

    docs(aside): update prop docs for aside component

--- packages/aside/index.tsx
+++ packages/aside/index.tsx
@@ -7,8 +7,11 @@ function Aside({
   children,
   type = 'info',
 }: {
+  /** Optional className to add to the root element. */
   className?: string
+  /** Children to render into the Alert block. */
   children: React.ReactNode
+  /** The type of message being displayed, which mainly affects coloration. Defaults to "info". */
   type?: 'info' | 'success' | 'warning' | 'danger'
 }): React.ReactElement {
   return (
