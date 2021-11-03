ac6e9f89075e5ca62b363d7d726bc9866db9b985
Bryce Kalow <bkalow@hashicorp.com>
2021-11-03 11:00:05 -0500

    restrict width of the consent manager banner content

--- packages/consent-manager/components/banner.js
+++ packages/consent-manager/components/banner.js
@@ -8,45 +8,47 @@ export default function ConsentBanner({
 }) {
   return (
     <div className={s.root} data-testid="consent-banner">
-      <span>
-        We use cookies & other similar technology to collect data to improve
-        your experience on our site, as described in our{' '}
-        <a
-          href={privacyPolicyLink}
-          className={s.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          data-testid="privacy-policy"
-        >
-          Privacy Policy
-        </a>{' '}
-        and{' '}
-        <a
-          href={cookiePolicyLink}
-          className={s.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          data-testid="cookie-policy"
-        >
-          Cookie Policy
-        </a>
-        .
-      </span>
-      <div>
-        <button
-          className={s.manageButton}
-          onClick={onManagePreferences}
-          data-testid="manage-preferences"
-        >
-          Manage Preferences
-        </button>
-        <button
-          className={s.acceptButton}
-          onClick={onAccept}
-          data-testid="accept"
-        >
-          Accept
-        </button>
+      <div className={s.innerContainer}>
+        <span>
+          We use cookies & other similar technology to collect data to improve
+          your experience on our site, as described in our{' '}
+          <a
+            href={privacyPolicyLink}
+            className={s.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            data-testid="privacy-policy"
+          >
+            Privacy Policy
+          </a>{' '}
+          and{' '}
+          <a
+            href={cookiePolicyLink}
+            className={s.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            data-testid="cookie-policy"
+          >
+            Cookie Policy
+          </a>
+          .
+        </span>
+        <div>
+          <button
+            className={s.manageButton}
+            onClick={onManagePreferences}
+            data-testid="manage-preferences"
+          >
+            Manage Preferences
+          </button>
+          <button
+            className={s.acceptButton}
+            onClick={onAccept}
+            data-testid="accept"
+          >
+            Accept
+          </button>
+        </div>
       </div>
     </div>
   )
