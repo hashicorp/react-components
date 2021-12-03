9d58e36808c6d12aeeb283fb8fed8706e121dcc0
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-12-03 12:16:56 -0500

    enhancement: Update <HashiStackMenu /> (#440)
    * Add support for description and CTA to groups
    
    * Rearrange/add data
    
    * Impl. CTA link per our main site
    
    * Add changeset
    
    * Adjust Packer cloud link

---
+++ .changeset/popular-wombats-knock.md
@@ -0,0 +1,5 @@
+---
+'@hashicorp/react-hashi-stack-menu': minor
+---
+
+Adds support for a custom group description and a group CTA. Also, moves some products around and updates the right-hand side, all to reflect current information.
--- packages/hashi-stack-menu/stack-group/index.jsx
+++ packages/hashi-stack-menu/stack-group/index.jsx
@@ -1,16 +1,27 @@
 import styles from './stack-group.module.css'
+import InlineSvg from '../../inline-svg'
+import LinkWrap from '../../link-wrap'
 import StackItem from '../stack-item'
 
-export default function StackGroup({ heading, items }) {
+export default function StackGroup({ heading, description, items, cta }) {
   return (
     <li className={styles.stackGroup}>
       <h5 className={styles.stackGroupHeading}>{heading}</h5>
+      {description && (
+        <p className={styles.stackGroupDescription}>{description}</p>
+      )}
       <ul className={styles.stackGroupLinkList}>
         {items.map((item, stableIdx) => (
           // eslint-disable-next-line react/no-array-index-key
           <StackItem key={stableIdx} item={item} />
         ))}
       </ul>
+      {cta && (
+        <LinkWrap href={cta.url} className={styles.stackGroupCta}>
+          {cta.text}
+          <InlineSvg src={require('../assets/icon-arrow.svg?include')} />
+        </LinkWrap>
+      )}
     </li>
   )
 }
