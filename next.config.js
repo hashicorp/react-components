c1ef1438f7b6ac0664761b54158152e1b417fee0
Bryce Kalow <bkalow@hashicorp.com>
2022-01-14 16:58:17 -0600

    patch SSR plugin for wasm worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,46 @@
 const withHashicorp = require('@hashicorp/platform-nextjs-plugin')
 const withSwingset = require('swingset')
+const SSRPlugin = require('next/dist/build/webpack/plugins/nextjs-ssr-import')
+  .default
+const { dirname, relative, resolve, join } = require('path')
+
+// Unfortunately there isn't an easy way to override the replacement function body, so we
+// have to just replace the whole plugin `apply` body.
+function patchSsrPlugin(plugin) {
+  plugin.apply = function apply(compiler) {
+    compiler.hooks.compilation.tap('NextJsSSRImport', (compilation) => {
+      compilation.mainTemplate.hooks.requireEnsure.tap(
+        'NextJsSSRImport',
+        (code, chunk) => {
+          // This is the block that fixes https://github.com/vercel/next.js/issues/22581
+          if (!chunk.name) {
+            return
+          }
+
+          // Update to load chunks from our custom chunks directory
+          const outputPath = resolve('/')
+          const pagePath = join('/', dirname(chunk.name))
+          const relativePathToBaseDir = relative(pagePath, outputPath)
+          // Make sure even in windows, the path looks like in unix
+          // Node.js require system will convert it accordingly
+          const relativePathToBaseDirNormalized = relativePathToBaseDir.replace(
+            /\\/g,
+            '/'
+          )
+          return code
+            .replace(
+              'require("./"',
+              `require("${relativePathToBaseDirNormalized}/"`
+            )
+            .replace(
+              'readFile(join(__dirname',
+              `readFile(join(__dirname, "${relativePathToBaseDirNormalized}"`
+            )
+        }
+      )
+    })
+  }
+}
 
 module.exports = withHashicorp({
   transpileModules: ['swingset', '@hashicorp/sentinel-embedded'],
@@ -31,6 +72,14 @@ module.exports = withHashicorp({
       },
     ],
     webpack(config) {
+      // ref: https://github.com/vercel/next.js/issues/22581#issuecomment-864476385
+      const ssrPlugin = config.plugins.find(
+        (plugin) => plugin instanceof SSRPlugin
+      )
+      if (ssrPlugin) {
+        patchSsrPlugin(ssrPlugin)
+      }
+
       config.module.rules.push({
         test: /\.wasm$/,
         type: 'asset/resource',
