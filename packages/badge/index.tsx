282709c8e36a8166ca5df4627c8f8d4fa4689187
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-02-25 12:19:19 -0500

    chore: make use of productStyles

--- packages/badge/index.tsx
+++ packages/badge/index.tsx
@@ -1,5 +1,6 @@
 import type { Products } from '@hashicorp/platform-product-meta'
-import classnames from 'classnames'
+import productStyles from '@hashicorp/platform-product-meta/style.module.css'
+import classNames from 'classnames'
 import s from './style.module.css'
 
 interface BadgeProps {
@@ -13,8 +14,15 @@ export default function Badge({
   variant = 'primary',
   theme = 'neutral',
 }: BadgeProps) {
+  let themeClass
+  const productClass = productStyles[theme]
+  if (productClass) {
+    themeClass = classNames(s.product, productClass)
+  } else {
+    themeClass = s[theme]
+  }
   return (
-    <span className={classnames(s.badge, s[theme], s[variant])}>
+    <span className={classNames(s.badge, themeClass, s[variant])}>
       {children}
     </span>
   )
