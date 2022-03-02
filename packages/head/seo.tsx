c2ae645987888e39c285904bad74300df379655d
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-03-02 13:37:20 -0800

    improve mergeSimilarTags readability

--- packages/head/seo.tsx
+++ packages/head/seo.tsx
@@ -12,6 +12,28 @@ export interface SeoMetatag {
   tag: 'meta' | 'title'
 }
 
+/**
+ * Returns a string representing a hyphenated version of the property and name
+ * attributes. For example:
+ * {
+ *   name: 'tag:name',
+ *   property: 'tag:property'
+ * }
+ * will return `tag:property-tag:name`. Additionally, something like
+ * {
+ *   name: 'tag:name'
+ * }
+ * will return `tag:name`, making this safe to use even if both property and
+ * name aren't present.
+ *
+ * This function is primarily used to handle instances where Dato provides us
+ * with an SEO attribute with a property and/or name value we're not doing any
+ * special handling for.
+ */
+function getMergedAttributeKey(attrs: Attributes): string {
+  return [attrs.property, attrs.name].filter(Boolean).join('-')
+}
+
 const KNOWN_PROPERTIES = [
   'og:title',
   'twitter:title',
@@ -27,7 +49,12 @@ const KNOWN_PROPERTIES = [
   'article:modified_time',
   'twitter:card',
 ]
-
+/**
+ * Given an attribute with both property and name values, this function returns
+ * a single value that can be used as a `key` for React reconciliation. If
+ * neither the property nor the name exists as a known value, it returns a
+ * merged version of the values.
+ */
 function getKeyForAttributes(attrs: Attributes): string {
   // Open Graph uses the `property` property, which we check first to ensure
   // that attributes with both `property` and `name` get keyed based on the
@@ -41,9 +68,29 @@ function getKeyForAttributes(attrs: Attributes): string {
     return attrs.name
   }
 
-  return [attrs.property, attrs.name].filter(Boolean).join('-')
+  return getMergedAttributeKey(attrs)
 }
 
+const SIMILAR_PROPERTIES = {
+  'twitter:description': 'og:description',
+  'twitter:title': 'og:title',
+  'twitter:image': 'og:image',
+}
+/**
+ * Maps known name values to preferred property values. Used primarily to
+ * merge twitter:* attributes into their equivalent og:* attribute.
+ */
+function getSimilarAttributeKey(attrs: Attributes): string {
+  if (attrs.name && attrs.name in SIMILAR_PROPERTIES) {
+    return SIMILAR_PROPERTIES[attrs.name]
+  }
+  return getMergedAttributeKey(attrs)
+}
+
+/**
+ * Given a metatag object from Dato, returns the props that will be passed to
+ * the element when rendered by React.
+ */
 function getPropsForMetatag(metatag: SeoMetatag): Record<string, string> {
   const props: Record<string, string> = {}
   if (metatag.attributes === null) {
@@ -73,58 +120,46 @@ function getPropsForMetatag(metatag: SeoMetatag): Record<string, string> {
   return props
 }
 
+/**
+ * Merges tags that are returned separately by Dato, but can exist together in
+ * the same tag due to their usage of different property/name values. Primarily
+ * used to merge twitter:* and og:* tags.
+ */
 function mergeSimilarTags(seo: SeoMetatag[]): SeoMetatag[] {
-  const tags: SeoMetatag[] = []
-
-  // Returns a boolean indicating whether mergeProperties added the given tag
-  // to the tags array
-  function mergeProperties(
-    tag: SeoMetatag,
-    property: string,
-    name: string
-  ): boolean {
-    if (
-      tag.attributes &&
-      (tag.attributes.property === property || tag.attributes.name === name)
-    ) {
-      const existingIndex = tags.findIndex(
-        (t) =>
-          t.attributes &&
-          (t.attributes.property === property || t.attributes.name === name)
-      )
-      if (
-        existingIndex > -1 &&
-        tags[existingIndex].attributes?.content === tag.attributes.content
-      ) {
-        tags[existingIndex].attributes = {
-          ...tags[existingIndex].attributes,
-          ...tag.attributes,
-        }
-
-        return true
-      } else {
-        tags.push(tag)
-
-        return true
-      }
-    } else {
-      return false
-    }
-  }
+  const tags: Record<string, SeoMetatag> = {}
+  const tagsWithoutAttributes: SeoMetatag[] = []
 
   for (const tag of seo) {
-    const wasReplaced = [
-      mergeProperties(tag, 'og:description', 'twitter:description'),
-      mergeProperties(tag, 'og:title', 'twitter:title'),
-      mergeProperties(tag, 'og:image', 'twitter:image'),
-    ]
-
-    if (!wasReplaced.some((t) => t)) {
-      tags.push(tag)
+    // If the tag doesn't have any attributes (such as title), we don't need to
+    // perform any attribute merging.
+    if (tag.attributes) {
+      // Get a stable key that's the same value for similar attributes
+      const key = getSimilarAttributeKey(tag.attributes)
+      if (key in tags) {
+        // Our stable key has already been used by another tag
+        if (tags[key].attributes?.content === tag.attributes.content) {
+          // The content values are equal, and we can safely reuse the existing
+          // object.
+          tags[key].attributes = {
+            ...tags[key].attributes,
+            ...tag.attributes,
+          }
+        } else {
+          // The content values are different, so we need to store this tag
+          // using a unique key
+          tags[getMergedAttributeKey(tag.attributes)] = tag
+        }
+      } else {
+        // key hasn't been used, so we go ahead and store the tag as-is
+        tags[key] = tag
+      }
+    } else {
+      tagsWithoutAttributes.push(tag)
     }
   }
 
-  return tags
+  // merge our attribute-less tags with our merged tags
+  return [...tagsWithoutAttributes, ...Object.values(tags)]
 }
 
 /**
