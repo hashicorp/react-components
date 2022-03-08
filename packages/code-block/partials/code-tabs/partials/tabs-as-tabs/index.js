9020cd5ec51e1a303a2164a402675e3c64307e53
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2022-03-08 15:52:42 -0500

    feat(code-block): left-align code-tabs when no heading is present

--- packages/code-block/partials/code-tabs/partials/tabs-as-tabs/index.js
+++ packages/code-block/partials/code-tabs/partials/tabs-as-tabs/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 import classNames from 'classnames'
 import s from './style.module.css'
 
-function TabsAsTabs({ tabLabels, activeTabIdx, setActiveTabIdx }) {
+function TabsAsTabs({ tabLabels, activeTabIdx, setActiveTabIdx, hasHeading }) {
   return (
-    <div className={s.tabsAsTabs}>
+    <div className={classNames(s.tabsAsTabs, { [s.hasHeading]: hasHeading })}>
       {tabLabels.map((tabLabel, idx) => {
         const isActive = idx == activeTabIdx
         return (
           <button
             // eslint-disable-next-line react/no-array-index-key
             key={idx}
-            className={classNames(s.tabControl, { [s.isActive]: isActive })}
+            className={classNames(
+              s.tabControl,
+              { [s.isActive]: isActive },
+              { [s.hasHeading]: hasHeading }
+            )}
             onClick={() => setActiveTabIdx(idx)}
           >
             {tabLabel}
