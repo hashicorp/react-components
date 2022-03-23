29c4f338115ca8aa701b59d1c7231b6b8a5cdebf
Kevin Wang <kwangsan@gmail.com>
2022-03-23 17:36:32 -0400

    logging

--- packages/docs-page/server/loaders/types.ts
+++ packages/docs-page/server/loaders/types.ts
@@ -1,7 +1,9 @@
 import { GetStaticPathsContext, GetStaticPropsContext } from 'next'
+import { Debugger } from 'debug'
 
 export interface DataLoader {
   opts: DataLoaderOpts
+  debug?: Debugger
   loadStaticPaths(ctx?: GetStaticPathsContext): Promise<$TSFixMe>
   loadStaticProps(ctx?: GetStaticPropsContext): Promise<$TSFixMe>
 }
