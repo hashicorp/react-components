de15ee0f045f2a3544b0a61ad35a55f08da628b5
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-28 14:04:24 -0400

    chore: rename packages

--- packages/quote/types.ts
+++ packages/quote/types.ts
@@ -1,4 +1,4 @@
-import type { AuthorBylineProps } from '@hashicorp/author-byline/types'
+import type { AuthorBylineProps } from '@hashicorp/react-author-byline/types'
 
 export interface QuoteProps extends AuthorBylineProps {
   /**
