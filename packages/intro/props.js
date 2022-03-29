924c0813d128b5bd3f079a757568922ff3500514
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-29 14:57:22 -0400

    chore: add descriptions

--- packages/intro/props.js
+++ packages/intro/props.js
@@ -1,26 +1,31 @@
 module.exports = {
+  textAlignment: {
+    type: 'string',
+    description: 'Controls the text alignment rendering.',
+    options: ['left' | 'center'],
+  },
   eyebrow: {
     type: 'string',
-    description: '',
+    description: 'Optional text displayed above the heading.',
   },
   heading: {
     required: true,
     type: 'string',
-    description: '',
+    description: 'Text displayed within the heading element.',
   },
   headingElement: {
     type: 'string',
-    description: '',
+    description: 'Controls which element the heading renders as.',
     options: ['h1', 'h2', 'h3', 'h4'],
   },
   headingSize: {
     type: 'number',
-    description: '',
+    description: 'Controls the size at which the heading is rendered.',
     options: [1, 2, 3],
   },
   description: {
     required: true,
     type: 'string',
-    description: '',
+    description: 'Text following the heading element.',
   },
 }
