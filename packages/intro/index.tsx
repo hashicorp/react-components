0571b72b3b6bceed39b4c70049d6732a8156419d
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-29 14:46:50 -0400

    chore: add styling

--- packages/intro/index.tsx
+++ packages/intro/index.tsx
@@ -1,18 +1,28 @@
+import classNames from 'classnames'
 import type { IntroProps } from './types'
 import s from './style.module.css'
 
 export default function Intro({
+  textAlignment = 'left',
   eyebrow,
   heading,
   headingElement: HeadingElement = 'h2',
-  headingSize,
+  headingSize = 2,
   description,
 }: IntroProps) {
+  const headingSizeClassname = `g-type-display-${headingSize}`
+  const descriptionSizeClassname = [1, 2].includes(headingSize)
+    ? 'g-type-body-large'
+    : 'g-type-body'
   return (
-    <div className={s.intro}>
+    <div className={classNames(s.intro, s[textAlignment])}>
       {eyebrow ? <p className={s.eyebrow}>{eyebrow}</p> : null}
-      <HeadingElement>{heading}</HeadingElement>
-      <p>{description}</p>
+      <HeadingElement className={classNames(s.heading, headingSizeClassname)}>
+        {heading}
+      </HeadingElement>
+      <p className={classNames(s.description, descriptionSizeClassname)}>
+        {description}
+      </p>
     </div>
   )
 }
