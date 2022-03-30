6dd764e821619f34647c37874ab03fb1a7167a1d
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-30 13:20:42 -0400

    chore: adding type descriptions and props descriptions

---
+++ packages/radio-input/props.js
@@ -0,0 +1,31 @@
+module.exports = {
+  variant: {
+    type: 'string',
+    description: '',
+    options: ['light', 'dark'],
+  },
+  label: {
+    type: 'string',
+    description: '',
+  },
+  name: {
+    type: 'string',
+    description: '',
+  },
+  value: {
+    type: 'string',
+    description: '',
+  },
+  checked: {
+    type: 'boolean',
+    description: '',
+  },
+  disabled: {
+    type: 'boolean',
+    description: '',
+  },
+  onChange: {
+    type: 'function',
+    description: '',
+  },
+}
