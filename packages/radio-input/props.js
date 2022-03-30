e2be5053663f5e863b3370acbdb137dc23927cf3
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-30 13:23:16 -0400

    chore: add props descriptions

--- packages/radio-input/props.js
+++ packages/radio-input/props.js
@@ -1,31 +1,32 @@
 module.exports = {
   variant: {
     type: 'string',
-    description: '',
+    description: 'Render on light or dark backgrounds.',
     options: ['light', 'dark'],
-  },
-  label: {
-    type: 'string',
-    description: '',
   },
   name: {
     type: 'string',
-    description: '',
+    description: 'The name attribute that will be applied to the radio option.',
+  },
+  label: {
+    type: 'string',
+    description: 'The label associated with the radio option.',
   },
   value: {
     type: 'string',
-    description: '',
+    description: 'The value associated with the radio option.',
   },
   checked: {
     type: 'boolean',
-    description: '',
+    description: 'If true, the radio option will be selected.',
   },
   disabled: {
     type: 'boolean',
-    description: '',
+    description: 'If true, the radio option will be disabled.',
   },
   onChange: {
     type: 'function',
-    description: '',
+    description:
+      'A function that will be called when the radio option is clicked.',
   },
 }
