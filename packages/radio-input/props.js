de8add39efea2a28218bf22d4329a2e6dd078d17
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-03-30 14:17:56 -0400

    chore: add required properties

--- packages/radio-input/props.js
+++ packages/radio-input/props.js
@@ -7,18 +7,22 @@ module.exports = {
   name: {
     type: 'string',
     description: 'The name attribute that will be applied to the radio option.',
+    required: true,
   },
   label: {
     type: 'string',
     description: 'The label associated with the radio option.',
+    required: true,
   },
   value: {
     type: 'string',
     description: 'The value associated with the radio option.',
+    required: true,
   },
   checked: {
     type: 'boolean',
     description: 'If true, the radio option will be selected.',
+    required: true,
   },
   disabled: {
     type: 'boolean',
@@ -28,5 +32,6 @@ module.exports = {
     type: 'function',
     description:
       'A function that will be called when the radio option is clicked.',
+    required: true,
   },
 }
