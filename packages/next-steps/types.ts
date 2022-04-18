dbac2d8c22b00b99e984492d217f39e2d5356d1c
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-18 13:12:47 -0400

    chore: add step types

--- packages/next-steps/types.ts
+++ packages/next-steps/types.ts
@@ -1,10 +1,24 @@
 import type { Products } from '@hashicorp/platform-product-meta'
 import type { ActionsProps } from '@hashicorp/react-actions/types'
 
+export interface StepProps {
+  heading: string
+  description?: string
+  cta: {
+    title: string
+    url: string
+  }
+}
+
+type SingleSteps = [StepProps]
+type DuoSteps = [StepProps, StepProps]
+type TrioSteps = [StepProps, StepProps, StepProps]
+
 export interface NextStepsProps {
   appearance: 'light' | 'dark'
   theme: Products
   heading: string
   description: string
   actions?: ActionsProps['ctas']
+  steps: SingleSteps | DuoSteps | TrioSteps
 }
