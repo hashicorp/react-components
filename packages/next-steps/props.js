6689306dbf8551cfff2e8a52c9f53303df45a49a
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-18 17:34:36 -0400

    chore: initial props documentation

--- packages/next-steps/props.js
+++ packages/next-steps/props.js
@@ -1,8 +1,48 @@
 module.exports = {
   heading: {
     type: 'string',
+    required: true,
   },
   description: {
     type: 'string',
+    required: true,
+  },
+  ctas: {
+    type: 'object',
+    properties: {
+      title: {
+        type: 'string',
+      },
+      url: {
+        type: 'string',
+      },
+    },
+  },
+  steps: {
+    type: 'object',
+    required: true,
+    properties: {
+      heading: {
+        type: 'string',
+        required: true,
+      },
+      description: {
+        type: 'string',
+      },
+      cta: {
+        type: 'object',
+        required: true,
+        properties: {
+          title: {
+            type: 'string',
+            required: true,
+          },
+          url: {
+            type: 'string',
+            required: true,
+          },
+        },
+      },
+    },
   },
 }
