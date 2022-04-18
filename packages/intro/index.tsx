07c898f8e5f5e2f09cb581a48a8423984d3ef382
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-18 17:02:45 -0400

    fix: intro type error

--- packages/intro/index.tsx
+++ packages/intro/index.tsx
@@ -17,7 +17,6 @@ export default function Intro({
   const descriptionSizeClassname = [1, 2].includes(headingSize)
     ? 'g-type-body-large'
     : 'g-type-body'
-  const renderActions = actions && actions.ctas?.length > 0
   return (
     <div
       className={classNames(s.intro, s[appearance], s[textAlignment])}
@@ -30,9 +29,9 @@ export default function Intro({
       <p className={classNames(s.description, descriptionSizeClassname)}>
         {description}
       </p>
-      {renderActions ? (
+      {actions && actions.ctas && actions.ctas.length > 0 ? (
         <div className={s.actions}>
-          <Actions appearance={appearance} {...actions} />
+          <Actions {...actions} appearance={appearance} ctas={actions.ctas} />
         </div>
       ) : null}
     </div>
