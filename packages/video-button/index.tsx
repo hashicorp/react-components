10b355b1d80a6db6afaae9a34dd6c3e25eb4e034
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-27 15:44:13 -0400

    chore: update play icon

--- packages/video-button/index.tsx
+++ packages/video-button/index.tsx
@@ -21,7 +21,33 @@ export default function VideoButton({
     >
       <span className={classNames(s.icon, s[theme], s[size], s[radius])}>
         <VisuallyHidden>{children}</VisuallyHidden>
-        {size === 'medium' ? <IconPlay16 /> : <IconPlay24 />}
+        {size === 'medium' ? (
+          <svg
+            width="11"
+            height="14"
+            viewBox="0 0 11 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.5 6.134a1 1 0 0 1 0 1.732l-9 5.196a1 1 0 0 1-1.5-.866V1.804A1 1 0 0 1 1.5.938l9 5.196Z"
+              fill="currentColor"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="17"
+            height="20"
+            viewBox="0 0 17 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.5 9.134a1 1 0 0 1 0 1.732l-15 8.66A1 1 0 0 1 0 18.66V1.34A1 1 0 0 1 1.5.474l15 8.66Z"
+              fill="currentColor"
+            />
+          </svg>
+        )}
       </span>
     </button>
   )
