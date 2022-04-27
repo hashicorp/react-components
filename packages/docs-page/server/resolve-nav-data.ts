5104a50988de87b3f1e266ef477e93248fa245b1
Kevin Wang <kwangsan@gmail.com>
2022-04-27 10:13:38 -0400

    fix(docs-page): internal types

--- packages/docs-page/server/resolve-nav-data.ts
+++ packages/docs-page/server/resolve-nav-data.ts
@@ -2,8 +2,12 @@ import path from 'path'
 import fs from 'fs'
 
 import { validateNavData } from './validate-nav-data'
+import type { NavNode } from 'packages/docs-sidenav/types'
 
-export async function resolveNavData(filePath, localContentDir) {
+export async function resolveNavData(
+  filePath: string,
+  localContentDir: string
+): Promise<NavNode[]> {
   const navDataFile = path.join(process.cwd(), filePath)
   const navDataRaw = JSON.parse(fs.readFileSync(navDataFile, 'utf8'))
   const withFilePaths = await validateNavData(navDataRaw, localContentDir)
