9160171066e97da52eb996a2479803b010544598
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-28 14:46:43 -0400

    chore: rename to CloseButton

--- packages/button-close/types.ts
+++
@@ -1,6 +0,0 @@
-export interface ButtonCloseProps {
-  variant?: 'light' | 'dark'
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-  disabled?: boolean
-  className?: string
-}
