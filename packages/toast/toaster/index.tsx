a9d7a01883de61ea463f233203d81c4bcff8408d
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-29 14:28:42 -0400

    chore: define toaster options

--- packages/toast/toaster/index.tsx
+++ packages/toast/toaster/index.tsx
@@ -1,5 +1,13 @@
 import { Toaster as ReactHotToaster } from 'react-hot-toast'
 
 export default function Toaster() {
-  return <ReactHotToaster position="bottom-left" />
+  return (
+    <ReactHotToaster
+      position="bottom-left"
+      gutter={16}
+      containerStyle={{
+        inset: 24,
+      }}
+    />
+  )
 }
