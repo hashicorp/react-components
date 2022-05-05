b2273a6cd77421cd8a0a8204094f5f69bbf3b9e5
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-05-05 15:32:44 -0400

    chore: remove updates from swingset config

--- pages/[[...swingset]].jsx
+++ pages/[[...swingset]].jsx
@@ -12,8 +12,6 @@ import { Tab } from '../packages/tabs'
 import codeMdxComponents from '../packages/code-block/mdx'
 import UsageDetails from '../swingset-extensions/usage-details'
 import tableStyleWrapper from '../packages/content/styles/table.module.css'
-import Notification from '../packages/notification'
-import toast, { Toaster } from '../packages/toast'
 
 function Logo() {
   return (
@@ -128,9 +126,6 @@ const components = {
   FormikStateViewer,
   NextRouterDebugger,
   ComboboxField, // @TODO - Consider Swingset support for components at nested entry points
-  Toaster,
-  Notification,
-  toast,
 }
 
 export default createPage({ components, logo: <Logo />, index: <Index /> })
