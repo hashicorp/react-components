4856632a34a924082d28ab11472ce45e0bcd3580
Kendall Strautman <kendallstrautman@gmail.com>
2022-05-18 16:52:50 -0700

    refactor; explicitly check for function

--- packages/consent-manager/scripts/segment.tsx
+++ packages/consent-manager/scripts/segment.tsx
@@ -48,7 +48,9 @@ export default function SegmentScript({
     preferences.segment ?? {}
   )
   function onReadyCb() {
-    onAnalyticsReady && onAnalyticsReady()
+    if (typeof onAnalyticsReady == 'function') {
+      onAnalyticsReady()
+    }
     segmentReadyCb()
   }
 
