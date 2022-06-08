af1c073bacdd6adcaa299f8214f583d2bce1a9ab
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-08 14:55:17 -0400

    regenerate lock file and remove dupe folder

--- packages/notifications/index.ts
+++
@@ -1,8 +0,0 @@
-import { notification } from './core/notification'
-
-export { useNotifications } from './core/use-notifications'
-export { Notifications } from './components/notifications'
-export { useStore as useNotificationStore } from './core/store'
-
-export { notification }
-export default notification
