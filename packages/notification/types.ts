98b100604098bf0f46a6c0ddde57cd60e01389ed
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-08 17:19:51 -0400

    move function to render notification

--- packages/notification/types.ts
+++ packages/notification/types.ts
@@ -5,16 +5,6 @@ export type Renderable = JSX.Element | string | null
 export type ValueFunction<TValue, TArg> = (arg: TArg) => TValue
 export type ValueOrFunction<TValue, TArg> = TValue | ValueFunction<TValue, TArg>
 
-const isFunction = <TValue, TArg>(
-  valOrFunction: ValueOrFunction<TValue, TArg>
-): valOrFunction is ValueFunction<TValue, TArg> =>
-  typeof valOrFunction === 'function'
-
-export const resolveValue = <TValue, TArg>(
-  valOrFunction: ValueOrFunction<TValue, TArg>,
-  arg: TArg
-): TValue => (isFunction(valOrFunction) ? valOrFunction(arg) : valOrFunction)
-
 export interface Notification {
   createdAt: number
   id: string
