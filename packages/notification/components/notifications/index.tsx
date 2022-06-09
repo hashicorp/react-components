49b805d857868348173016c6c4cdaadefd975fd2
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-06-09 09:40:21 -0400

    reverts toast and dialog types

--- packages/notification/components/notifications/index.tsx
+++ packages/notification/components/notifications/index.tsx
@@ -4,34 +4,6 @@ import { renderNotification } from '../../core/utils'
 import { useNotifications } from '../../core/use-notifications'
 import s from './style.module.css'
 
-interface HTMLDialogElement extends HTMLElement {
-  open: boolean
-  returnValue: string
-  close(returnValue?: string): void
-  show(): void
-  showModal(): void
-}
-
-const Dialog = ({ children }) => {
-  const dialogRef = React.useRef<HTMLDialogElement>(null)
-
-  React.useEffect(() => {
-    const dialog = dialogRef.current
-    // Opens the dialog on mount
-    dialog?.show()
-    // Closes the dialog on unmount (if not already closed)
-    return () => {
-      dialog?.close()
-    }
-  }, [])
-
-  return (
-    <dialog ref={dialogRef} aria-modal={false} style={{ all: 'unset' }}>
-      {children}
-    </dialog>
-  )
-}
-
 export const Notifications = () => {
   const { notifications, handlers } = useNotifications()
   return (
@@ -54,13 +26,7 @@ export const Notifications = () => {
                 transition: { duration: 0.2 },
               }}
             >
-              {n.type === 'toast' ? (
-                <div role="status" aria-live="polite">
-                  {renderNotification(n.message, n)}
-                </div>
-              ) : (
-                <Dialog>{renderNotification(n.message, n)}</Dialog>
-              )}
+              {renderNotification(n.message, n)}
             </motion.div>
           )
         })}
