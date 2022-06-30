90011d75492356b413e01c3f033a00d986f2aa3e
Pamela Bortnick <pbortnick@gmail.com>
2022-06-30 14:37:36 -0400

    Add props file

--- packages/tier-card/index.tsx
+++ packages/tier-card/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import Image from 'next/image'
-import classNames from 'classnames'
+import Link from 'next/link'
 import type { TierCardProps } from './types'
 import s from './style.module.css'
+import Button from 'packages/button'
 
 export default function TierCard({
   icon,
@@ -15,5 +15,23 @@ export default function TierCard({
   supplementaryInfo,
   size,
 }: TierCardProps): React.ReactElement {
-  return <div className={s.root} />
+  return (
+    <div className={s.root}>
+      {icon && <div className={s.icon}>{icon}</div>}
+      <h2 className={s.tierName}>{title}</h2>
+      <div className={s.details}>
+        <span className={s.label}>{label}</span>
+        <span className={s.price}>{price}</span>
+        <span className={s.consumption}>{consumption}</span>
+      </div>
+      <p className={s.description}>{description}</p>
+      <div className={s.cta}>
+        {cta.type === 'button' ? (
+          <Button url={cta.url} title={cta.title} theme={cta.theme} />
+        ) : (
+          <Link href={cta.url}>{cta.title}</Link>
+        )}
+      </div>
+    </div>
+  )
 }
