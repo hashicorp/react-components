bb270a8cbd809bff76d3f81ab7ed9c8a0a8a1b8d
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-07-01 15:47:40 -0700

    feat: add API helpers

--- packages/marketo-form/server/api-routes.ts
+++ packages/marketo-form/server/api-routes.ts
@@ -1,34 +1,13 @@
-import { URL } from 'url'
-import createFetch from '@vercel/fetch'
+import * as client from './client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const fetch = createFetch()
-
-interface MarketoTokenResponse {
-  access_token: string
-}
-
-async function getToken(): Promise<MarketoTokenResponse> {
-  const url = new URL(`${process.env.MARKETO_IDENTITY}/oauth/token`)
-  url.searchParams.append('grant_type', 'client_credentials')
-  url.searchParams.append('client_id', process.env.MARKETO_CLIENT_ID!)
-  url.searchParams.append('client_secret', process.env.MARKETO_CLIENT_SECRET!)
-  const res = await fetch(url.toString())
-  return await res.json()
+function flatten(param: string | string[]): string {
+  return Array.isArray(param) ? param[0] : param
 }
 
 async function getForm(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { access_token } = await getToken()
-    const marketoRes = await fetch(
-      `${process.env.MARKETO_ENDPOINT}/asset/v1/form/${req.query.form}/fields.json`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_token}`,
-        },
-      }
-    )
+    const marketoRes = await client.getForm(flatten(req.query.form))
     const form = await marketoRes.json()
     return res.status(marketoRes.status).json(form)
   } catch (err) {
@@ -39,18 +18,7 @@ async function getForm(req: NextApiRequest, res: NextApiResponse) {
 
 async function submitForm(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { access_token } = await getToken()
-    const marketoRes = await fetch(
-      `${process.env.MARKETO_ENDPOINT}/v1/leads/submitForm.json`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_token}`,
-        },
-        body: JSON.stringify(req.body),
-      }
-    )
+    const marketoRes = await client.submitForm(req.body)
     const form = await marketoRes.json()
     return res.status(marketoRes.status).json(form)
   } catch (err) {
