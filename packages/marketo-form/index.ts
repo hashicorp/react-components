f2cebb5c0379e2f84b5edb59717a840efae10118
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-07-01 15:27:48 -0700

    feat: add support for NameField

--- packages/marketo-form/index.ts
+++ packages/marketo-form/index.ts
@@ -1,3 +1,13 @@
 import Form from './form'
+import NameField from './partials/fields/name-field'
+
+const defaultFieldGroupings = {
+  name: {
+    fields: ['FirstName', 'LastName'],
+    component: NameField,
+  },
+}
+
+export { defaultFieldGroupings }
 
 export default Form
