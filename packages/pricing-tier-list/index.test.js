06ef8e2c3264b61fbdbfe1adc4d5e43ffef64773
Pamela Bortnick <pbortnick@gmail.com>
2022-07-12 15:55:39 -0400

    Update tests

--- packages/pricing-tier-list/index.test.js
+++ packages/pricing-tier-list/index.test.js
@@ -20,22 +20,16 @@ describe('<PricingTierList />', () => {
   it('should have the correct className and sized cards with 3 tiers', () => {
     const { container } = render(<PricingTierList tiers={threeTiers} />)
     const listElement = container.querySelector('.length3')
-    const cardElement = container.querySelector('.mdSize')
     expect(listElement).toBeInTheDocument()
-    expect(cardElement).toBeInTheDocument()
   })
   it('should have the correct className and sized cards with 4 tiers', () => {
     const { container } = render(<PricingTierList tiers={fourTiers} />)
     const listElement = container.querySelector('.length4')
-    const cardElement = container.querySelector('.smSize')
     expect(listElement).toBeInTheDocument()
-    expect(cardElement).toBeInTheDocument()
   })
   it('should have the correct className and sized cards with 5 tiers', () => {
     const { container } = render(<PricingTierList tiers={fiveTiers} />)
     const listElement = container.querySelector('.length5')
-    const cardElement = container.querySelector('.xsSize')
     expect(listElement).toBeInTheDocument()
-    expect(cardElement).toBeInTheDocument()
   })
 })
