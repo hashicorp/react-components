9befda5c7a358395390fd48f323e01c4c13ee102
Pamela Bortnick <pbortnick@gmail.com>
2022-07-12 15:00:09 -0400

    Use fragment to align each row

--- packages/pricing-tier-list/index.tsx
+++ packages/pricing-tier-list/index.tsx
@@ -1,15 +1,9 @@
 import classNames from 'classnames'
-import PricingTierItem from './partials/pricing-tier-item'
 import { PricingTierListProps } from './types'
 import s from './style.module.css'
-
-const CARD_SIZES = {
-  1: 'lg',
-  2: 'lg',
-  3: 'md',
-  4: 'sm',
-  5: 'xs',
-}
+import Button from '@hashicorp/react-button'
+import Link from 'next/link'
+import React from 'react'
 
 export default function PricingTierList({ tiers }: PricingTierListProps) {
   const tiersLength = tiers.length
@@ -24,13 +18,75 @@ export default function PricingTierList({ tiers }: PricingTierListProps) {
           } as React.CSSProperties
         }
       >
-        {tiers.map((tier) => (
-          <PricingTierItem
-            {...tier}
-            key={tier.title}
-            size={CARD_SIZES[tiersLength]}
-          />
-        ))}
+        {tiers.map(
+          (
+            {
+              icon,
+              title,
+              label,
+              price,
+              consumption,
+              description,
+              cta,
+              supplementaryInfo,
+            },
+            colIdx
+          ) => {
+            const _columnIndex = colIdx + 1
+            return (
+              <React.Fragment key={title}>
+                <div
+                  className={s.header}
+                  style={
+                    {
+                      '--column-index': _columnIndex,
+                    } as React.CSSProperties
+                  }
+                >
+                  {icon && <div className={s.icon}>{icon}</div>}
+                  <h3 className={s.tierName}>{title}</h3>
+                  <div className={s.details}>
+                    {label && <span className={s.label}>{label}</span>}
+                    {price && <span className={s.price}>{price}</span>}
+                    {consumption && (
+                      <span className={s.consumption}>{consumption}</span>
+                    )}
+                  </div>
+                </div>
+                <div
+                  className={s.description}
+                  dangerouslySetInnerHTML={{ __html: description }}
+                  style={
+                    {
+                      '--column-index': _columnIndex,
+                    } as React.CSSProperties
+                  }
+                />
+                <div className={s.bottom}>
+                  <div className={s.cta}>
+                    {cta.type === 'button' && tiersLength < 5 ? (
+                      <Button
+                        url={cta.url}
+                        title={cta.title}
+                        theme={cta.theme}
+                      />
+                    ) : (
+                      <Link href={cta.url}>
+                        <a className={s.textLink}>{cta.title}</a>
+                      </Link>
+                    )}
+                  </div>
+                  {supplementaryInfo && (
+                    <div
+                      className={s.supplementaryInfo}
+                      dangerouslySetInnerHTML={{ __html: supplementaryInfo }}
+                    />
+                  )}
+                </div>
+              </React.Fragment>
+            )
+          }
+        )}
       </div>
     </div>
   )
