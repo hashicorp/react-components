cae32dece35fa2b3944eec85e760710eb73f90f8
Pamela Bortnick <pbortnick@gmail.com>
2022-07-19 16:00:35 -0400

    Add error test

--- packages/pricing-tier-list/index.test.js
+++ packages/pricing-tier-list/index.test.js
@@ -1,32 +1,48 @@
-import { render, cleanup } from '@testing-library/react'
+import { screen, render, cleanup } from '@testing-library/react'
 import PricingTierList from '.'
-import props from './fixtures/data.json'
+import tiersData from './fixtures/tiersData.json'
 
-const { oneTier, twoTiers, threeTiers, fourTiers, fiveTiers } = props
+const { oneTier, twoTiers, threeTiers, fourTiers, fiveTiers } = tiersData
 
 afterEach(cleanup)
 
 describe('<PricingTierList />', () => {
+  it('should render', () => {
+    render(<PricingTierList tiers={twoTiers} />)
+    const element = screen.getByTestId('pricing-tier-list')
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should throw error when tiers length more than maximum', () => {
+    expect(() =>
+      render(<PricingTierList tiers={[...threeTiers, ...fiveTiers]} />)
+    ).toThrow('<PricingTierList /> only supports up to five tiers')
+  })
+
   it('should have the correct className with 1 tier', () => {
     const { container } = render(<PricingTierList tiers={oneTier} />)
     const listElement = container.querySelector('.length1')
     expect(listElement).toBeInTheDocument()
   })
+
   it('should have the correct className with 2 tiers', () => {
     const { container } = render(<PricingTierList tiers={twoTiers} />)
     const listElement = container.querySelector('.length2')
     expect(listElement).toBeInTheDocument()
   })
+
   it('should have the correct className and sized cards with 3 tiers', () => {
     const { container } = render(<PricingTierList tiers={threeTiers} />)
     const listElement = container.querySelector('.length3')
     expect(listElement).toBeInTheDocument()
   })
+
   it('should have the correct className and sized cards with 4 tiers', () => {
     const { container } = render(<PricingTierList tiers={fourTiers} />)
     const listElement = container.querySelector('.length4')
     expect(listElement).toBeInTheDocument()
   })
+
   it('should have the correct className and sized cards with 5 tiers', () => {
     const { container } = render(<PricingTierList tiers={fiveTiers} />)
     const listElement = container.querySelector('.length5')
