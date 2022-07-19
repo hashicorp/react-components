ca1c02823230f5e3e9be256eaa175036d129c5b5
Pamela Bortnick <pbortnick@gmail.com>
2022-07-19 12:44:12 -0400

    Fix map and add readme

--- packages/pricing-feature-tabs/index.tsx
+++ packages/pricing-feature-tabs/index.tsx
@@ -30,12 +30,43 @@ export default function PricingFeatureTabs({
       onChange={handleTabChange}
       orientation={TabsOrientation.Horizontal}
     >
-      <AnimateSharedLayout>
-        <TabList
-          className={classNames(s.tabList, {
-            [s.smList]: featuresLength < 4,
-            [s.mdList]: featuresLength >= 4 && featuresLength <= 5,
-            [s.lgList]: featuresLength > 5,
+      <TabList
+        className={classNames(s.tabList, {
+          [s.smList]: featuresLength < 4,
+          [s.mdList]: featuresLength >= 4 && featuresLength <= 5,
+          [s.lgList]: featuresLength > 5,
+        })}
+        style={
+          {
+            '--col': featuresLength,
+          } as React.CSSProperties
+        }
+      >
+        {features.map(({ tabLabel }, index) => (
+          <Tab
+            className={classNames(s.tab, {
+              [s.active]: index === activeTabIndex,
+            })}
+            key={tabLabel.feature}
+          >
+            <div className={s.iconWrapper}>{tabLabel.icon}</div>
+            <span className={s.featureText}>{tabLabel.feature}</span>
+          </Tab>
+        ))}
+      </TabList>
+      <div className={s.tabPanels}>
+        <TabPanels>
+          {features.map((item, index) => {
+            return (
+              <TabPanel
+                key={item.tabLabel.feature}
+                className={classNames({
+                  [s.fadeIn]: index === activeTabIndex,
+                })}
+              >
+                {item.tabContent}
+              </TabPanel>
+            )
           })}
           style={
             {
