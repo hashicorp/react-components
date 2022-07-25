c0636c669a7bf1dcef94bcffd5e09fec36216e00
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-07-25 15:45:12 -0400

    Update commands

--- packages/product-download-page/index.test.js
+++ packages/product-download-page/index.test.js
@@ -46,7 +46,7 @@ describe('<ProductDownloadsPage />', () => {
       expect(
         getByText(
           downloadCards,
-          'curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-releases.gpg'
+          'curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o	/usr/share/keyrings/hashicorp.gpg'
         )
       ).toBeInTheDocument()
 
--- packages/product-download-page/package-managers.ts
+++ packages/product-download-page/package-managers.ts
@@ -15,8 +15,8 @@ export function generateDefaultPackageManagers(
     {
       label: 'Ubuntu/Debian',
       commands: [
-        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-releases.gpg`,
-        `echo "deb [signed-by=/usr/share/keyrings/hashicorp-releases.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg`,
+        `echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
         `sudo apt update && sudo apt install ${productSlug}`,
       ],
       os: 'linux',
@@ -66,8 +66,8 @@ export function generateEnterprisePackageManagers(
     {
       label: 'Ubuntu/Debian',
       commands: [
-        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-releases.gpg`,
-        `echo "deb [signed-by=/usr/share/keyrings/hashicorp-releases.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg`,
+        `echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
         `sudo apt update && sudo apt install ${productSlug}-enterprise`,
       ],
       os: 'linux',
