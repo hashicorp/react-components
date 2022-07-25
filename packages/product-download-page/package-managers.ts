64f86278585decd8a4962422960197c5a9610a1b
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-07-25 15:42:33 -0400

    Update commands

--- packages/product-download-page/package-managers.ts
+++ packages/product-download-page/package-managers.ts
@@ -1,103 +1,103 @@
 import { PackageManagerConfig } from './'
 
 export function generateDefaultPackageManagers(
-  productSlug: string
+	productSlug: string
 ): PackageManagerConfig[] {
-  return [
-    {
-      label: 'Homebrew',
-      commands: [
-        `brew tap hashicorp/tap`,
-        `brew install hashicorp/tap/${productSlug}`,
-      ],
-      os: 'darwin',
-    },
-    {
-      label: 'Ubuntu/Debian',
-      commands: [
-        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-releases.gpg`,
-        `echo "deb [signed-by=/usr/share/keyrings/hashicorp-releases.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
-        `sudo apt update && sudo apt install ${productSlug}`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'CentOS/RHEL',
-      commands: [
-        `sudo yum install -y yum-utils`,
-        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
-        `sudo yum -y install ${productSlug}`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'Fedora',
-      commands: [
-        `sudo dnf install -y dnf-plugins-core`,
-        `sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
-        `sudo dnf -y install ${productSlug}`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'Amazon Linux',
-      commands: [
-        `sudo yum install -y yum-utils`,
-        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
-        `sudo yum -y install ${productSlug}`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'Homebrew',
-      commands: [
-        `brew tap hashicorp/tap`,
-        `brew install hashicorp/tap/${productSlug}`,
-      ],
-      os: 'linux',
-    },
-  ]
+	return [
+		{
+			label: 'Homebrew',
+			commands: [
+				`brew tap hashicorp/tap`,
+				`brew install hashicorp/tap/${productSlug}`,
+			],
+			os: 'darwin',
+		},
+		{
+			label: 'Ubuntu/Debian',
+			commands: [
+				`curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg`,
+				`echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+				`sudo apt update && sudo apt install ${productSlug}`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'CentOS/RHEL',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Fedora',
+			commands: [
+				`sudo dnf install -y dnf-plugins-core`,
+				`sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
+				`sudo dnf -y install ${productSlug}`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Amazon Linux',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Homebrew',
+			commands: [
+				`brew tap hashicorp/tap`,
+				`brew install hashicorp/tap/${productSlug}`,
+			],
+			os: 'linux',
+		},
+	]
 }
 
 export function generateEnterprisePackageManagers(
-  productSlug: string
+	productSlug: string
 ): PackageManagerConfig[] {
-  return [
-    {
-      label: 'Ubuntu/Debian',
-      commands: [
-        `curl -sSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-releases.gpg`,
-        `echo "deb [signed-by=/usr/share/keyrings/hashicorp-releases.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
-        `sudo apt update && sudo apt install ${productSlug}-enterprise`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'CentOS/RHEL',
-      commands: [
-        `sudo yum install -y yum-utils`,
-        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
-        `sudo yum -y install ${productSlug}-enterprise`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'Fedora',
-      commands: [
-        `sudo dnf install -y dnf-plugins-core`,
-        `sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
-        `sudo dnf -y install ${productSlug}-enterprise`,
-      ],
-      os: 'linux',
-    },
-    {
-      label: 'Amazon Linux',
-      commands: [
-        `sudo yum install -y yum-utils`,
-        `sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
-        `sudo yum -y install ${productSlug}-enterprise`,
-      ],
-      os: 'linux',
-    },
-  ]
+	return [
+		{
+			label: 'Ubuntu/Debian',
+			commands: [
+				`curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg`,
+				`echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list`,
+				`sudo apt update && sudo apt install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'CentOS/RHEL',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Fedora',
+			commands: [
+				`sudo dnf install -y dnf-plugins-core`,
+				`sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo`,
+				`sudo dnf -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+		{
+			label: 'Amazon Linux',
+			commands: [
+				`sudo yum install -y yum-utils`,
+				`sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo`,
+				`sudo yum -y install ${productSlug}-enterprise`,
+			],
+			os: 'linux',
+		},
+	]
 }
