b1dc712ad9675bfe434871d91b3563fb4f8a7a8c
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-08-15 14:21:48 -0400

    feat: Prepare Card JSX

--- packages/card/index.tsx
+++ packages/card/index.tsx
@@ -1,21 +1,70 @@
 import type { CardProps } from './types'
 import classNames from 'classnames'
 import s from './style.module.css'
+import Button from 'packages/button'
 
 export default function Card({
   children,
   appearance,
   featured,
-  thumbnail,
-  meta,
   heading,
   description,
-  badges,
   cta,
 }: CardProps) {
   return (
-    <a href={cta.url} className={classNames(s.card, s[appearance])}>
+    <a
+      href={cta.url}
+      className={classNames(s.card, s[appearance], featured && s.featured)}
+    >
       {children}
+      {featured ? null : (
+        <>
+          <p className={s.heading}>{heading}</p>
+          {description ? <p className={s.description}>{description}</p> : null}
+        </>
+      )}
+
+      <Button title={cta.text || ''} url={cta.url} linkType="inbound" />
     </a>
   )
+}
+
+export function Thumbnail({ url }) {
+  const isVideo = ['mp4', 'webm'].includes(parseFormat(url))
+
+  return (
+    <div className={s.thumbnail}>
+      {isVideo ? <div>Play button</div> : null}
+      JSX for the thumbnail
+    </div>
+  )
+}
+
+export function Meta({ date, category }) {
+  const meta = date && category ? `${date} | ${category}` : date || category
+
+  return <div className={s.meta}>{meta}</div>
+}
+
+export function Badges({ badges }) {
+  return (
+    <ul className={s.badges}>
+      {badges.map(({ label, product }) => {
+        return (
+          <li key={label}>
+            <span>{product}</span>
+            <span>{label}</span>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+// DUPLICATE FUNCTION
+// Lifted from packages/image
+function parseFormat(url) {
+  const extensionMatch = url.match(/\.(\w+)$/)
+  const hasExtension = !!extensionMatch && !!extensionMatch[1]
+  return hasExtension ? extensionMatch[1] : false
 }
--- packages/card/types.ts
+++ packages/card/types.ts
@@ -4,19 +4,28 @@ export interface CardProps {
   children: string
   featured?: boolean
   appearance: 'light' | 'dark'
-  meta?: {
-    date?: string
-    category?: string
-  }
   heading: string
   description?: string
-  thumbnail?: $TSFixMe
-  badges?: Array<{
-    label: string
-    product: Products
-  }>
   cta: {
     text?: string
     url: string
+  }
+}
+
+export interface ThumbnailProps {
+  url: string
+}
+
+export interface BadgesProps {
+  badges: Array<{
+    label: string
+    product: Products
+  }>
+}
+
+export interface MetaProps {
+  meta: {
+    date?: string
+    category?: string
   }
 }
