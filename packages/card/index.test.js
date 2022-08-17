3464c85c12136a342ee8ff465cccdf46960d1263
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-08-17 16:00:06 -0400

    fix: add required 'link' prop to tests
    also, use grid for examples, and force card to grow in flex containers

--- packages/card/index.test.js
+++ packages/card/index.test.js
@@ -4,18 +4,24 @@ import Card from '.'
 describe('<Card />', () => {
   it('should render the provided React children', () => {
     const cardText = 'Children text'
-    const { container } = render(<Card>{cardText}</Card>)
+    const { container } = render(
+      <Card link="https://hashicorp.com">{cardText}</Card>
+    )
     expect(container).toHaveTextContent(cardText)
   })
 
   it('should render the default (light) variant', () => {
-    const { container } = render(<Card heading="Heading" />)
+    const { container } = render(
+      <Card link="https://hashicorp.com" heading="Heading" />
+    )
     expect(container).toHaveClass('card', 'light')
     expect(container).not.toHaveClass('dark')
   })
 
   it('should render the dark variant', () => {
-    const { container } = render(<Card appearance="dark" heading="Heading" />)
+    const { container } = render(
+      <Card appearance="dark" link="https://hashicorp.com" heading="Heading" />
+    )
     expect(container).toHaveClass('card', 'dark')
     expect(container).not.toHaveClass('light')
   })
