3ca66e5eda30375b257c3d08a542252671d26e9e
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-09-13 10:17:04 -0400

    chore: add tests

--- packages/standalone-link/index.test.js
+++ packages/standalone-link/index.test.js
@@ -1,4 +1,26 @@
 import { render, screen } from '@testing-library/react'
 import StandaloneLink from '.'
 
-describe('<StandaloneLink />', () => {})
+const defaultProps = {
+  href: 'https://hashicorp.com',
+  children: 'Call-to-action',
+}
+
+describe('<StandaloneLink />', () => {
+  it('should render with default props', () => {
+    render(<StandaloneLink {...defaultProps} />)
+    const linkText = screen.getByText(defaultProps.children)
+    const linkElement = linkText.closest('a')
+    expect(linkElement).toHaveAttribute('href', defaultProps.href)
+    expect(linkElement).toHaveClass('root', 'primary')
+    expect(linkElement).not.toHaveClass('secondary', 'tertiary')
+  })
+
+  it('should render the correct themed variant', () => {
+    render(<StandaloneLink {...defaultProps} variant="secondary" />)
+    const linkText = screen.getByText(defaultProps.children)
+    const linkElement = linkText.closest('a')
+    expect(linkElement).toHaveClass('root', 'secondary')
+    expect(linkElement).not.toHaveClass('primary', 'tertiary')
+  })
+})
