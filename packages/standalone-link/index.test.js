5884e72d086a2851ddc65796c881f88a5d42fe19
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-09-19 14:34:11 -0400

    feat: `<StandaloneLink />` component (#723)
    * chore: initial structure
    
    * feat: add animation
    
    * refactor: animate scale x
    
    * chore: flesh out types and props data
    
    * chore: add tests
    
    * chore: add onClick test
    
    * refactor: extend JSX.IntrinsicElements['a']
    
    * chore: install packages
    
    * chore: add outline styling
    
    * refactor: outline to use focus-visible
    
    * chore: conver line-height to unitless value
    
    * chore: cleanup props file:
    
    * chore: add period to description
    
    * chore: add rules and ux writing guidelines
    
    * refactor: use theme
    
    * fix: variant to theme test
    
    * chore: add changeset
    
    * fix: dark hover states
    
    * chore: reduce gap and timing duration
    
    * chore add animation to color and fill
    
    * chore: tweak animations
    
    * chore: reduce timing function

---
+++ packages/standalone-link/index.test.js
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import StandaloneLink from '.'
+
+const defaultProps = {
+  href: 'https://hashicorp.com',
+  children: 'Call-to-action',
+}
+
+describe('<StandaloneLink />', () => {
+  it('should render with default props', () => {
+    render(<StandaloneLink {...defaultProps} />)
+    const linkText = screen.getByText(defaultProps.children)
+    const linkElement = linkText.closest('a')
+    expect(linkElement).toHaveAttribute('href', defaultProps.href)
+    expect(linkElement).toHaveClass('root', 'primary')
+    expect(linkElement).not.toHaveClass('secondary', 'tertiary')
+  })
+
+  it('should render the correct theme', () => {
+    render(<StandaloneLink {...defaultProps} theme="secondary" />)
+    const linkText = screen.getByText(defaultProps.children)
+    const linkElement = linkText.closest('a')
+    expect(linkElement).toHaveClass('root', 'secondary')
+    expect(linkElement).not.toHaveClass('primary', 'tertiary')
+  })
+
+  it('should call onClick function', () => {
+    const mockOnClick = jest.fn()
+    render(<StandaloneLink {...defaultProps} onClick={mockOnClick} />)
+    const linkElement = screen.getByText(defaultProps.children).closest('a')
+    fireEvent.click(linkElement)
+    expect(mockOnClick).toHaveBeenCalled()
+  })
+})
