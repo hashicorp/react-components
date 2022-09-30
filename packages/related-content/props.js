cd3c836fb3acb073586a3202d2c3a7c1ca7973c2
Nels Andereck <nels.andereck@hashicorp.com>
2022-09-30 10:26:58 -0700

    update props and docs

--- packages/related-content/props.js
+++ packages/related-content/props.js
@@ -11,7 +11,7 @@ const baseProps = {
   cards: {
     type: 'array',
     required: true,
-    description: 'See <Card /> below in Sub-component-specific Props',
+    description: `See props for the Card component <a href="https://react-components.vercel.app/components/card">here</a>`,
   },
   cta: {
     type: 'string',
@@ -24,62 +24,6 @@ const baseProps = {
   },
 }
 
-const cardProps = {
-  children: {
-    type: 'function',
-    description: 'Children to render into the Card.',
-    required: true,
-  },
-  thumbnail: {
-    type: 'object',
-    description: "An image showcasing the card's destination.",
-    properties: {
-      src: {
-        description: 'URL for the image',
-        type: 'string',
-        control: { type: 'input' },
-        required: true,
-      },
-      alt: {
-        description: 'Alternative text for the image',
-        type: 'string',
-        control: { type: 'input' },
-        required: false,
-      },
-    },
-  },
-  meta: {
-    type: 'array',
-    description: 'Content shown above the card heading.',
-    required: false,
-    testValue: ['August 15, 2022', 'Category'],
-  },
-  appearance: {
-    description: 'Styles the card with either a light or dark theme.',
-    type: 'string',
-    required: false,
-    options: ['light', 'dark'],
-    testValue: 'light',
-  },
-  heading: {
-    description: "A bold headline describing the card's destination.",
-    type: 'string',
-    required: false,
-  },
-  description: {
-    description: "Text describing the card's destination.",
-    type: 'string',
-    required: false,
-  },
-  link: {
-    type: 'string',
-    description: 'A link followed when the card is clicked.',
-    required: true,
-    testValue: 'https://hashicorp.com',
-  },
-}
-
 module.exports = {
   baseProps,
-  cardProps,
 }
