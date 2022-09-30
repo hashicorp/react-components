c4ff8d4c7676a607cb8e3ad829e7d5cee88b51c8
Nels Andereck <nels.andereck@hashicorp.com>
2022-09-30 10:31:55 -0700

    use standalonelinkprops

--- packages/related-content/types.ts
+++ packages/related-content/types.ts
@@ -1,13 +1,9 @@
 import type { CardProps } from '@hashicorp/react-card/types'
-import type { ThemeVariant } from '@hashicorp/react-button/types'
+import type { StandaloneLinkProps } from '@hashicorp/react-standalone-link/types'
 export interface RelatedContentProps {
   appearance?: 'light' | 'dark'
   headline: string
   description?: string
   cards: Array<CardProps>
-  cta?: {
-    title: string
-    url: string
-    variant: ThemeVariant
-  }
+  cta?: StandaloneLinkProps
 }
