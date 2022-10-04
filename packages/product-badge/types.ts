fc95afa8a7bdc218c4625887d34525277e972f25
Nels Andereck <nels.andereck@hashicorp.com>
2022-10-04 11:00:39 -0700

    update prop names

--- packages/product-badge/types.ts
+++ packages/product-badge/types.ts
@@ -3,6 +3,6 @@ import type { Products } from '@hashicorp/platform-product-meta'
 export interface ProductBadgeProps {
   productName: Products
   appearance?: 'light' | 'dark'
-  variant?: 'primary' | 'secondary'
+  theme?: 'primary' | 'secondary'
   hasDot?: boolean
 }
