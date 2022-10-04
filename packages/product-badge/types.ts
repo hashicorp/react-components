f4b5618e35558e5ef83563b521c89575db2204c4
Nels Andereck <nels.andereck@hashicorp.com>
2022-10-04 10:58:25 -0700

    make hasDot a prop

--- packages/product-badge/types.ts
+++ packages/product-badge/types.ts
@@ -3,5 +3,6 @@ import type { Products } from '@hashicorp/platform-product-meta'
 export interface ProductBadgeProps {
   productName: Products
   appearance?: 'light' | 'dark'
-  variant?: 'primary' | 'secondary' | 'hasDot'
+  variant?: 'primary' | 'secondary'
+  hasDot?: boolean
 }
