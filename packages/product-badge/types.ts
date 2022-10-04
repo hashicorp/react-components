41f5899d8f7555015a81306f06e14426baa0e586
Nels Andereck <nels.andereck@hashicorp.com>
2022-10-03 22:21:04 -0700

    add variants to types

--- packages/product-badge/types.ts
+++ packages/product-badge/types.ts
@@ -3,4 +3,5 @@ import type { Products } from '@hashicorp/platform-product-meta'
 export interface ProductBadgeProps {
   productName: Products
   appearance?: 'light' | 'dark'
+  variant?: 'primary' | 'secondary' | 'hasDot'
 }
