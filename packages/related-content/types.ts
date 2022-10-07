2a26b0bf09bc94fbee4607ae7a2b321bfb99a829
Nels Andereck <39201593+nandereck@users.noreply.github.com>
2022-10-07 10:08:26 -0700

    feat: `<RelatedContent />` component (#731)
    🎟️ [Asana Task](https://app.asana.com/0/1202802428287999/1203075725709007/f)
    🔍 [Preview Link](https://react-components-git-nels-related-contentcreat-fb7ceb-hashicorp.vercel.app/components/relatedcontent)
    
    ---
    
    ## Description
    
    Adds new `<RelatedContent />` component. One thing to note is that this does not include the initially proposed dropdown. Instead, we've replaced with a CTA (using the `<StandaloneLink />`).
    
    [Figma](https://www.figma.com/file/VvpEQaWhKQExx9QTWRyayd/Patterns?node-id=1085%3A11715)
    
    ### PR Checklist 🚀
    
    Items in this checklist may not may not apply to your PR, but please consider each item carefully.
    
    - [x] Add Asana and Preview links above.
    - [x] Conduct thorough self-review.
    - [x] Add or update tests as appropriate.
    - [x] Conduct reasonable cross browser testing for both compatibility and responsive behavior (We have a [Sauce Labs](https://app.saucelabs.com/) account for this, if you don't have access, just ask!).
    - [ ] Conduct reasonable accessibility review (use the [WAS](https://accessible.org/Web-Accessibility-Standards-WAS-2.pdf) as a guide or an [axe browser plugin](https://www.deque.com/axe/) until we establish more formal checks).
    - [ ] Identify (in the description above) and document (add Asana tasks on [this board](https://app.asana.com/0/1100423001970639/list)) any technical debt that you're aware of, but are not addressing as part of this PR.

---
+++ packages/related-content/types.ts
@@ -0,0 +1,16 @@
+import type { CardProps } from '@hashicorp/react-card/types'
+import type { StandaloneLinkProps } from '@hashicorp/react-standalone-link/types'
+export interface RelatedContentProps {
+  appearance?: 'light' | 'dark'
+  headline: string
+  description?: string
+  cards: Array<RelatedContentCardProps>
+  cta: RelatedContentCtaProps
+}
+
+interface RelatedContentCtaProps
+  extends Omit<StandaloneLinkProps, 'children' | 'appearance'> {
+  text: string
+}
+
+type RelatedContentCardProps = Omit<CardProps, 'appearance'>
