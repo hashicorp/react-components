66b39f8d72f403e7dd84e60cb71d0cdd9cd2cc9b
Nels Andereck <39201593+nandereck@users.noreply.github.com>
2022-10-21 10:57:38 -0700

    feat: `<ProductsUsed />` component (#767)
    🎟️ [Asana Task](https://app.asana.com/0/1202802428287999/1203075725709005/f)
    🔍 [Preview Link](https://react-components-git-nels-products-usedcreate-f74ae8-hashicorp.vercel.app/components/productsused)
    🎨 [Figma](https://www.figma.com/file/VvpEQaWhKQExx9QTWRyayd/Patterns?node-id=677%3A4054)
    
    ---
    
    <!-- Reminder: This is an open source project, make sure not to include any sensitive information in the pull request. -->
    
    ## Description
    
    Adds new `<ProductsUsed />` component:
    - Bumps `mktg-global-styles` so as to utilize spacing tokens
    - Adds docs, props, types and component files for `ProductsUsed`
    
    ## Validation Steps
    - Pull down branch and run `npm run test:watch`
    - Verify no failed tests
    - Navigate to the [component in Preview](https://react-components-git-nels-products-usedcreate-f74ae8-hashicorp.vercel.app/components/productsused)
    - Verify component [matches the spec](https://www.figma.com/file/VvpEQaWhKQExx9QTWRyayd/Patterns?node-id=677%3A4054)
    
    
    ### PR Checklist 🚀
    
    Items in this checklist may not may not apply to your PR, but please consider each item carefully.
    
    - [ ] Add Asana and Preview links above.
    - [ ] Conduct thorough self-review.
    - [ ] Add or update tests as appropriate.
    - [ ] Conduct reasonable cross browser testing for both compatibility and responsive behavior (We have a [Sauce Labs](https://app.saucelabs.com/) account for this, if you don't have access, just ask!).
    - [ ] Conduct reasonable accessibility review (use the [WAS](https://accessible.org/Web-Accessibility-Standards-WAS-2.pdf) as a guide or an [axe browser plugin](https://www.deque.com/axe/) until we establish more formal checks).
    - [ ] Identify (in the description above) and document (add Asana tasks on [this board](https://app.asana.com/0/1100423001970639/list)) any technical debt that you're aware of, but are not addressing as part of this PR.

---
+++ packages/products-used/props.js
@@ -0,0 +1,35 @@
+module.exports = {
+  appearance: {
+    type: 'string',
+    description: 'Display on light or dark backgrounds.',
+    options: ['light', 'dark'],
+  },
+  products: {
+    type: 'object',
+    description:
+      'Array of products or products with links. Only supports one or the other, not a mix of the two.',
+    properties: {
+      name: {
+        description:
+          'A lower-case product identifier to render the name of the product',
+        type: 'string',
+        control: { type: 'select' },
+        options: [
+          'boundary',
+          'consul',
+          'nomad',
+          'packer',
+          'terraform',
+          'vault',
+          'vagrant',
+          'waypoint',
+        ],
+        required: true,
+      },
+      href: {
+        type: 'string',
+        description: 'Where the anchor element links to when clicked.',
+      },
+    },
+  },
+}
