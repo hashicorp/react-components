63972972781a923abcafbf5873b67a0c845e2530
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-10-25 12:01:14 -0400

    chore: Remove unused wrapper

--- packages/card/event-card/index.tsx
+++ packages/card/event-card/index.tsx
@@ -23,44 +23,35 @@ export function EventCard({
 
   return (
     <Card heading={title} link={link} appearance={appearance} withArrow={false}>
-      <div className={s.eventCardInner}>
-        <Card.Content>
-          <Card.Meta items={[eventDate, eventType]}></Card.Meta>
-          <Card.Heading>{title}</Card.Heading>
-          {productBadges && productBadges?.length > 0 ? (
-            <Card.ProductBadges
-              badges={productBadges}
-              appearance={appearance}
-            />
-          ) : null}
-          <span className={s.pseudoCta}>
-            <span className={s.pseudoCtaLabel}>{ctaText}</span>
-            <span className={s.pseudoCtaIcon}>
-              <svg
-                className={s.pseudoCtaIconArrow}
-                width="9"
-                height="10"
-                viewBox="0 0 9 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                focusable={false}
-              >
-                <path
-                  d="M4 1L8 5L4 9"
-                  stroke="currentColor"
-                  strokeWidth={1.8}
-                />
-                <path
-                  className={s.pseudoCtaIconLine}
-                  d="M8 5H0"
-                  stroke="currentColor"
-                  strokeWidth={1.8}
-                />
-              </svg>
-            </span>
+      <Card.Content>
+        <Card.Meta items={[eventDate, eventType]}></Card.Meta>
+        <Card.Heading>{title}</Card.Heading>
+        {productBadges && productBadges?.length > 0 ? (
+          <Card.ProductBadges badges={productBadges} appearance={appearance} />
+        ) : null}
+        <span className={s.pseudoCta}>
+          <span className={s.pseudoCtaLabel}>{ctaText}</span>
+          <span className={s.pseudoCtaIcon}>
+            <svg
+              className={s.pseudoCtaIconArrow}
+              width="9"
+              height="10"
+              viewBox="0 0 9 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              focusable={false}
+            >
+              <path d="M4 1L8 5L4 9" stroke="currentColor" strokeWidth={1.8} />
+              <path
+                className={s.pseudoCtaIconLine}
+                d="M8 5H0"
+                stroke="currentColor"
+                strokeWidth={1.8}
+              />
+            </svg>
           </span>
-        </Card.Content>
-      </div>
+        </span>
+      </Card.Content>
     </Card>
   )
 }
