70b85a74cf53e234d72ea1e7d8abc136219615e8
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-10-25 17:05:38 -0700

    feat: use next/jest

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,21 @@
-module.exports = {
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
   moduleNameMapper: {
     '\\.module.css$': 'identity-obj-proxy',
     '\\.graphql$': '<rootDir>/__test-helpers/mocks/graphql-fragment-mock.js',
     // For .svg?include imports, remove the ?include suffix,
     // so that it can be resolved and loaded with jest-raw-loader
     '(.*)\\.svg\\?include$': '$1.svg',
-    // For all other svg imports, use a mock
-    '\\.svg$': '<rootDir>/__test-helpers/mocks/svg-mock.js',
   },
   transform: {
-    // Note: babel-jest transform is from Jest's defaults for `transform`
-    // ref: https://jestjs.io/docs/next/configuration
-    '\\.[jt]sx?$': [
-      '@swc-node/jest',
-      {
-        jsx: true,
-        react: { runtime: 'automatic' },
-        minify: false,
-      },
-    ],
     // Load .svg imports as raw strings.
     // Our mapping above means this only targets .svg?include imports.
-    '\\.svg$': 'jest-raw-loader',
+    '\\.svg$': '<rootDir>/__test-helpers/raw-loader.js',
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(@hashicorp/mktg-logos|@hashicorp/react-|@hashicorp/sentinel-embedded|@hashicorp/nextjs-scripts|@hashicorp/platform-|unist-|@hashicorp/flight-icons))',
@@ -29,5 +23,19 @@ module.exports = {
   ],
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/__test-helpers/extend-expect.js'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
+}
+
+const config = createJestConfig(customJestConfig)
+
+module.exports = async () => {
+  const baseConfig = await config()
+  // This step is necessary because the config provided by Next _explicitly_
+  // does not transform node_modules.
+  baseConfig.transformIgnorePatterns = customJestConfig.transformIgnorePatterns
+  return baseConfig
 }
