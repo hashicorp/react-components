887342d1b57df2fd3fc074b4608d37e9c8d199ac
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-10-27 16:35:46 -0400

    chore: Bring props.js up to spec

--- packages/video-feature/props.js
+++ packages/video-feature/props.js
@@ -1,16 +1,39 @@
+const introProps = require('../intro/props')
+const authorBylineProps = require('../author-byline/props')
+
 module.exports = {
-  children: {
-    description: 'TODO Update/remove this',
+  appearance: {
     type: 'string',
-    control: { type: 'text' },
-    testValue: 'New',
-    required: true,
+    description: 'Display the component on light or dark backgrounds.',
+    options: ['light', 'dark'],
   },
-  textSide: {
+  contentSide: {
     description:
-      'Side that content will appear on (update to correct nomenclature)',
+      'Side that content will appear on. (update to correct nomenclature)',
     type: 'string',
     control: { type: 'text' },
     testValue: 'left',
+  },
+  heading: introProps.heading,
+  description: introProps.description,
+  author: {
+    type: 'object',
+    description: 'Details about the video author.',
+    properties: authorBylineProps,
+  },
+  // TODO Move props into <InlineVideo /> and import from there
+  video: {
+    type: 'object',
+    properties: {
+      url: {
+        type: 'string',
+        description: 'URL for the featured video. [Host] links are supported.',
+        required: true,
+      },
+      caption: {
+        type: 'string',
+        description: 'Description for the video.',
+      },
+    },
   },
 }
