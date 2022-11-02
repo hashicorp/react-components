9ded44ce3eb43561ab89f646556fb3e6f335ce01
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-11-02 13:55:08 -0400

    feat: First pass at test suite

--- packages/video-feature/index.test.js
+++ packages/video-feature/index.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { queryByTestId, render, screen } from '@testing-library/react'
 import VideoFeature from '.'
 
 const defaultProps = {
   appearance: 'light',
-  contentSide: 'left',
+  contentPosition: 'left',
   heading: 'Display copy, tellus massa in lementum nulla donec nec',
   description:
     'Body copy, pharetra pellentesque sed elementum risus accumsan et. Tristique tortor, morbi vivamus nibh mollis. Ultrices aliquet sit nibh consequat quam vestibulum ipsum turpis. Sed quam vitae porttitor egestas. Luctus nibh ut posuere ultrices est arcu at aliquam in. Sem aenean laoreet molestie non.',
@@ -21,9 +21,60 @@ const defaultProps = {
 }
 
 describe('<VideoFeature />', () => {
-  it('TODO: Write tests', () => {
+  it('should render the provided heading text', () => {
     render(<VideoFeature {...defaultProps} />)
     const element = screen.getByText(defaultProps.heading)
     expect(element).toBeInTheDocument()
+  })
+
+  it('should render the provided description text', () => {
+    render(<VideoFeature {...defaultProps} />)
+    const element = screen.getByText(defaultProps.description)
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should render the default (light) appearance', () => {
+    const { container } = render(<VideoFeature {...defaultProps} />)
+
+    expect(container.firstChild).toHaveClass('videoFeature', 'light')
+    expect(container.firstChild).not.toHaveClass('dark')
+  })
+
+  it('should render the dark appearance', () => {
+    const { container } = render(
+      <VideoFeature {...defaultProps} appearance="dark" />
+    )
+
+    expect(container.firstChild).toHaveClass('videoFeature', 'dark')
+    expect(container.firstChild).not.toHaveClass('light')
+  })
+
+  it('should render with content positioned on the left by default', () => {
+    const { container } = render(<VideoFeature {...defaultProps} />)
+
+    expect(container.firstChild).toHaveClass('videoFeature', 'left')
+    expect(container.firstChild).not.toHaveClass('right')
+  })
+
+  it('should render with content positioned on the right', () => {
+    const { container } = render(
+      <VideoFeature {...defaultProps} contentPosition="right" />
+    )
+
+    expect(container.firstChild).toHaveClass('videoFeature', 'right')
+    expect(container.firstChild).not.toHaveClass('left')
+  })
+
+  it('should not render <AuthorByline /> without a provided author prop', () => {
+    const { container } = render(
+      <VideoFeature
+        heading={defaultProps.heading}
+        description={defaultProps.description}
+        video={defaultProps.video}
+      />
+    )
+
+    const element = queryByTestId(container, 'author-byline')
+    expect(element).not.toBeInTheDocument()
   })
 })
