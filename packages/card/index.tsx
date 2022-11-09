1d831e0ed2a9e376c0783cdb66704515e0cd5da2
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-11-09 09:28:23 -0500

    fix: Export PersonCard from index

--- packages/card/index.tsx
+++ packages/card/index.tsx
@@ -1,5 +1,6 @@
 import Card from './card'
 import { EventCard } from './event-card'
+import { PersonCard } from './person-card'
 
 export default Card
-export { EventCard }
+export { EventCard, PersonCard }
