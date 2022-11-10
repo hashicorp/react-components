6bd4f1cbf150b2f6238500004e00bd116c25a40a
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-11-10 14:00:44 -0800

    feat: export Field component (#791)

--- packages/marketo-form/index.ts
+++ packages/marketo-form/index.ts
@@ -1,5 +1,6 @@
 import { useFormContext, useFormState, useWatch } from 'react-hook-form'
 import Form, { defaultFieldGroupings } from './form'
+import Field from './partials/field'
 
-export { useFormContext, useFormState, useWatch, defaultFieldGroupings }
+export { useFormContext, useFormState, useWatch, defaultFieldGroupings, Field }
 export default Form
