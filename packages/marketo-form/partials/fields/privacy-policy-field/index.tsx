aff9ad98b613a458d1a2449349bbf0718e9446a0
Dylan Staley <88163+dstaley@users.noreply.github.com>
2022-11-10 16:30:15 -0800

    feat: add support for ReactNode in privacy policy label

--- packages/marketo-form/partials/fields/privacy-policy-field/index.tsx
+++ packages/marketo-form/partials/fields/privacy-policy-field/index.tsx
@@ -3,6 +3,7 @@ import CheckboxInput from '@hashicorp/react-form-fields/checkbox'
 import FieldWrapper from '../../field-wrapper'
 import type { MarketoFormCheckboxField } from '../../../types'
 import { useErrorMessage } from '../../../utils'
+import style from './style.module.css'
 
 const PrivacyPolicyField = ({ field }: { field: MarketoFormCheckboxField }) => {
   const { register, watch } = useFormContext()
@@ -12,14 +13,19 @@ const PrivacyPolicyField = ({ field }: { field: MarketoFormCheckboxField }) => {
   return (
     <FieldWrapper size="lg">
       <CheckboxInput
-        label={`I agree to HashiCorp’s
-                   <a
-                     href="https://www.hashicorp.com/privacy"
-                     target="_blank"
-                     rel="noopener noreferrer"
-                   >
-                     Privacy Policy
-                   </a>`}
+        label={
+          <>
+            I agree to HashiCorp’s{' '}
+            <a
+              href="https://www.hashicorp.com/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>
+            <span className={style.required}>*</span>
+          </>
+        }
         field={{
           ...register(field.id),
           checked,
