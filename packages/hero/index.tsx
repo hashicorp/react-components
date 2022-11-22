d4b869cd0bbd711dbaab8f1ddd15b215145dbff7
Nels Andereck <nels.andereck@hashicorp.com>
2022-11-22 13:15:21 -0800

    wip

--- packages/hero/index.tsx
+++ packages/hero/index.tsx
@@ -4,23 +4,26 @@ import type { HeroProps } from './types'
 import s from './style.module.css'
 
 const Hero = ({
-  appearance = 'light',
+  appearance = 'dark',
   eyebrow,
   heading,
   description,
   actions,
   url,
+  smallImage = `${require('./images/graphic-small.svg')}`,
+  mediumImage = `${require('./images/graphic-medium.svg')}`,
+  desktopImage = `${require('./images/graphic.svg')}`,
 }: HeroProps) => {
   return (
     <div className={s.root}>
       {url ? (
         <div className={s.videoWrapper}>
-          <InlineVideo appearance={appearance} url={url} />{' '}
+          <InlineVideo appearance={appearance} url={url} />
         </div>
       ) : null}
-      <div className={s.introWrapper}>
+      <div className={s.content}>
         <div>
-          <EyebrowWithPattern text={eyebrow} />
+          <EyebrowWithPattern appearance={appearance} text={eyebrow} />
         </div>
         <Intro
           appearance={appearance}
@@ -34,7 +37,7 @@ const Hero = ({
   )
 }
 
-const EyebrowWithPattern = ({ text }) => {
+const EyebrowWithPattern = ({ appearance, text }) => {
   return (
     <div>
       <p>{text}</p>
--- packages/hero/types.ts
+++ packages/hero/types.ts
@@ -1,8 +1,14 @@
 import { InlineVideoProps } from '@hashicorp/react-inline-video/types'
 import { IntroProps } from '@hashicorp/react-intro/types'
 
-export type HeroProps = Pick<
+export type ContentProps = Pick<
   IntroProps,
   'appearance' | 'eyebrow' | 'heading' | 'description' | 'actions'
 > &
   Pick<InlineVideoProps, 'url'>
+
+export interface HeroProps extends ContentProps {
+  smallImage: string
+  mediumImage: string
+  desktopImage: string
+}
