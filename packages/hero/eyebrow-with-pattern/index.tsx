cc7eaf85b712327a13ed6cb5ccf40f3202a84141
Nels Andereck <nels.andereck@hashicorp.com>
2022-11-22 14:23:03 -0800

    create eyebrowWithPattern

---
+++ packages/hero/eyebrow-with-pattern/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classNames from 'classnames'
+import s from './style.module.css'
+
+interface EyebrowProps {
+  text: string
+  appearance: 'light' | 'dark'
+  theme: 'infrastructure' | 'security' | 'networking' | 'applications'
+}
+
+export default function Eyebrow({
+  text,
+  appearance = 'light',
+  theme,
+}: EyebrowProps) {
+  return (
+    <div className={classNames(s.eyebrow, s[theme])}>
+      <span className={classNames(s.text, s[appearance])}>{text}</span>
+      <span
+        className={s.pattern}
+        style={
+          {
+            '--bg-image': `url(${require('./img/bg.svg')})`,
+          } as React.CSSProperties
+        }
+      />
+    </div>
+  )
+}
