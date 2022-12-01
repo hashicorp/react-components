60c12abb9495ee8c6836559a2f5f0158ad1b084f
Nels Andereck <nels.andereck@hashicorp.com>
2022-12-01 12:13:17 -0800

    swap out intro with custom jsx

--- packages/hero/types.ts
+++ packages/hero/types.ts
@@ -1,19 +1,28 @@
 import { EyebrowProps } from './eyebrow-with-pattern'
 import { InlineVideoProps } from '@hashicorp/react-inline-video/types'
-import { IntroProps } from '@hashicorp/react-intro/types'
+import { StandaloneLinkProps } from '@hashicorp/react-standalone-link/types'
+import { Theme } from '@hashicorp/react-button/types'
 
 type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 
-export type ContentProps = Pick<
-  IntroProps,
-  'appearance' | 'eyebrow' | 'heading' | 'description' | 'actions'
-> &
-  WithOptional<InlineVideoProps, 'url'> &
+export type ContentProps = WithOptional<InlineVideoProps, 'url'> &
   WithOptional<EyebrowProps, 'theme'>
 
 export interface HeroProps extends ContentProps {
+  appearance: 'light' | 'dark'
+  eyebrow?: string
+  title: string
+  description: string
+  primaryCta: PrimaryCtaProps
+  secondaryCta?: StandaloneLinkProps
   backgroundColor?: string
   smallImage?: string
   mediumImage?: string
   largeImage?: string
+}
+
+interface PrimaryCtaProps {
+  title: string
+  href: string
+  brand: Pick<Theme, 'brand'>
 }
