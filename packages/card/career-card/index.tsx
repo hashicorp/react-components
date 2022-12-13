e6df0d97a20c6fba1f344e76dcacc1a3dfed6baa
Pamela Bortnick <pbortnick@gmail.com>
2022-12-13 10:10:30 -0500

    update: add career card

---
+++ packages/card/career-card/index.tsx
@@ -0,0 +1,34 @@
+import * as CardPrimitives from '../primitives'
+import type { CardProps, MetaProps } from '../types'
+import s from './style.module.css'
+
+interface CareerCardProps {
+  heading: string
+  subHeading: string
+  meta: MetaProps['items']
+  link: string
+  productBadges: CardProps['productBadges']
+}
+
+export default function CareerCard({
+  heading,
+  meta,
+  link,
+  subHeading,
+  productBadges,
+}: CareerCardProps) {
+  return (
+    <CardPrimitives.Card heading={heading} link={link} withArrow={false}>
+      <CardPrimitives.Content>
+        <CardPrimitives.Meta items={meta}></CardPrimitives.Meta>
+        <p className={s.subHeading}>{subHeading}</p>
+        <CardPrimitives.Heading>{heading}</CardPrimitives.Heading>
+        <div className={s.bottom}>
+          {productBadges && productBadges?.length > 0 ? (
+            <CardPrimitives.ProductBadges badges={productBadges} />
+          ) : null}
+        </div>
+      </CardPrimitives.Content>
+    </CardPrimitives.Card>
+  )
+}
