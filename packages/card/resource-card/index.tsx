702650232d54c123841171f8cfaf11530f049c84
Pamela Bortnick <pbortnick@gmail.com>
2022-12-13 10:17:31 -0500

    update: add resource card

---
+++ packages/card/resource-card/index.tsx
@@ -0,0 +1,34 @@
+import * as CardPrimitives from '../primitives'
+import type { MetaProps, CardProps, ThumbnailProps } from '../types'
+import s from './style.module.css'
+
+interface ResourceCardProps {
+  heading: string
+  meta: MetaProps['items']
+  link: string
+  productBadges: CardProps['productBadges']
+  thumbnail: ThumbnailProps
+}
+
+export default function ResourceCard({
+  heading,
+  meta,
+  link,
+  productBadges,
+  thumbnail,
+}: ResourceCardProps): JSX.Element {
+  return (
+    <CardPrimitives.Card heading={heading} link={link} withArrow={false}>
+      <CardPrimitives.Thumbnail {...thumbnail} />
+      <CardPrimitives.Content>
+        <CardPrimitives.Meta items={meta}></CardPrimitives.Meta>
+        <CardPrimitives.Heading>{heading}</CardPrimitives.Heading>
+        <div className={s.bottom}>
+          {productBadges && productBadges?.length > 0 ? (
+            <CardPrimitives.ProductBadges badges={productBadges} />
+          ) : null}
+        </div>
+      </CardPrimitives.Content>
+    </CardPrimitives.Card>
+  )
+}
