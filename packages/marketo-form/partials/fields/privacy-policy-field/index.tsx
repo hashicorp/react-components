7112ab8f7f95e4ef72908a159d36f0e6afcc2f13
Dylan Staley <88163+dstaley@users.noreply.github.com>
2023-01-03 13:04:21 -0800

    feat: add support for Marketo-driven localization

--- packages/marketo-form/partials/fields/privacy-policy-field/index.tsx
+++ packages/marketo-form/partials/fields/privacy-policy-field/index.tsx
@@ -1,11 +1,73 @@
+import { useContext } from 'react'
 import { useFormContext } from 'react-hook-form'
 import CheckboxInput from '@hashicorp/react-form-fields/checkbox'
 import FieldWrapper from '../../field-wrapper'
+import { FormMetadataContext } from '../../../contexts/FormMetadata'
 import type { MarketoFormCheckboxField } from '../../../types'
 import { useErrorMessage } from '../../../utils'
 import style from './style.module.css'
 
+const LABELS_BY_LANGUAGE = {
+  English: (
+    <>
+      I agree to HashiCorp’s{' '}
+      <a
+        href="https://www.hashicorp.com/privacy"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Privacy Policy
+      </a>
+      <span className={style.required}>*</span>
+    </>
+  ),
+  French: (
+    <>
+      J&apos;accepte la politique de confidentialité d&apos;HashiCorp.
+      <span className={style.required}>*</span>
+    </>
+  ),
+  Korean: (
+    <>
+      향후 HashiCorp의 마케팅 이벤트 및 프로모션, 유용한 정보와 교육 등 안내를
+      받으실 수 있습니다. (자세한 내용은 Privacy Policy를 참고하시기 바랍니다:
+      HashiCorp)
+    </>
+  ),
+  Japanese: (
+    <>
+      HashiCorpのプライバシーポリシーに同意します。
+      <span className={style.required}>*</span>
+    </>
+  ),
+  German: (
+    <>
+      Ich stimme den Datenschutzrichtlinien von HashiCorp zu.
+      <span className={style.required}>*</span>
+    </>
+  ),
+  Spanish: (
+    <>
+      Estoy de acuerdo con la política de privacidad de HashiCorp.
+      <span className={style.required}>*</span>
+    </>
+  ),
+  'Portuguese (Brazil)': (
+    <>
+      Eu concordo com a Política de Privacidade da HashiCorp.
+      <span className={style.required}>*</span>
+    </>
+  ),
+  Italian: (
+    <>
+      Accetto l&apos;informativa sulla privacy di HashiCorp.
+      <span className={style.required}>*</span>
+    </>
+  ),
+}
+
 const PrivacyPolicyField = ({ field }: { field: MarketoFormCheckboxField }) => {
+  const metadata = useContext(FormMetadataContext)
   const { register, watch } = useFormContext()
   const error = useErrorMessage(field.id)
   const checked = watch(field.id, false)
@@ -14,17 +76,9 @@ const PrivacyPolicyField = ({ field }: { field: MarketoFormCheckboxField }) => {
     <FieldWrapper fieldId={field.id}>
       <CheckboxInput
         label={
-          <>
-            I agree to HashiCorp’s{' '}
-            <a
-              href="https://www.hashicorp.com/privacy"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Privacy Policy
-            </a>
-            <span className={style.required}>*</span>
-          </>
+          metadata && metadata.language in LABELS_BY_LANGUAGE
+            ? LABELS_BY_LANGUAGE[metadata.language]
+            : LABELS_BY_LANGUAGE.English
         }
         field={{
           ...register(field.id),
