ba362e6fd974d3f256970df3238eeb78a738bf26
Nels Andereck <nels.andereck@hashicorp.com>
2023-04-28 13:15:23 -0700

    Revert "export dommax to allow for layout animations"
    This reverts commit 341357853a00c01ec2cbf8b0ec0981058ddc9f84.

--- packages/motion-config/features.ts
+++ packages/motion-config/features.ts
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { domMax } from 'framer-motion'
+import { domAnimation } from 'framer-motion'
+import { layoutFeatures } from 'framer-motion/dist/es/motion/features/layout'
+import { HTMLProjectionNode } from 'framer-motion/dist/es/projection/node/HTMLProjectionNode'
 
-export default domMax
+export default {
+  ...domAnimation,
+  ...layoutFeatures,
+  projectionNodeConstructor: HTMLProjectionNode,
+}
