d1f12b04bc54681b919213ed7bb3acb585a57c92
Pamela Bortnick <pbortnick@gmail.com>
2023-05-03 10:17:24 -0400

    chore: delete unused component and update type name

--- packages/consent-manager/types.ts
+++ packages/consent-manager/types.ts
@@ -13,7 +13,11 @@ export interface ConsentManagerService {
   name: string
   url?: string
   dataAttrs?: { name: string; value: string }[]
-  hide?: () => boolean
+  /**
+   * Optional function that determines whether
+   * or not the service should load on the page
+   */
+  shouldLoad?: () => boolean
 }
 
 export interface ConsentManagerPreset {
