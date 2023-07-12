0b968b06ef4a6d1af429fa07c254ebc18dac8d50
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2023-07-12 12:29:53 -0400

    chore: remove unnecessary console log

--- packages/card/newsroom-card/index.tsx
+++ packages/card/newsroom-card/index.tsx
@@ -25,7 +25,6 @@ export function NewsroomCard({
   appearance,
   withArrow = false,
 }: NewsroomCardProps): JSX.Element {
-  console.log(withArrow)
   return (
     <CardPrimitives.Card
       heading={heading}
