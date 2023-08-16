c3b49c15aa25f293df869c1c735eb1add2ebbcde
Nels Andereck <nels.andereck@hashicorp.com>
2023-08-16 16:31:04 -0700

    add vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,14 +5,18 @@
 
 import './global.css'
 import { TabProvider } from '../packages/tabs'
+import { Analytics } from '@vercel/analytics/react'
 import MotionConfig from '../packages/motion-config'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <MotionConfig>
-      <TabProvider>
-        <Component {...pageProps} />
-      </TabProvider>
-    </MotionConfig>
+    <>
+      <Analytics />
+      <MotionConfig>
+        <TabProvider>
+          <Component {...pageProps} />
+        </TabProvider>
+      </MotionConfig>
+    </>
   )
 }
