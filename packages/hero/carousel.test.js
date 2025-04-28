d8bb4be2ac9756a09e64f3d1224dcf48d3188d32
Leah Bush <leah.bush@hashicorp.com>
2025-04-28 10:14:38 -0500

    chore: audit io components

--- packages/hero/carousel.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
-test.todo('renders each video')
-test.todo('shows the currently active video')
-test.todo('switches videos automatically when one ends')
-test.todo('switches videos on click')
